1cc621b871cf38132cededaa3c3b4ea8beb4b989
Sebas200702 <sebastorregroza6@gmail.com>
2024-12-08 23:40:10 -0500

    test getAnime enpoint with other anime to fix problems with database queries

--- src/test/api-tests/getAnime.test.ts
+++ src/test/api-tests/getAnime.test.ts
@@ -5,11 +5,14 @@ import type { Anime } from '../../types'
 describe('Endpoint test get anime with slug', () => {
   it('should return a anime', async () => {
     const anime: Anime | undefined = await fetch(
-      `${baseUrl}/api/getAnime?slug=Spy%20x%20Family%20Part%202`
+      `${baseUrl}/api/getAnime?slug=Kaguya-sama%20wa%20Kokurasetai?%20Tensai-tachi%20no%20Renai%20Zunousen`
     )
       .then((res) => res.json())
       .then((data) => data.anime)
     expect(anime).toBeDefined()
-    expect(anime?.title).toBe('Spy x Family Part 2')
+    console.log(anime)
+    expect(anime?.title).toBe(
+      'Kaguya-sama wa Kokurasetai? Tensai-tachi no Renai Zunousen'
+    )
   })
 })
