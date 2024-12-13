b30a964de4c039c5fb39a6be3c91b833551252c1
Sebas200702 <sebastorregroza6@gmail.com>
2024-12-13 17:39:00 -0500

    add pagination to animes endpoint

--- src/pages/api/animes.ts
+++ src/pages/api/animes.ts
@@ -5,6 +5,7 @@ import { normalizeString } from '@utils'
 export const GET: APIRoute = async ({ url }) => {
   enum Filters {
     limit_count = 'limit_count',
+    page_number = 'page_number',
     genre_filter = 'genre_filter',
     type_filter = 'type_filter',
     studio_filter = 'studio_filter',
@@ -32,8 +33,9 @@ export const GET: APIRoute = async ({ url }) => {
   const filters = GetFilters(Filters)
 
   const { data, error } = await supabase.rpc('get_animes', filters)
-
+  console.log(data)
   if (error) {
+    console.error(error)
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
       headers: {
