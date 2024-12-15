9323b470d2159a061278a2d373d35fd901ae3c33
Sebas200702 <sebastorregroza6@gmail.com>
2024-12-15 12:28:12 -0500

    add skip prop to useFecth

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -3,35 +3,42 @@ import { useState, useEffect } from 'react'
 interface Params {
   url: string
   options?: RequestInit
+  skip?: boolean
 }
+
 type Error = string | null
 
-export const useFetch = <T>({ url, options }: Params) => {
+export const useFetch = <T>({ url, options, skip }: Params) => {
   const [data, setData] = useState<T | null>(null)
   const [error, setError] = useState<Error>(null)
   const [loading, setLoading] = useState<boolean>(false)
 
   useEffect(() => {
-    if (!url) return
+    if (!url || skip) return
 
     const fetchData = async () => {
       setLoading(true)
       try {
         const response = await fetch(url, options)
-        if (!response.ok) throw new Error('Error fetching data')
 
-        const json = await response.json().then((data) => data.anime)
+        if (!response.ok) {
+          const errorMessage = `Error fetching data: ${response.statusText}`
+          throw new Error(errorMessage)
+        }
 
+        const json: T = await response.json().then((data) => data.anime)
         setData(json)
-      } catch (error) {
-        setError(error as Error)
+      } catch (error: any) {
+        const errorMessage =
+          error instanceof Error ? error.message : 'An unknown error occurred'
+        setError(errorMessage)
       } finally {
         setLoading(false)
       }
     }
 
     fetchData()
-  }, [url, options])
+  }, [url, options, skip])
 
   return { data, error, loading }
 }
