062aa3ae7e9df9a36f4ffe3a8ccaac7337ebd8a3
Sebas200702 <sebastorregroza6@gmail.com>
2024-12-19 11:31:13 -0500

    fix url

--- src/utils.ts
+++ src/utils.ts
@@ -45,7 +45,7 @@ export enum AnimeFilters {
 
 export const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://animeflix.vercel.app'
+    ? 'https://animeflix-rust.vercel.app'
     : 'http://localhost:4321'
 
 export const reduceSynopsis = (synopsis?: string, size = 450) => {
