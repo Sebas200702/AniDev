4cc9eb0c8fddf19fee88e0ae3039e078e5f5c411
Sebas200702 <sebastorregroza6@gmail.com>
2024-12-19 19:42:26 -0500

    remove image services vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,6 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    imageService: true,
-  }),
+  adapter: vercel(),
   integrations: [tailwind(), react()],
 })
