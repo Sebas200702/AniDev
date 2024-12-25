d05a483580b0ca7964cf94f0c0667c7ae3302834
Sebas200702 <sebastorregroza6@gmail.com>
2024-12-25 18:54:52 -0500

    update anime search results

--- src/components/search-results.tsx
+++ src/components/search-results.tsx
@@ -13,19 +13,16 @@ export const SearchResults = () => {
 
   if (!animes || loading) {
     return (
-      <div className="grid w-full grid-cols-3 gap-4">
-        {Array(6)
+      <div className="mx-auto grid w-full max-w-7xl grid-cols-6 gap-4">
+        {Array(30)
           .fill(0)
           .map((_, i) => (
             <div
               key={i + 1}
-              className="flex h-28 w-full animate-pulse flex-row items-center gap-6 rounded-lg bg-gray-200 p-4 duration-200"
+              className="flex h-auto w-full animate-pulse flex-col gap-2 rounded-lg bg-gray-200 p-2 duration-200"
             >
-              <div className="aspect-[225/330] h-full w-[20%] animate-pulse rounded-lg bg-gray-400 duration-200"></div>
-              <div className="flex h-full w-full flex-col justify-center gap-2">
-                <div className="h-[30%] w-[60%] animate-pulse rounded-lg bg-gray-400 duration-200"></div>
-                <div className="h-[30%] w-[20%] animate-pulse rounded-full bg-gray-400 duration-200"></div>
-              </div>
+              <div className="aspect-[225/330] h-[89%] w-full animate-pulse rounded-lg bg-gray-400 p-2 duration-200"></div>
+              <div className="flex h-6 w-full animate-pulse rounded-lg bg-gray-400 transition-all duration-200 ease-in-out"></div>
             </div>
           ))}
       </div>
@@ -42,7 +39,7 @@ export const SearchResults = () => {
 
   return (
     <ul
-      className={`grid w-full grid-cols-3 gap-4 transition-opacity duration-500 ${fadeIn ? 'opacity-100' : 'opacity-0'}`}
+      className={`mx-auto grid w-full max-w-7xl grid-cols-6 gap-4 transition-opacity duration-500 ${fadeIn ? 'opacity-100' : 'opacity-0'}`}
     >
       {animes.map(({ title, image_webp, mal_id, type }) => (
         <AnimeResult
@@ -56,3 +53,4 @@ export const SearchResults = () => {
     </ul>
   )
 }
+
