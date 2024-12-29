3472d05e889a70eb1c41cc2d2900405367b2873a
Sebas200702 <sebastorregroza6@gmail.com>
2024-12-28 21:43:38 -0500

    add dinamic filters

--- src/components/anime-tag.tsx
+++ src/components/anime-tag.tsx
@@ -1,3 +1,4 @@
+import { getFilterOfTag } from '@utils/get-filter-of-tag'
 import { getTagColor } from '@utils/get-tag-color'
 
 interface Props {
@@ -7,12 +8,13 @@ interface Props {
 }
 
 export const AnimeTag = ({ tag, type, style }: Props) => {
-  const tagColor = getTagColor(type ?? '')
+  const tagColor = getTagColor(tag ?? '')
+  const filter = getFilterOfTag(tag)
 
   return (
     <a
-      href=""
-      className={`${style ?? 'w-min'} h-min rounded-2xl px-2 py-1 text-xs font-medium text-gray-800 transition-all duration-200 ease-in-out hover:${tagColor}`}
+      href={`/search?${filter}=${type?.toLowerCase()}`}
+      className={`${style ?? 'w-min'} h-min rounded-2xl px-2 py-1 text-xs font-medium text-gray-800 transition-all duration-200 ease-in-out ${tagColor}`}
       aria-label={`Tag: ${tag}`}
     >
       {tag}
