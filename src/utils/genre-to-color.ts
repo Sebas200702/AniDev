df1fdec89224e7f876741a2c1be7c4f9be7c30c8
Sebas200702 <sebastorregroza6@gmail.com>
2024-12-30 20:53:51 -0500

    create utility genre to color

---
+++ src/utils/genre-to-color.ts
@@ -0,0 +1,16 @@
+import { AnimeGenres } from 'types'
+export const genreToColor = (genre: string) => {
+  if (genre === AnimeGenres.ACTION) return 'md:group-hover:text-red-400'
+  if (genre === AnimeGenres.ADVENTURE) return 'md:group-hover:text-blue-400'
+  if (genre === AnimeGenres.GIRLS_LOVE) return 'md:group-hover:text-purple-200'
+  if (genre === AnimeGenres.COMEDY) return 'md:group-hover:text-yellow-400'
+  if (genre === AnimeGenres.DRAMA) return 'md:group-hover:text-purple-800'
+  if (genre === AnimeGenres.ROMANCE) return 'md:group-hover:text-pink-400'
+  if (genre === AnimeGenres.SCI_FI) return 'md:group-hover:text-indigo-400'
+  if (genre === AnimeGenres.SLICE_OF_LIFE) return 'md:grouphover:text-green-400'
+  if (genre === AnimeGenres.SPORTS) return 'md:group-hover:text-orange-400'
+  if (genre === AnimeGenres.FANTASY) return 'md:group-hover:text-pink-600'
+  if (genre === AnimeGenres.MYSTERY) return 'md:group-hover:text-yellow-200'
+  if (genre === AnimeGenres.SCI_FI) return 'md:group-hover:text-slate-400'
+  return 'md:group-hover:text-gray-400'
+}
