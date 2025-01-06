e7b79675c865733877df5d62aecade3bd9fd54f5
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-05 21:58:14 -0500

    create utility split on point

---
+++ src/utils/split-text-on-p.ts
@@ -0,0 +1,16 @@
+export const splitTextOnP = (text: string) => {
+  const midpoint = Math.floor(text.length / 2)
+
+  let splitPoint = text.lastIndexOf('.', midpoint)
+  if (splitPoint === -1) {
+    splitPoint = text.indexOf('.', midpoint)
+  }
+
+  if (splitPoint === -1) {
+    return [text, '']
+  }
+  const part1 = text.slice(0, splitPoint + 1).trim()
+  const part2 = text.slice(splitPoint + 1).trim()
+
+  return [part1, part2]
+}
