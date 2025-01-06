4665797e0b7580f32091b4771401ab420b907b5e
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-05 21:49:20 -0500

    fix bug on icon styles

--- src/components/icons/play-icon.tsx
+++ src/components/icons/play-icon.tsx
@@ -1,15 +1,21 @@
 interface Props {
-  class?: string
+  width?: string
+  height?: string
 }
 
-export const PlayIcon = ({ class: className }: Props) => (
+export const PlayIcon = ({ width, height }: Props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    fill="currentColor"
-    className={`${className ?? ''} `}
-    viewBox="0 0 32 32"
+    fill="none"
+    width={width}
+    height={height}
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    viewBox="0 0 24 24"
   >
-    <path fill="none" d="M0 0h24v24H0z"></path>
-    <path d="M6 4v16a1 1 0 0 0 2 1l13-8a1 1 0 0 0 0-2L8 3a1 1 0 0 0-2 1z"></path>
+    <path d="M0 0h24v24H0z" stroke="none" />
+    <path d="M7 4v16l13-8z" />
   </svg>
 )
