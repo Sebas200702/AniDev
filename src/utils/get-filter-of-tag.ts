d3536a9190e9bd2b17a43f39f9e30610e9da5266
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-05 21:56:54 -0500

    fix bug on getfilter tag

--- src/utils/get-filter-of-tag.ts
+++ src/utils/get-filter-of-tag.ts
@@ -1,4 +1,6 @@
 import { AnimeFilters } from 'types'
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+const haveNumbers = (str: string) => numbers.some((n) => str.includes(n))
 export const getFilterOfTag = (tag: string) => {
   if (tag === 'Anime') return AnimeFilters.Type
   if (tag === 'Special') return AnimeFilters.Type
@@ -6,5 +8,6 @@ export const getFilterOfTag = (tag: string) => {
   if (tag === 'ONA') return AnimeFilters.Type
   if (tag === 'Movie') return AnimeFilters.Type
   if (tag === 'Music') return AnimeFilters.Type
+  if (haveNumbers(tag)) return AnimeFilters.Year
   return AnimeFilters.Genre
 }
