fad28076bf771ef58bf2243b7669ce45ee36daec
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-05 21:30:33 -0500

    simplify desing ui

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,8 +1,6 @@
-import { AnimeTag } from '@components/anime-tag'
 import { useFetch } from '@hooks/useFetch'
 import { createImageUrlProxy } from '@utils/craete-imageurl-proxy'
-import { reduceSynopsis } from '@utils/reduce-synopsis'
-import { normalizeString } from '@utils/normalize-string'
+import { reduceString } from '@utils/reduce-string'
 import { createDynamicBannersUrl } from '@utils/create-dynamic-banners-url'
 import { useCarouselStore } from '@store/carousel-store'
 import { memo, useCallback, useEffect } from 'react'
@@ -10,6 +8,7 @@ import { useCarouselScroll } from '@hooks/useCarouselScroll'
 import type { Anime } from 'types'
 import '@styles/no-scrolbar.css'
 import '@styles/fade-out.css'
+import '@styles/buttons.css'
 
 const Indicator = memo(
   ({
@@ -35,15 +34,17 @@ const Indicator = memo(
 )
 
 const LoadingCarousel = () => (
-  <div className="carousel-anime-banner relative h-[500px] animate-pulse bg-gray-200">
+  <div className="carousel-anime-banner relative h-[80dvh] animate-pulse bg-zinc-800">
     <div className="relative flex h-full w-full flex-shrink-0 flex-col items-center px-8 py-4 md:flex-row">
-      <div className="z-10 ml-5 flex h-auto max-h-[60%] w-full items-center justify-center p-4 md:h-full md:max-h-full md:w-1/3">
-        <div className="mx-auto aspect-[225/330] h-auto w-full max-w-52 animate-pulse rounded-lg bg-gray-400 md:h-full md:max-h-[90%] md:w-auto md:max-w-full"></div>
-      </div>
-      <div className="z-10 mx-auto flex w-full flex-col items-center p-6 text-white md:ml-8 md:mr-16 md:items-start">
-        <div className="z-30 mb-4 mt-4 h-8 w-full max-w-[70%] animate-pulse rounded-lg bg-gray-400 md:w-[60%]"></div>
-        <div className="mb-6 hidden h-20 w-full animate-pulse rounded-lg bg-gray-400 md:flex"></div>
-        <div className="h-8 w-[40%] animate-pulse rounded-lg bg-gray-400"></div>
+      <div className="z-10 mx-auto flex w-full max-w-2xl flex-col gap-4 items-center p-6 text-white md:ml-8 md:mr-16 md:items-start">
+        <div className="z-30 mb-4 mt-4 h-20 w-full  animate-pulse rounded-lg bg-zinc-700 "></div>
+        <div className="z-30  h-6 w-full animate-pulse rounded-lg bg-zinc-700"></div>
+        <div className="z-30 mb-4 h-6 w-full animate-pulse rounded-lg bg-zinc-700"></div>
+
+        <div className="flex w-full flex-row items-center gap-4 mt-2">
+          <div className="hidden h-10 w-28 animate-pulse rounded-lg bg-zinc-700 md:flex"></div>
+          <div className="h-10 w-28 animate-pulse rounded-lg bg-zinc-700"></div>
+        </div>
       </div>
     </div>
     <div className="absolute bottom-6 left-1/2 z-30 flex h-6 w-60 -translate-x-1/2 animate-pulse rounded-lg bg-gray-400"></div>
@@ -144,7 +145,7 @@ export const Carousel = () => {
 
   return (
     <div
-      className={`carousel-anime-banner relative left-0 right-0 h-[500px] ${fadeIn ? 'opacity-100 transition-all duration-200' : 'opacity-0'} `}
+      className={`carousel-anime-banner relative left-0 right-0 h-[50dvh] md:h-[90dvh] ${fadeIn ? 'opacity-100 transition-all duration-200' : 'opacity-0'} `}
       data-carousel="slide"
       style={{ position: 'sticky' }}
     >
@@ -165,45 +166,39 @@ export const Carousel = () => {
               <div
                 className="absolute inset-0 -z-10 h-full w-full bg-cover bg-center"
                 style={{
-                  backgroundImage: `url(${createImageUrlProxy(anime.banner_image, '0', '20', 'webp')})`,
+                  backgroundImage: `url(${createImageUrlProxy(anime.banner_image, '1920', '50', 'webp')})`,
                 }}
               />
-              <div className="absolute inset-0 bg-gradient-to-b from-black/60 to-black/90" />
-              <a
-                href={`${normalizeString(anime.title)}_${anime.mal_id}`}
-                className="z-10 flex h-auto max-h-[60%] w-full items-center justify-center p-4 md:h-full md:max-h-[90%] md:w-1/4"
-              >
-                <picture className="flex aspect-[225/330] h-auto max-h-72 w-full items-center justify-center rounded-lg object-cover object-center shadow-lg md:max-h-[90%]">
-                  <img
-                    src={anime.image_large_webp}
-                    className="aspect-[225/330] h-auto max-h-72 w-auto rounded-lg object-cover object-center shadow-lg md:max-h-[90%]"
-                    alt={anime.title}
-                    loading="lazy"
-                  />
-                </picture>
-              </a>
+              <div className="absolute bottom-0 left-0 right-0 h-full bg-gradient-to-b from-transparent to-base" />
               <div
-                className={`flex-1 p-6 ${index % 2 === 0 ? 'md:ml-8 md:mr-16' : 'md:ml-16 md:mr-8'} z-10 text-white`}
+                className={`mb-20 flex flex-col items-center gap-8 p-6 md:items-start md:justify-start md:gap-4 ${index % 2 === 0 ? 'md:ml-8 md:mr-16' : 'md:ml-16 md:mr-8'} z-10 max-w-2xl text-white`}
               >
-                <a href={`${normalizeString(anime.title)}_${anime.mal_id}`}>
-                  <h2 className="max-h-16 overflow-hidden text-center text-2xl font-bold text-white drop-shadow-md md:mb-4 md:text-left md:text-3xl">
-                    {anime.title}
-                  </h2>
-                  <p className="mb-4 hidden text-base text-white drop-shadow md:flex">
-                    {reduceSynopsis(anime.synopsis, 300)}
-                  </p>
-                </a>
-                <footer className="mx-auto mt-4 flex w-full flex-row justify-center gap-2 md:justify-normal">
-                  {anime.genres.slice(0, 3).map((tag: string) => (
-                    <AnimeTag key={tag} tag={tag} type={tag} style="w-auto" />
-                  ))}
-                </footer>
+                <h2 className="max-h-32 overflow-hidden text-center text-2xl font-bold text-white drop-shadow-md md:mb-4 md:text-left md:text-5xl">
+                  {reduceString(anime.title, 40)}
+                </h2>
+                <p className="mb-4 hidden text-xl text-white drop-shadow md:flex">
+                  {reduceString(anime.synopsis, 100)}
+                </p>
+                <div className="flex flex-row items-center gap-4">
+                  <a
+                    href={`/${anime.title}_${anime.mal_id}`}
+                    className="button-primary"
+                  >
+                    Learn More
+                  </a>
+                  <a
+                    href={`/watch/${anime.title}_${anime.mal_id}`}
+                    className="button-secondary"
+                  >
+                    Watch Now
+                  </a>
+                </div>
               </div>
             </div>
           ))}
         </div>
       </div>
-      <div className="absolute bottom-6 left-1/2 z-30 flex -translate-x-1/2 space-x-3">
+      <div className="absolute bottom-[20%] left-1/2 z-30 flex -translate-x-1/2 space-x-3">
         {banners.map((anime, index) => (
           <Indicator
             key={anime.mal_id}
