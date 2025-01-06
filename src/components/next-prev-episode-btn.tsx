3c3c8410ddab9f92c23336936d5c3793c80e202f
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-05 21:46:42 -0500

    more informative anime nex prev buton episode

--- src/components/next-prev-episode-btn.tsx
+++ src/components/next-prev-episode-btn.tsx
@@ -1,4 +1,4 @@
-type ActionType = 'Next' | 'Prev'
+type ActionType = 'Next' | 'Previous'
 interface Props {
   episodesLength: number
   currentEpisode: number
@@ -17,7 +17,7 @@ export const NextPrevEpisodeBtn = ({
     if (action === 'Next' && currentEpisode < episodesLength) {
       return `/watch/${slug}?ep=${currentEpisode + 1}`
     }
-    if (action === 'Prev' && currentEpisode > 1) {
+    if (action === 'Previous' && currentEpisode > 1) {
       return `/watch/${slug}?ep=${currentEpisode - 1}`
     }
     return ''
@@ -25,10 +25,10 @@ export const NextPrevEpisodeBtn = ({
 
   return (
     <a
-      className={`flex items-center justify-center rounded-full bg-gray-800 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 ${action === 'Prev' && isStart ? 'hidden' : ''} ${action === 'Next' && isEnd ? 'hidden' : ''}`}
+      className={`flex items-center justify-center rounded-sm  px-4 py-2 text-sm font-medium text-white transition-colors hover:opacity-50 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 ${action === 'Previous' && isStart ? 'hidden' : ''} ${action === 'Next' && isEnd ? 'hidden' : ''}`}
       href={getHref()}
     >
-      {action} Episode
+      {action}
     </a>
   )
 }
