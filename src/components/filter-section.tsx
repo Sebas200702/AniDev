eea936f7367b56a6bf2bcf7c8d25706b7a75a5d5
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-05 21:32:54 -0500

    add new filter an remove unnecesar things

--- src/components/filter-section.tsx
+++ src/components/filter-section.tsx
@@ -1,7 +1,13 @@
 import React, { useCallback, useMemo } from 'react'
 import { useSearchStoreResults } from '@store/search-results-store'
 import { FilterDropdown } from '@components/filter-dropdown'
-import { genreOptions, statusOptions, yearOptions, formatOptions } from 'types'
+import {
+  genreOptions,
+  statusOptions,
+  yearOptions,
+  formatOptions,
+  seasonOptions,
+} from 'types'
 import { studioOptions } from '@utils/create-studios-options'
 import type { AppliedFilters } from 'types'
 
@@ -39,41 +45,19 @@ export const FilterSection = () => {
     [setAppliedFilters]
   )
 
-  const handleInput = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setQuery(e.target.value)
-    },
-    [setQuery]
-  )
-
   const appliedFiltersEntries = useMemo(
     () => Object.entries(appliedFilters),
     [appliedFilters]
   )
 
   return (
-    <div className="md: relative mx-auto h-min w-full max-w-6xl space-y-4 p-4 md:h-52 xl:max-w-7xl">
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-6 xl:grid-cols-7">
-        <div className="relative">
-          <label className="mb-1 block text-sm font-medium text-gray-700">
-            Search
-          </label>
-          <input
-            type="search"
-            id="default-search"
-            className="flex max-h-[80px] w-full flex-wrap items-start gap-1 overflow-y-auto rounded border border-gray-200 bg-gray-50 px-3 py-2 text-sm text-gray-600 focus-within:border-gray-400 focus-within:ring-1 focus-within:ring-gray-400 focus:outline-none"
-            placeholder="Search Animes..."
-            value={query}
-            onChange={handleInput}
-          />
-        </div>
-
+    <div className="relative h-full w-full space-y-4 border-r border-gray-100/10 p-4 text-white">
+      <div className="grid grid-cols-1 gap-8">
         <FilterDropdown
           label="Genres"
           values={appliedFilters.genre_filter ?? []}
           onChange={(values) => updateFilter('genre_filter', values)}
           onClear={() => updateFilter('genre_filter', [])}
-          placeholder="Select Genres"
           options={genreOptions}
         />
         <FilterDropdown
@@ -81,7 +65,6 @@ export const FilterSection = () => {
           values={appliedFilters.year_filter ?? []}
           onChange={(values) => updateFilter('year_filter', values)}
           onClear={() => updateFilter('year_filter', [])}
-          placeholder="Any year"
           options={yearOptions}
         />
         <FilterDropdown
@@ -89,7 +72,6 @@ export const FilterSection = () => {
           values={appliedFilters.status_filter ?? []}
           onChange={(values) => updateFilter('status_filter', values)}
           onClear={() => updateFilter('status_filter', [])}
-          placeholder="Any Status"
           options={statusOptions}
         />
         <FilterDropdown
@@ -97,7 +79,6 @@ export const FilterSection = () => {
           values={appliedFilters.type_filter ?? []}
           onChange={(values) => updateFilter('type_filter', values)}
           onClear={() => updateFilter('type_filter', [])}
-          placeholder="Any Format"
           options={formatOptions}
         />
         <FilterDropdown
@@ -105,17 +86,20 @@ export const FilterSection = () => {
           values={appliedFilters.studio_filter ?? []}
           onChange={(values) => updateFilter('studio_filter', values)}
           onClear={() => updateFilter('studio_filter', [])}
-          placeholder="Any Studio"
           options={studioOptions}
         />
+        <FilterDropdown
+          label="Season"
+          values={appliedFilters.season_filter ?? []}
+          onChange={(values) => updateFilter('season_filter', values)}
+          onClear={() => updateFilter('season_filter', [])}
+          options={seasonOptions}
+        />
         <div>
-          <label className="mb-1 block text-sm font-medium text-gray-700">
-            Reset
-          </label>
           <button
             type="button"
             onClick={resetFilters}
-            className="inline-flex items-center rounded border border-gray-200 bg-gray-50 px-4 py-2 text-sm text-gray-600 hover:bg-gray-100"
+            className="bg-secondary border-secondary mx-auto flex w-full max-w-32 items-center justify-center rounded border px-4 py-2 text-sm transition-all hover:border-white hover:bg-base hover:opacity-90"
           >
             <svg
               className="mr-2 h-4 w-4"
@@ -130,47 +114,6 @@ export const FilterSection = () => {
           </button>
         </div>
       </div>
-
-      {
-        <div className="bottom-0 left-0 right-0 h-24 rounded-md bg-gray-50 p-4 md:absolute">
-          <h3 className="mb-2 text-sm font-semibold text-gray-700">
-            Applied Filters:
-          </h3>
-          <div className="flex flex-wrap gap-2">
-            {appliedFiltersEntries.map(([category, values]) =>
-              values?.map((value) => (
-                <span
-                  key={`${category}-${value}`}
-                  className="inline-flex items-center rounded-full bg-blue-100 px-2 py-1 text-xs font-medium text-blue-800"
-                >
-                  {value}
-                  <button
-                    type="button"
-                    onClick={() =>
-                      removeFilter(category as keyof AppliedFilters, value)
-                    }
-                    className="ml-1 inline-flex h-4 w-4 items-center justify-center text-blue-400 hover:text-blue-500"
-                  >
-                    <svg
-                      className="h-3 w-3"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M6 18L18 6M6 6l12 12"
-                      />
-                    </svg>
-                  </button>
-                </span>
-              ))
-            )}
-          </div>
-        </div>
-      }
     </div>
   )
 }
