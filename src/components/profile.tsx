385f9c9fdba265ce1c552660a70365aca51af1a8
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-07 15:44:18 -0500

    create prafile component

---
+++ src/components/profile.tsx
@@ -0,0 +1,17 @@
+export const Profile = () => {
+  return (
+    <div className="flex items-center justify-end gap-4">
+      <div className="text-end font-medium dark:text-white md:block hidden">
+        <div>Jese Leos</div>
+        <div className="text-end text-sm text-gray-500 dark:text-gray-400">
+          Joined in August 2014
+        </div>
+      </div>
+      <img
+        className="h-10 w-10 rounded-full"
+        src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+        alt=""
+      />
+    </div>
+  )
+}
