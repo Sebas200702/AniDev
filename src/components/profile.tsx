57938d314a8d31f80132adeea1f1c74a23272f3f
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-15 10:30:21 -0500

    format file

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,7 +1,7 @@
 export const Profile = () => {
   return (
     <div className="flex items-center justify-end gap-4">
-      <div className="text-end font-medium dark:text-white md:block hidden">
+      <div className="hidden text-end font-medium md:block dark:text-white">
         <div>Jese Leos</div>
         <div className="text-end text-sm text-gray-500 dark:text-gray-400">
           Joined in August 2014
