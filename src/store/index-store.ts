ec11c2631dbf1b0ca9f76992d54397f69494cd5a
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-18 23:55:37 -0500

    use index store to annime collection

--- src/store/index-store.ts
+++ src/store/index-store.ts
@@ -1,11 +1,15 @@
 import { create } from 'zustand'
-
+import type { Collection } from 'types'
 interface IndexStore {
   animeBanners: number[]
+  collections: Collection[]
   setAnimeBanners: (animeBanners: number[]) => void
+  setCollections: (collections: Collection[]) => void
 }
 
 export const useIndexStore = create<IndexStore>((set) => ({
   animeBanners: [],
+  collections: [],
   setAnimeBanners: (animeBanners) => set({ animeBanners }),
+  setCollections: (collections) => set({ collections }),
 }))
