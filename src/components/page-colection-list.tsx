06ab6b6be5135db5140f07e2c6ddcfeadec2910e
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-20 22:22:33 -0500

    creaate colection list component to page of collecton

--- src/components/page-colection-list.tsx
+++ src/components/page-colection-list.tsx
@@ -1,24 +1,59 @@
-import { useEffect } from "react"
-import { useIndexStore } from '@store/index-store'
+import { useEffect, useState } from 'react'
+import type { Anime } from 'types'
+import { CollectionItem } from '@components/collection-item'
 
 interface Props {
   title: string
+  id: string
 }
-export const PageColectionList = ({ title }: Props) => {
-    const { collections } = useIndexStore()
-    useEffect(() => {
+export const PageColectionList = ({ title, id }: Props) => {
+  const [url, setUrl] = useState('')
+  const [animes, setAnimes] = useState<Anime[]>()
+  useEffect(() => {
+    const collection = JSON.parse(
+      sessionStorage.getItem(`animeCollection_${id}`) ?? ''
+    )
+    if (!collection) return
 
-        const colection = collections.find((collection) => collection.title === title)
-        console.log(colection)
+    setUrl(`/api/animes?${collection.query}&banners_filter=false`)
 
+    const getAnimes = async () => {
+      if (!url) return
+      const data = await fetch(url).then((res) => res.json())
+      setAnimes(data.data)
+    }
+    getAnimes()
+  }, [setAnimes, setUrl, url, id])
 
-
-
-    }, [])
+  if (!animes)
+    return (
+      <div className="mx-auto flex flex-col gap-4">
+        <div className="mx-10 mb-6 h-10 w-96 animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out"></div>
+        <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 xl:grid-cols-3">
+          {Array(20)
+            .fill(0)
+            .map((_, i) => (
+              <div
+                key={i + 1}
+                className="mx-auto flex aspect-[500/260] h-full w-full max-w-[500px] animate-pulse flex-row rounded-lg bg-secondary"
+              >
+                <div className="aspect-[225/330] h-full animate-pulse rounded-l-lg bg-zinc-800 object-cover object-center transition-all ease-in-out"></div>
+              </div>
+            ))}
+        </div>
+      </div>
+    )
 
   return (
-    <div className="mx-auto flex max-w-7xl flex-col gap-4 p-4">
-      <h2 className="max-w-96 text-3xl font-bold text-white">{title}</h2>
+    <div className="mx-auto flex flex-col gap-4">
+      <header className="flex flex-row justify-between">
+        <h2 className="mb-6 px-10 text-3xl font-bold text-white">{title}</h2>
+      </header>
+      <ul className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 xl:grid-cols-3">
+        {animes.map((anime) => (
+          <CollectionItem key={anime.mal_id} anime={anime} />
+        ))}
+      </ul>
     </div>
   )
 }
