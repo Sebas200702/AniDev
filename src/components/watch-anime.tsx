631dba39b0246615133c45115de24bf19e75e323
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-20 22:52:10 -0500

    format files

--- src/components/watch-anime.tsx
+++ src/components/watch-anime.tsx
@@ -5,7 +5,7 @@ export const WatchAnimeButton = ({ url }: { url: string }) => {
       href={url}
       className="button-primary w-full items-center justify-center gap-2"
     >
-      <PlayIcon className='md:h-6 md:w-6 w-4 h-4' />
+      <PlayIcon className="h-4 w-4 md:h-6 md:w-6" />
       Watch Now
     </a>
   )
