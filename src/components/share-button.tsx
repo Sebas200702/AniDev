610fc9fd2c40ebadb20af2ba4c0b25181aba5bbf
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-21 10:47:03 -0500

    fix buttons

--- src/components/share-button.tsx
+++ src/components/share-button.tsx
@@ -32,7 +32,7 @@ export const ShareButton = ({ title, url, text }: Props) => {
 
   return (
     <button onClick={handleClick} className="button-secondary" title="Share">
-      <ShareIcon style="text-white md:w-6 md:h-6 w-4 h-4" />
+      <ShareIcon style="text-white md:w-5 md:h-5 w-4 h-4" />
     </button>
   )
 }
