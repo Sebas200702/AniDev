537f9fcf8b066ab929c7bdef030eb5d3fc38b95c
Sebastian Torregroza <143149476+Sebas200702@users.noreply.github.com>
2025-01-20 23:13:30 -0500

    Update page-colection-list.tsx

--- src/components/page-colection-list.tsx
+++ src/components/page-colection-list.tsx
@@ -47,7 +47,7 @@ export const PageColectionList = ({ title, id }: Props) => {
   return (
     <div className="mx-auto flex flex-col gap-4">
       <header className="flex flex-row justify-between">
-        <h2 className="mb-6 px-10 text-3xl font-bold text-white">{title}</h2>
+        <h2 className="mb-6 md:px-10 md:text-3xl text-xl font-bold text-white">{title}</h2>
       </header>
       <ul className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 xl:grid-cols-3">
         {animes.map((anime) => (
