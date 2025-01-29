9714d71179cb7d90f2704d4e4beab077a1ee9f7a
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-29 18:26:31 -0500

    caret icon

---
+++ src/components/icons/caret-icon.tsx
@@ -0,0 +1,17 @@
+export const CaretIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="m6 10 6 6 6-6H6" />
+    </svg>
+  )
+}
