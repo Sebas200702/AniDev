be6c84ede45d02386db90a6cc0df96345b3391a9
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-29 18:08:03 -0500

    add more options

---
+++ src/components/more-options-icon.tsx
@@ -0,0 +1,17 @@
+export const MoreOptionsIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M11 12a1 1 0 1 0 2 0 1 1 0 1 0-2 0M11 19a1 1 0 1 0 2 0 1 1 0 1 0-2 0M11 5a1 1 0 1 0 2 0 1 1 0 1 0-2 0" />
+    </svg>
+  )
+}
