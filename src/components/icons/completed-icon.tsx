0a91f66535385262338e9c6daf4f7c02b53d0d02
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-29 18:27:00 -0500

    completed icon

---
+++ src/components/icons/completed-icon.tsx
@@ -0,0 +1,17 @@
+export const CompletedIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="m5 12 5 5L20 7" />
+    </svg>
+  )
+}
