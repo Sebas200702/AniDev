c5fdfb0b31bb5689f70578ffe0c5d9592d5e2e43
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-29 18:34:12 -0500

    usert-list store

---
+++ src/store/user-list-store.ts
@@ -0,0 +1,39 @@
+import { create } from 'zustand'
+import type { Section } from 'types'
+import { CollectionIcon } from '@components/icons/collection-icon'
+import { CompletedIcon } from '@components/icons/completed-icon'
+import { ToWatchIcon } from '@components/icons/to-watch-icon'
+import { WatchingIcon } from '@components/icons/watch-icon'
+
+interface UserListsStore {
+  userList: Section[]
+  setUserList: (userList: Section[]) => void
+}
+
+export const useUserListsStore = create<UserListsStore>((set) => ({
+  userList: [
+    {
+      label: 'Collection',
+      icon: CollectionIcon,
+      selected: true,
+    },
+    {
+      label: 'Completed',
+      icon: CompletedIcon,
+      selected: false,
+    },
+    {
+      label: 'To Watch',
+      icon: ToWatchIcon,
+      selected: false,
+    },
+    {
+      label: 'Watching',
+      icon: WatchingIcon,
+      selected: false,
+    },
+  ],
+  setUserList: (userList) => {
+    set({ userList })
+  },
+}))
