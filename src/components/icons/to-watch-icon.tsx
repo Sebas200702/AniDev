dc6252a01a143e9476809d2f475633806713447e
Sebas200702 <sebastorregroza6@gmail.com>
2025-01-29 18:31:36 -0500

    to watch icon

---
+++ src/components/icons/to-watch-icon.tsx
@@ -0,0 +1,17 @@
+export const ToWatchIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      viewBox="0 0 24 24"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M18 7v14l-6-4-6 4V7a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4z" />
+    </svg>
+  )
+}
