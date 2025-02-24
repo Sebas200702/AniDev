d6c6d3e614406d9a5428dfbcb900fa39cfe20637
Sebas200702 <sebastorregroza6@gmail.com>
2025-02-23 19:43:03 -0500

    use custom text

--- src/components/anime-tag.tsx
+++ src/components/anime-tag.tsx
@@ -14,7 +14,7 @@ export const AnimeTag = ({ tag, type, style }: Props) => {
   return (
     <a
       href={`/search?${filter}=${type?.toLowerCase()}`}
-      className={`${style ?? 'w-min'} h-min rounded-sm px-2 py-1 text-xs font-medium transition-all duration-200 ease-in-out ${tagColor}`}
+      className={`${style ?? 'w-min'} text-sx h-min rounded-sm px-2 py-1 font-medium transition-all duration-200 ease-in-out ${tagColor}`}
       aria-label={`Tag: ${tag}`}
     >
       {tag}
