8e4053cb8e228a269bed530f4da234a53ecefa1f
Sebastian Torregroza <143149476+Sebas200702@users.noreply.github.com>
2025-02-25 06:51:17 -0500

    Update header.tsx

--- src/components/index/anime-top/header.tsx
+++ src/components/index/anime-top/header.tsx
@@ -1,7 +1,7 @@
 export const AnimeTopHeader = () => {
   return (
     <header className="relative mx-auto w-[100dvw] justify-center mb-4">
-      <h4 className="text-center text-4xl font-bold">Top Anime</h4>
+      <h4 className="text-center text-xl font-bold">Top Anime</h4>
     </header>
   )
 }
