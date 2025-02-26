e6d465faed4d3143cd0ddb9092579b09cb00a626
Sebas200702 <sebastorregroza6@gmail.com>
2025-02-26 16:29:23 -0500

    add cursor pointer

--- src/components/index/carousel/indicator.tsx
+++ src/components/index/carousel/indicator.tsx
@@ -13,7 +13,7 @@ export const Indicator = memo(
     return (
       <button
         onClick={() => onClick(index)}
-        className={`h-3 rounded-full transition-all duration-300 ease-in-out ${
+        className={`h-3 cursor-pointer rounded-full transition-all duration-300 ease-in-out ${
           currentIndex === index ? 'bg-enfasisColor w-8' : 'w-3 bg-white'
         }`}
         aria-current={currentIndex === index ? 'true' : 'false'}
