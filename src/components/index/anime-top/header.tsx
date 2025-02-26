c13b3b0c36643572cf4910844aa42545b26a5eba
Sebastian Torregroza <143149476+Sebas200702@users.noreply.github.com>
2025-02-26 16:22:40 +0000

    add lines to header

--- src/components/index/anime-top/header.tsx
+++ src/components/index/anime-top/header.tsx
@@ -1,7 +1,11 @@
 export const AnimeTopHeader = () => {
   return (
-    <header className="relative mx-auto w-[100dvw] justify-center mb-4">
-      <h4 className="text-center text-xl font-bold">Top Anime</h4>
+    <header className="relative mx-auto w-[100dvw] justify-center mb-4 flex items-center flex-row gap-4 px-4">
+      
+      <div className="mt-2 flex-1 border-t border-white/20"></div>
+      <h4 className="text-center text-2xl font-bold">Top Anime</h4>
+      
+      <div className="mt-2 flex-1 border-t border-white/20"></div>
     </header>
   )
 }
