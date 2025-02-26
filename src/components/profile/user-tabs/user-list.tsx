55d3f8775a6424e31578a412668d7af399c7e059
Sebas200702 <sebastorregroza6@gmail.com>
2025-02-26 16:32:12 -0500

    use section list to userlist

--- src/components/profile/user-tabs/user-list.tsx
+++ src/components/profile/user-tabs/user-list.tsx
@@ -1,14 +1,18 @@
+import { SectionList } from '@components/section-list'
 import { UserListOptions } from '@components/profile/user-tabs/user-list-options'
-import { UserListSection } from '@components/profile/user-tabs/user-list-section'
 import { useUserListsStore } from '@store/user-list-store'
-
 export const UserList = () => {
-  const { userList: sections } = useUserListsStore()
+  const { userList: sections, setUserList } = useUserListsStore()
+
   return (
     <nav className="mt-16 flex w-full flex-row items-center justify-between md:mt-0">
       <ul className="flex flex-row py-4 md:p-4">
         {sections.map((section) => (
-          <UserListSection key={section.label} section={section} />
+          <SectionList
+            key={section.label}
+            section={section}
+            sections={{ list: sections, set: setUserList }}
+          />
         ))}
       </ul>
       <UserListOptions />
