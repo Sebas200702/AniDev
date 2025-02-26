f0d6f0d2f71f09e29fff165e71bf626cfeda1a69
Sebas200702 <sebastorregroza6@gmail.com>
2025-02-26 16:26:27 -0500

    add fade out to anime top

--- src/components/index/anime-top/top-anime.tsx
+++ src/components/index/anime-top/top-anime.tsx
@@ -12,7 +12,7 @@ export const AnimeTop = () => {
   if (loading || !aninme) return <AnimeTopLoader />
 
   return (
-    <section className="relative mx-auto w-[100dvw] justify-center">
+    <section className="relative mx-auto w-[100dvw] justify-center fade-out">
       <AnimeTopHeader />
       <ul className="mx-auto grid grid-cols-2 justify-around gap-4 md:grid-cols-5 md:px-4 xl:grid-cols-10">
         {aninme.map((anime, index) => (
