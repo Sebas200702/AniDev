06aaa56c228eb34280faedc8a6de90f79cc000ec
Sebas200702 <sebastorregroza6@gmail.com>
2025-02-26 16:23:54 -0500

    ajust styles of watch anime button

--- src/components/buttons/watch-anime.tsx
+++ src/components/buttons/watch-anime.tsx
@@ -4,9 +4,9 @@ export const WatchAnimeButton = ({ url }: { url: string }) => {
   return (
     <a
       href={url}
-      className="button-primary text-s flex w-full items-center justify-center gap-4"
+      className="button-primary text-s flex w-full items-center justify-center gap-2"
     >
-      <PlayIcon className="h-4 w-4" />
+      <PlayIcon className="xl:h-4 h-3 w-3 xl:w-4" />
       Watch Now
     </a>
   )
