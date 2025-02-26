1b19fb904f57c0196957a2dc769ba2e638cac334
Sebastian Torregroza <143149476+Sebas200702@users.noreply.github.com>
2025-02-26 16:23:57 +0000

    componetize anime slider loader

--- src/components/index/slider/anime-slider-list.tsx
+++ src/components/index/slider/anime-slider-list.tsx
@@ -106,21 +106,21 @@ export const AnimeSlider = ({ query, title }: Props) => {
 
   const displayAnimes = cachedAnimes.length > 0 ? cachedAnimes : (animes ?? [])
 
-  if (loading || !cachedAnimes || !animes || !displayAnimes)
+  if (!loading || !cachedAnimes || !animes || !displayAnimes)
     return (
       <div className="relative mx-auto w-[100dvw]">
-        <div className="mt-1 flex flex-row items-center justify-center space-x-4 py-4 md:mt-0">
-          <span className="ml-4 inline-flex h-6 w-24 animate-pulse rounded-lg bg-zinc-800 md:ml-8 md:h-8 md:w-32"></span>
+        <div className="mt-1 flex flex-row items-center justify-center space-x-4 p-4 md:mt-0 mb-6">
+          <span className="ml-16 inline-flex h-6 w-24 animate-pulse rounded-lg bg-zinc-800  md:h-8 md:w-32"></span>
           <div className="flex-1 border-t border-white/20 md:mt-2"></div>
         </div>
         <div className="relative overflow-hidden">
-          <div className="anime-list flex w-full flex-row overflow-x-auto md:px-[calc(((100dvw)/4.4)*0.2)] xl:px-[calc(((100dvw)/6.4)*0.2)]">
+          <div className="anime-list flex w-full flex-row overflow-x-auto md:px-20 px-4 gap-10">
             {Array(24)
               .fill(0)
               .map((_, i) => (
                 <div
                   key={i + 1}
-                  className="flex h-auto w-full min-w-[calc((100dvw)/2.4)] flex-col items-center gap-1 p-4 duration-200 md:min-w-[calc((100dvw)/4.4)] xl:min-w-[calc((100dvw)/6.4)]"
+                  className="flex h-auto w-full min-w-[calc((100dvw-32px)/2.4)] flex-col items-center  duration-200 md:min-w-[calc((100dvw-280px)/4)] xl:min-w-[calc((100dvw-360px)/6)]"
                 >
                   <div className="aspect-[225/330] h-auto w-full animate-pulse rounded-lg bg-zinc-800 md:aspect-[225/330]"></div>
                 </div>
