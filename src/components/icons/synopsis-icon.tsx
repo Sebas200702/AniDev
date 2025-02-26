90d6630668f9cec3153bd0365adce99706eafcce
Sebas200702 <sebastorregroza6@gmail.com>
2025-02-26 16:25:36 -0500

    create synopsis icon

---
+++ src/components/icons/synopsis-icon.tsx
@@ -0,0 +1,18 @@
+
+export const SynopsisIcon = ({className}: {className?: string}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={className}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M4 6l16 0" />
+    <path d="M4 12l16 0" />
+    <path d="M4 18l12 0" />
+  </svg>
+)
