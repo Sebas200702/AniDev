f0c61487921b54889955a1a95d8f5483d3a6ed82
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-04 16:05:17 -0500

    fix image

--- src/components/anime-info/anime-aside.tsx
+++ src/components/anime-info/anime-aside.tsx
@@ -1,6 +1,8 @@
 import type { Anime } from 'types'
+import { Picture } from '@components/picture'
 import { ShareButton } from '@components/buttons/share-button'
 import { WatchAnimeButton } from '@components/buttons/watch-anime'
+import { createImageUrlProxy } from '@utils/craete-imageurl-proxy'
 
 interface Props {
   animeData: Anime
@@ -15,15 +17,28 @@ export const AnimeAside = ({
   url,
 }: Props) => {
   return (
-    <aside className="top-28 z-10 anime-aside row-span-2 p-4 md:p-0 flex h-min w-full flex-col gap-8 md:items-start xl:sticky">
-      <figure className="aspect-[225/330] w-auto rounded-lg">
+    <aside className="anime-aside top-28 z-10 row-span-2 flex h-min w-full flex-col gap-8 p-4 md:items-start md:p-0 xl:sticky">
+      <Picture
+        image={createImageUrlProxy(
+          animeData.image_large_webp,
+          '100',
+          '0',
+          'webp'
+        )}
+        styles="aspect-[225/330] w-full rounded-lg object-cover object-center transition-all ease-in-out"
+      >
         <img
-          src={animeData.image_large_webp}
-          alt={`Portada oficial de ${animeData.title}`}
-          className="aspect-[225/330] h-auto rounded-lg transition-all ease-in-out w-full"
+          className="aspect-[225/330] w-full rounded-lg object-cover object-center transition-all ease-in-out"
+          src={createImageUrlProxy(
+            animeData.image_large_webp,
+            '1920',
+            '50',
+            'webp'
+          )}
+          alt={animeData.title}
           loading="lazy"
         />
-      </figure>
+      </Picture>
       <div className="flex h-full w-full flex-row justify-end gap-2">
         <WatchAnimeButton url={watchNowUrl} />
         <ShareButton title={animeData.title} url={url} text={shareText} />
