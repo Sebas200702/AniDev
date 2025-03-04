8fbcf782b3fb199ab7b3cbe3d42b379b07a4cb05
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-03 23:08:44 -0500

    fix styles

--- src/components/index/anime-top/header.tsx
+++ src/components/index/anime-top/header.tsx
@@ -1,10 +1,10 @@
 export const AnimeTopHeader = () => {
   return (
-    <header className="relative mx-auto w-[100dvw] justify-center mb-4 flex items-center flex-row gap-4 px-4">
-      
+    <header className="relative mx-auto w-[100dvw] justify-center mb-10 flex items-center flex-row gap-4 px-4">
+
       <div className="mt-2 flex-1 border-t border-white/20"></div>
-      <h4 className="text-center text-2xl font-bold">Top Anime</h4>
-      
+      <h4 className="text-center text-lx font-bold">Top Anime</h4>
+
       <div className="mt-2 flex-1 border-t border-white/20"></div>
     </header>
   )
--- src/components/index/anime-top/top-anime.tsx
+++ src/components/index/anime-top/top-anime.tsx
@@ -12,7 +12,7 @@ export const AnimeTop = () => {
   if (loading || !aninme) return <AnimeTopLoader />
 
   return (
-    <section className="relative mx-auto w-[100dvw] justify-center fade-out">
+    <section className="relative  mx-auto w-[100dvw] justify-center fade-out gap-10">
       <AnimeTopHeader />
       <ul className="mx-auto grid grid-cols-2 justify-around gap-4 md:grid-cols-5 md:px-4 xl:grid-cols-10">
         {aninme.map((anime, index) => (
