11a71489daa4cea4dd961e06c8361efe69ce6724
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-04 15:54:19 -0500

    fix loader

--- src/components/anime-info/anime-loader.tsx
+++ src/components/anime-info/anime-loader.tsx
@@ -6,7 +6,7 @@ export const AnimeLoader = () => {
       <div className="relative aspect-[1080/600] h-[65dvh]  w-full animate-pulse bg-zinc-800 duration-300">
         <div className="to-Primary-950/100 absolute right-0 bottom-0 left-0 h-full w-full bg-gradient-to-b from-transparent transition-all duration-300 ease-in-out" />
       </div>
-      <div className="z-10 grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-4 md:gap-15 md:px-20 px-4 ">
+      <div className="md:-mt-54 -mt-96 z-10 grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-4 md:gap-15 md:px-20 px-4 ">
         <div className=" row-span-2 p-8  md:p-0">
           <div className="aspect-[225/330] h-auto w-full animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out"></div>
         </div>
