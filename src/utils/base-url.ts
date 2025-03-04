a1b642df3a08908413b3c941653080e063c3b592
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-03 23:18:43 -0500

    create base title to dinamic metadatas

--- src/utils/base-url.ts
+++ src/utils/base-url.ts
@@ -2,3 +2,6 @@ export const baseUrl =
   process.env.NODE_ENV === 'production'
     ? 'https://ani-dev.vercel.app'
     : 'http://localhost:4321'
+
+
+export const baseTitle = 'AniDev - Watch anime online free'
