df0acccfd6c503f89d4025a3045836daf93aa018
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-03 23:13:37 -0500

    use new icon

--- src/components/index/slider/next-prev-btn-slider-list.tsx
+++ src/components/index/slider/next-prev-btn-slider-list.tsx
@@ -1,3 +1,5 @@
+import { NextPrevIcon } from '@components/icons/next-prev-icon'
+
 interface NexPrevBtnSlideListProps {
   label: string
   styles: string
@@ -8,25 +10,13 @@ export const NexPrevBtnSlideList = ({
 }: NexPrevBtnSlideListProps) => {
   return (
     <nav
-      className={` ${label} ${styles} to-Primary-950/90 absolute top-0 bottom-0 z-20 hidden h-full w-20 items-center justify-end bg-gradient-to-r from-transparent md:flex`}
+      className={` ${label} ${styles} to-Primary-950/90 absolute top-0 bottom-0 z-20 hidden h-full w-20 items-center justify-start bg-gradient-to-l from-transparent md:flex`}
     >
       <button
-        className="group bg-enfasisColor z-10 my-auto h-16 w-10 cursor-pointer rounded-lg transition-all duration-300 ease-in-out focus:outline-none"
+        className="group bg-enfasisColor z-10 my-auto h-16 w-10 cursor-pointer items-center justify-center rounded-lg transition-all duration-300 ease-in-out focus:outline-none"
         aria-label="Siguiente"
       >
-        <svg
-          className="mx-auto my-auto h-6 w-6"
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          fill="none"
-          stroke="#fff"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-        >
-          <path d="M7 7l5 5-5 5M13 7l5 5-5 5"></path>
-        </svg>
+        <NextPrevIcon />
       </button>
     </nav>
   )
