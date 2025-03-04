12b3b34d4fc32f7b1a75d7e1717cf58204a1258e
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-03 23:06:50 -0500

    fix styles

--- src/components/collection/page-collection-item.tsx
+++ src/components/collection/page-collection-item.tsx
@@ -1,10 +1,11 @@
+import { AddToListButton } from '@components/buttons/add-to list-button'
 import type { Anime } from 'types'
 import { AnimeTag } from '@components/anime-tag'
+import { Picture } from '@components/picture'
 import { ShareButton } from '@components/buttons/share-button'
 import { StarIcon } from '@components/icons/star-icon'
 import { WatchAnimeButton } from '@components/buttons/watch-anime'
 import { normalizeString } from '@utils/normalize-string'
-import { reduceString } from '@utils/reduce-string'
 
 interface Props {
   anime: Anime
@@ -13,52 +14,46 @@ export const CollectionItem = ({ anime }: Props) => {
   return (
     <li
       key={anime.mal_id}
-      className="bg-Complementary mx-auto  aspect-[600/390] w-full  flex  rounded-lg overflow-hidden"
+      className="bg-Complementary mx-auto flex aspect-[700/400] w-full overflow-hidden rounded-lg"
     >
-      <picture
-        className="aspect-[225/330] h-full w-auto overflow-hidden rounded-l-lg"
-        title={anime.title}
-        style={{
-          backgroundImage: `url(${anime.image_small_webp})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-        }}
+      <Picture
+        image={anime.image_small_webp}
+        styles="aspect-[225/330] h-full w-auto overflow-hidden rounded-l-lg"
       >
         <a href={`/${normalizeString(anime.title)}_${anime.mal_id}`}>
           <img
             src={anime.image_large_webp}
             alt={anime.title}
-            className="w-full aspect-[225/330]  h-full rounded-lg object-cover object-center transition-all duration-300 ease-in-out md:hover:scale-110"
+            className="aspect-[225/330] h-full w-full rounded-lg object-cover object-center transition-all duration-300 ease-in-out md:hover:scale-110"
             loading="lazy"
           />
         </a>
-      </picture>
-
-      <div className="flex h-full w-[65%]  flex-col justify-between p-2 xl:p-4">
+      </Picture>
+      <div className="flex h-full w-[75%] flex-col justify-between p-4">
         <h3
           title={anime.title}
-          className="overflow-hidden w-full font-bold text-pretty text-white text-l"
+          className="text-l w-full xl:line-clamp-2 line-clamp-1 overflow-hidden font-bold text-pretty text-white"
         >
-          {reduceString(anime.title, 20)}
+          {anime.title}
         </h3>
 
-        <div className="mt-1 flex flex-row gap-2 text-xs text-gray-500 xl:mt-2 text-s">
+        <div className="text-s mt-1 flex flex-row gap-2 text-xs text-gray-500 xl:mt-2">
           {anime.genres.slice(0, 2).map((genre) => (
             <AnimeTag key={genre} tag={genre} type={genre} style="w-auto" />
           ))}
         </div>
-        <p className=" text-sx text-Primary-200 flex">
-          {reduceString(anime.synopsis, 50)}
+        <p className="text-sx line-clamp-2 text-Primary-200 ">
+          {anime.synopsis ?? 'No description available'}
         </p>
-        <span className="flex items-center gap-2 text-sx text-gray-400 ">
+        <span className="text-sx flex items-center gap-2 text-gray-400">
           <StarIcon style="text-enfasisColor h-5 w-5 " />
           {anime.score}/10
         </span>
-        <footer className=" flex flex-row h-min  gap-2 ">
+        <footer className="flex h-min flex-row gap-2">
           <WatchAnimeButton
             url={`/watch/${normalizeString(anime.title)}_${anime.mal_id}`}
           />
+          <AddToListButton />
           <ShareButton
             title={anime.title}
             url={`/${normalizeString(anime.title)}_${anime.mal_id}`}
--- src/components/collection/page-colletion-loader.tsx
+++ src/components/collection/page-colletion-loader.tsx
@@ -1,14 +1,14 @@
 export const PageCollectionLoader = () => {
   return (
     <div className="mx-auto flex flex-col gap-4">
-      <div className="mb-6 h-20 w-full animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out md:mx-10 md:h-10 md:w-96"></div>
+      <div className="mb-6 h-20 w-full animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out  md:h-10 md:w-96"></div>
       <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 xl:grid-cols-3">
         {Array(20)
           .fill(0)
           .map((_, i) => (
             <div
               key={i + 1}
-              className="bg-Primary-950 mx-auto flex aspect-[500/260] h-full w-full max-w-[500px] animate-pulse flex-row rounded-lg"
+              className="bg-Primary-950 mx-auto flex aspect-[700/400] h-full w-full  animate-pulse flex-row rounded-lg"
             >
               <div className="aspect-[225/330] h-full animate-pulse rounded-l-lg bg-zinc-800 object-cover object-center transition-all ease-in-out"></div>
             </div>
