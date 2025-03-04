270e019231c3a005692d51719068809ee12304a8
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-03 23:03:00 -0500

    fix responsive clases

--- src/components/section-list.tsx
+++ src/components/section-list.tsx
@@ -24,23 +24,23 @@ export const SectionList = ({ section, sections }: Props) => {
   return (
     <li key={section.label} title={section.label}>
       <button
-        className={`flex h-full max-h-8 items-center gap-1 border-b p-2 transition ease-in-out duration-300 hover:bg-zinc-800/50 md:max-h-12 md:gap-3 md:p-5 ${
+        className={`flex h-full max-h-8 items-center gap-1 border-b p-2 transition ease-in-out duration-100 cursor-pointer hover:bg-zinc-800/50 md:max-h-12 xl:gap-3 xl:p-5 ${
           isSelected
             ? 'border-enfasisColor text-enfasisColor'
             : 'border-gray-100/20 text-gray-400'
         }`}
         onClick={(e) => handleClick(e, section.label)}
       >
-        <div className="flex h-4 w-4 items-center justify-center md:h-6 md:w-6">
+        <div className="flex h-4 w-4 items-center justify-center xl:h-6 xl:w-6">
           {section.icon ? (
-            <section.icon className="h-full w-full transition-all duration-300 ease-in-out" />
+            <section.icon className="h-full w-full transition-all duration-100 ease-in-out" />
           ) : (
             <span className="text-red-500">⚠️</span>
           )}
         </div>
 
         <ul
-          className={`flex flex-row gap-3 text-xs transition-all duration-300 ease-in-out md:text-lg ${
+          className={`flex flex-row gap-3 text-m transition-all duration-300 ease-in-out xl:text-l ${
             isSelected ? 'ml-2 w-auto opacity-100' : 'ml-0 hidden opacity-0'
           }`}
         >
