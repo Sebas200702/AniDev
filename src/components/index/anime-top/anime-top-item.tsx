ca7a48bb1ae70e5548cecbb7bbab795e566b3f56
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-03 23:08:20 -0500

    use overlay an picture componets

--- src/components/index/anime-top/anime-top-item.tsx
+++ src/components/index/anime-top/anime-top-item.tsx
@@ -1,4 +1,6 @@
 import type { Anime } from 'types'
+import { Overlay } from '@components/overlay'
+import { Picture } from '@components/picture'
 
 interface AnimeTopItemProps {
   anime: Anime
@@ -16,14 +18,9 @@ export const AnimeTopItem = ({ anime, index }: AnimeTopItemProps) => {
         className={`flex h-auto flex-col items-center rounded-lg`}
         aria-label={`View details for ${anime.title}`}
       >
-        <picture
-          className="relative h-full w-full max-w-36 overflow-hidden rounded-lg"
-          style={{
-            backgroundImage: `url(${anime.image_small_webp})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-          }}
+        <Picture
+          image={anime.image_small_webp}
+          styles="aspect-[225/330]  w-full max-w-36 overflow-hidden rounded-lg relative"
         >
           <img
             src={anime.image_large_webp}
@@ -31,11 +28,11 @@ export const AnimeTopItem = ({ anime, index }: AnimeTopItemProps) => {
             className="aspect-[225/330] w-full rounded-lg object-cover object-center transition-all ease-in-out group-hover:scale-105"
             loading="lazy"
           />
-          <div className="absolute bottom-0 left-0 h-1/3 w-full rounded-lg bg-gradient-to-b from-transparent to-black/70 opacity-100 transition-all duration-400 ease-in-out md:group-hover:h-full md:group-hover:to-black/90" />
+          <Overlay className="to-Primary-950/80 h-1/3 w-full bg-gradient-to-b md:group-hover:h-full" />
           <span className="absolute bottom-0 left-2 text-5xl font-bold">
             {index + 1}
           </span>
-        </picture>
+        </Picture>
 
         <footer className="mt-4">
           <h5 className="max-w-32 truncate text-left text-sm">{anime.title}</h5>
