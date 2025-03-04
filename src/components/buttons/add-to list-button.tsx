8e24850755c6d415b86ceb6a755c0f9c6e07fd82
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-03 23:06:23 -0500

    create add to list button

---
+++ src/components/buttons/add-to list-button.tsx
@@ -0,0 +1,19 @@
+export const AddToListButton = () => {
+  return (
+    <button className="button-secondary">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="w-3 h-3 xl:w-4 xl:h-4"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" />
+        <path d="M19 8H5M5 12h9M11 16H5M15 16h6M18 13v6" />
+      </svg>
+    </button>
+  )
+}
