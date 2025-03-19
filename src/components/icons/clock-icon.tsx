5b97f26bfba3eed4158c66707f1f77ac6a6b9b93
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-19 17:03:17 -0500

    edit props of clock icons

--- src/components/icons/clock-icon.tsx
+++ src/components/icons/clock-icon.tsx
@@ -1,8 +1,8 @@
 interface Props {
-  class?: string
+  className?: string
 }
 
-export const ClockIcon = ({ class: className }: Props) => (
+export const ClockIcon = ({ className }: Props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
