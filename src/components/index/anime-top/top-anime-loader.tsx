60d524f63024df853247ab5e0d054fdc554d8401
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-19 17:04:45 -0500

    fix anime top loader styles

--- src/components/index/anime-top/top-anime-loader.tsx
+++ src/components/index/anime-top/top-anime-loader.tsx
@@ -1,11 +1,11 @@
 export const AnimeTopLoader = () => {
   return (
-    <div className="relative px-4 md:px-20">
-      <header className="mb-10 flex w-full flex-row items-center gap-4">
-        <div className="bg-enfasisColor h-10 w-2 rounded-lg"></div>
-        <div className="h-6 w-28 rounded-md bg-zinc-800 md:h-8 md:w-40"></div>
+    <div className="relative">
+      <header className=" flex w-full flex-row items-center space-x-4 py-4  px-4 md:px-20">
+        <span className="bg-enfasisColor h-8 w-2 rounded-lg xl:h-10"></span>
+        <span className="inline-flex h-7.5 w-32 animate-pulse rounded-lg bg-zinc-800 xl:h-10.5"></span>
       </header>
-      <div className="mx-auto grid grid-cols-1 items-center justify-around gap-4 xl:grid-cols-2 xl:gap-x-12">
+      <div className="mx-auto grid grid-cols-1 items-center justify-around gap-4 px-4 py-4 md:px-20 xl:grid-cols-2 xl:gap-x-12">
         {Array(10)
           .fill(0)
           .map((_, i) => (
