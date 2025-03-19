640e9a5597a54c9e8ce6b15407933d77ff72ddc3
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-18 22:37:44 -0500

    update top anime loader

--- src/components/index/anime-top/top-anime-loader.tsx
+++ src/components/index/anime-top/top-anime-loader.tsx
@@ -1,12 +1,11 @@
 export const AnimeTopLoader = () => {
   return (
-    <div className="relative">
-      <header className="mb-10 flex w-full flex-row items-center justify-around gap-4">
-        <div className="mt-2 flex-1 border-t border-white/20"></div>
+    <div className="relative px-4 md:px-20">
+      <header className="flex w-full flex-row gap-4 items-center mb-10">
+        <div className="bg-enfasisColor h-10 w-2 rounded-lg"></div>
         <div className="h-6 w-28 rounded-md bg-zinc-800 md:h-8 md:w-40"></div>
-        <div className="mt-2 flex-1 border-t border-white/20"></div>
       </header>
-      <div className="justify-aroundmx-auto grid grid-cols-2 items-center gap-4 px-4 md:px-20 xl:gap-x-12">
+      <div className="mx-auto grid xl:grid-cols-2 grid-cols-1 items-center justify-around gap-4 xl:gap-x-12">
         {Array(10)
           .fill(0)
           .map((_, i) => (
@@ -15,11 +14,10 @@ export const AnimeTopLoader = () => {
               className="flex w-full flex-row items-center gap-2"
             >
               <div className="h-full w-full max-w-18"></div>
-              <div className="w-full bg-Complementary animate-pulse duration-300 ">
-              <div className="aspect-[225/330] h-auto w-full max-w-32 animate-pulse rounded-lg bg-zinc-800 "></div>
+              <div className="bg-Complementary w-full animate-pulse rounded-lg duration-300">
+                <div className="aspect-[225/330] h-auto w-full max-w-20 md:max-w-32 animate-pulse rounded-lg bg-zinc-800"></div>
               </div>
-              </div>
-
+            </div>
           ))}
       </div>
     </div>
