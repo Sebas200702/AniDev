79f413901df5c457e05bafa6e8f6697aea4db916
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-18 23:03:50 -0500

    delete blank space

--- src/components/index/slider/slider-header.tsx
+++ src/components/index/slider/slider-header.tsx
@@ -6,7 +6,6 @@ export const SliderHeader = ({ title }: SliderHeaderProps) => {
     <header className="flex items-center space-x-4 px-4 py-4 md:px-20 ">
         <span className="xl:h-10 h-8 w-2 rounded-lg bg-enfasisColor"></span>
       <h3 className="text-lx font-bold md:text-3xl ">{title}</h3>
-
     </header>
   )
 }
