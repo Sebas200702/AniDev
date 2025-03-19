5d91c089888b7b3e27aa222fdf7bd1a28f7d0904
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-19 17:02:59 -0500

    edit props of caledar icon

--- src/components/icons/calendar-icon.tsx
+++ src/components/icons/calendar-icon.tsx
@@ -1,8 +1,8 @@
 interface Props {
-  class?: string
+  className?: string
 }
 
-export const CalendarIcon = ({ class: className }: Props) => {
+export const CalendarIcon = ({ className }: Props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
