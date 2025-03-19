c48b1f501f51750296dd9058892cdf9af9f44a60
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-19 17:14:11 -0500

    chage font

--- src/components/index/slider/slider-header.tsx
+++ src/components/index/slider/slider-header.tsx
@@ -5,7 +5,7 @@ export const SliderHeader = ({ title }: SliderHeaderProps) => {
   return (
     <header className="flex items-center space-x-4 px-4 py-4 md:px-20">
       <span className="bg-enfasisColor h-8 w-2 rounded-lg xl:h-10"></span>
-      <h3 className="text-lx font-bold md:text-3xl">{title}</h3>
+      <h3 className="text-lx font-bold ">{title}</h3>
     </header>
   )
 }
