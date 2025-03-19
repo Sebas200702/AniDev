ad8508f582c33ae9d282ec7033898b0444b7ffd5
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-19 17:14:46 -0500

    fix skeleton

--- src/components/search/results/serch-results-loader.tsx
+++ src/components/search/results/serch-results-loader.tsx
@@ -1,14 +1,13 @@
 export const SearchResultsLoader = () => {
   return (
-    <div className="mx-auto grid h-full w-full max-w-7xl grid-cols-2 xl:gap-2 gap-6 md:grid-cols-4 xl:grid-cols-6">
+    <div className="mx-auto grid h-full w-full max-w-7xl grid-cols-2 gap-6 md:grid-cols-4 xl:grid-cols-6 xl:gap-2">
       {Array(30)
         .fill(0)
         .map((_, i) => (
           <div
             key={i + 1}
-            className="flex h-auto w-full animate-pulse flex-col rounded-lg p-4 bg-zinc-700 duration-200 h-full aspect-[225/330] "
-          >
-            
+            className="flex aspect-[225/330] h-full w-full animate-pulse flex-col rounded-lg bg-zinc-700 p-4 duration-200"
+          ></div>
         ))}
     </div>
   )
