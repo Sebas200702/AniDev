fc5f925a2fc9a42c3bd50ce3e58b170544b04d41
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-19 17:05:30 -0500

    ad py on top anime

--- src/components/index/anime-top/top-anime.tsx
+++ src/components/index/anime-top/top-anime.tsx
@@ -14,7 +14,7 @@ export const AnimeTop = () => {
   return (
     <section className="fade-out relative mx-auto w-[100dvw] justify-center">
       <AnimeTopHeader />
-      <ul className="mx-auto grid grid-cols-1 justify-around gap-y-4 px-4 md:px-20 xl:grid-cols-2 xl:gap-x-12">
+      <ul className="mx-auto grid grid-cols-1 justify-around gap-y-4 px-4 py-4 md:px-20 xl:grid-cols-2 xl:gap-x-12">
         {aninme.map((anime, index) => (
           <AnimeTopItem key={anime.mal_id} anime={anime} index={index} />
         ))}
