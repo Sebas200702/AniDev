b64bbfa5089f1ec77d8489809bd8af59f60138d0
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-19 17:04:27 -0500

    fix styles of anime top header

--- src/components/index/anime-top/header.tsx
+++ src/components/index/anime-top/header.tsx
@@ -1,7 +1,7 @@
 export const AnimeTopHeader = () => {
   return (
-    <header className="relative mx-auto mb-10 flex w-[100dvw] flex-row items-center gap-4 px-4 md:px-20">
-      <div className="bg-enfasisColor h-10 w-2 rounded-lg"></div>
+    <header className="relative mx-auto  flex w-[100dvw] flex-row items-center gap-4 py-4 px-4 md:px-20">
+      <span className="bg-enfasisColor h-8 w-2 rounded-lg xl:h-10"></span>
       <h4 className="text-lx text-center font-bold">Top Anime</h4>
     </header>
   )
