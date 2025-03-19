d308c6b85916678a3c30c1e61a460f4729b4d000
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-18 23:05:55 -0500

    fix bug on not yet aired anime status

--- src/utils/status-colors.ts
+++ src/utils/status-colors.ts
@@ -2,7 +2,7 @@ export const statusColors = (status: string) => {
   enum COLORS {
     'Currently Airing' = 'text-green-400 md:group-hover:text-green-500',
     'Finished Airing' = 'text-blue-400 md:group-hover:text-blue-500',
-    'Not Yet' = 'text-yellow-400 md:group-hover:text-yellow-500',
+    'Not yet aired' = 'text-yellow-400 md:group-hover:text-yellow-500',
     'unknown' = 'text-gray-400 md:group-hover:text-gray-500',
   }
 
