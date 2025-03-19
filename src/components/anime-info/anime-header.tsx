820d4d8c38acc8b71e896582ebfe1fd2d146673d
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-19 17:01:55 -0500

    Delete anime numbers

--- src/components/anime-info/anime-header.tsx
+++ src/components/anime-info/anime-header.tsx
@@ -1,6 +1,5 @@
 import type { Anime } from 'types'
 import { AnimeNavBar } from '@components/anime-info/anime-nav-bar'
-import { AnimeNumbers } from '@components/anime-info/anime-numbers'
 import { AnimeTag } from '@components/anime-tag'
 import { getAnimeType } from '@utils/getanime-type'
 import { normalizeRating } from '@utils/normalize-rating'
@@ -12,11 +11,11 @@ interface Props {
 export const AnimeHeader = ({ animeData }: Props) => {
   return (
     <header className="anime-header flex w-auto flex-col justify-end gap-4 md:col-span-2 xl:col-span-4 xl:mt-0">
-      <h2 className="title max-w-[30ch] text-pretty md:text-wrap">
+      <h2 className="title max-w-[30ch] text-pretty text-center md:text-left md:text-wrap">
         {animeData.title}
       </h2>
       <ul
-        className="flex flex-row flex-wrap gap-3"
+        className="flex flex-row items-center justify-center md:justify-start flex-wrap gap-3"
         aria-label="Categorías y géneros"
       >
         <AnimeTag tag={getAnimeType(animeData.type)} type={animeData.type} />
@@ -33,11 +32,7 @@ export const AnimeHeader = ({ animeData }: Props) => {
 
       <div className="anime-header grid grid-cols-1 items-center justify-between gap-4 md:grid-cols-2 xl:grid-cols-3">
         <AnimeNavBar />
-        <AnimeNumbers
-          score={animeData.score}
-          year={animeData.year}
-          duration={animeData.duration}
-        />
+
       </div>
     </header>
   )
