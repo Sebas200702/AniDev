bf5012d7b5f5a3d171299f676f0ebd60685ce50a
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-19 16:40:33 -0500

    fix styles

--- src/components/section-list.tsx
+++ src/components/section-list.tsx
@@ -24,10 +24,10 @@ export const SectionList = ({ section, sections }: Props) => {
   return (
     <li key={section.label} title={section.label}>
       <button
-        className={`group relative flex h-full max-h-8 cursor-pointer items-center justify-center overflow-hidden p-2 transition-colors duration-200 ease-in-out hover:bg-zinc-800/50 md:max-h-12 xl:p-5 ${isSelected ? 'text-enfasisColor' : 'text-gray-400'} after:bg-enfasisColor after:absolute after:bottom-0 after:left-0 after:h-[2px] after:transition-all after:duration-300 after:ease-in-out ${isSelected ? 'after:w-full' : 'after:w-0'}`}
+        className={`group relative flex h-full  cursor-pointer items-center justify-center overflow-hidden  transition-colors duration-200 ease-in-out hover:bg-zinc-800/50 max-h-12 p-5 ${isSelected ? 'text-enfasisColor' : 'text-gray-400'} after:bg-enfasisColor after:absolute after:bottom-0 after:left-0 after:h-[2px] after:transition-all after:duration-300 after:ease-in-out ${isSelected ? 'after:w-full' : 'after:w-0'}`}
         onClick={(e) => handleClick(e, section.label)}
       >
-        <div className="flex h-4 w-4 items-center justify-center xl:h-6 xl:w-6">
+        <div className="flex  items-center justify-center h-6 w-6">
           {section.icon ? (
             <section.icon
               className={`h-full w-full transition-transform duration-300 ${
