238d361e5faa5112ac5b2c444a528c039b4bcc5d
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-20 23:10:32 -0500

    fix

--- global.d.ts
+++
@@ -1,8 +0,0 @@
-import type { SignInOptions } from 'next-auth/react'
-
-declare module '@auth-astro/client' {
-  export interface AstroSignInOptions extends SignInOptions {
-    callbackUrl?: string
-    prefix?: string
-  }
-}
