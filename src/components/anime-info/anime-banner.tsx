ce285700aa862cc67d0014b4ebb854bc3e181dcb
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-25 21:13:04 -0500

    change to webp to avif

--- src/components/anime-info/anime-banner.tsx
+++ src/components/anime-info/anime-banner.tsx
@@ -55,20 +55,20 @@ export const AnimeBanner = ({
       <Picture
         image={
           banner_image
-            ? createImageUrlProxy(banner_image, '100', '0', 'webp')
-            : createImageUrlProxy(image_large_webp, '100', '0', 'webp')
+            ? createImageUrlProxy(banner_image, '100', '0', 'avif')
+            : createImageUrlProxy(image_large_webp, '100', '0', 'avif')
         }
         styles=" w-full object-cover object-center"
       >
         <img
           src={
             banner_image
-              ? createImageUrlProxy(banner_image, '1920', '50', 'webp')
-              : createImageUrlProxy(image_large_webp, '1920', '50', 'webp')
+              ? createImageUrlProxy(banner_image, '1920', '50', 'avif')
+              : createImageUrlProxy(image_large_webp, '1920', '50', 'avif')
           }
           alt={`${title} banner`}
           loading="lazy"
-          className="h-full w-full object-cover object-center"
+          className="relative h-full w-full object-cover object-center"
           width={225}
           height={330}
         />
