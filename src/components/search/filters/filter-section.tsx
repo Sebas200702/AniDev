1f102498f7307c8728e168a6bbda1058f40a25d2
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-27 20:30:03 -0500

    reorganize filters

--- src/components/search/filters/filter-section.tsx
+++ src/components/search/filters/filter-section.tsx
@@ -54,81 +54,79 @@ export const FilterSection = () => {
   )
 
   return (
-    <section className="relative h-full w-full space-y-4 border-r border-gray-100/10 p-4 text-white md:px-8">
-      <ul className="grid grid-cols-2 gap-10 md:grid-cols-1">
-        <FilterDropdown
-          label="Genres"
-          values={appliedFilters.genre_filter ?? []}
-          onChange={(values) => updateFilter('genre_filter', values)}
-          onClear={() => updateFilter('genre_filter', [])}
-          options={genreOptions}
-        />
-        <FilterDropdown
-          label="Year"
-          values={appliedFilters.year_filter ?? []}
-          onChange={(values) => updateFilter('year_filter', values)}
-          onClear={() => updateFilter('year_filter', [])}
-          options={yearOptions}
-        />
-        <FilterDropdown
-          label="Status"
-          values={appliedFilters.status_filter ?? []}
-          onChange={(values) => updateFilter('status_filter', values)}
-          onClear={() => updateFilter('status_filter', [])}
-          options={statusOptions}
-        />
-        <FilterDropdown
-          label="Format"
-          values={appliedFilters.type_filter ?? []}
-          onChange={(values) => updateFilter('type_filter', values)}
-          onClear={() => updateFilter('type_filter', [])}
-          options={formatOptions}
-        />
-        <FilterDropdown
-          label="Studio"
-          values={appliedFilters.studio_filter ?? []}
-          onChange={(values) => updateFilter('studio_filter', values)}
-          onClear={() => updateFilter('studio_filter', [])}
-          options={studioOptions}
-        />
-        <FilterDropdown
-          label="Season"
-          values={appliedFilters.season_filter ?? []}
-          onChange={(values) => updateFilter('season_filter', values)}
-          onClear={() => updateFilter('season_filter', [])}
-          options={seasonOptions}
-        />
-        <FilterDropdown
-          label="Rating"
-          values={appliedFilters.rating_filter ?? []}
-          onChange={(values) => updateFilter('rating_filter', values)}
-          onClear={() => updateFilter('rating_filter', [])}
-          options={ratingOptions}
-        />
-        <FilterDropdown
-          label="Order By"
-          values={appliedFilters.order_by ?? []}
-          onChange={(values) => updateFilter('order_by', values)}
-          onClear={() => updateFilter('order_by', [])}
-          options={orderByOptions}
-        />
-        <button
-          type="button"
-          onClick={resetFilters}
-          className="button-primary text-s flex w-full items-center justify-center gap-2"
+    <ul className="no-scrollbar md:sticky top-16 mt-16 grid md:h-full w-full md:w-auto md:max-h-[calc(100vh-192px)] md:border-r border-zinc-300/10 grid-cols-2 gap-10 md:overflow-y-auto md:grid-cols-1 p-4">
+      <FilterDropdown
+        label="Genres"
+        values={appliedFilters.genre_filter ?? []}
+        onChange={(values) => updateFilter('genre_filter', values)}
+        onClear={() => updateFilter('genre_filter', [])}
+        options={genreOptions}
+      />
+      <FilterDropdown
+        label="Year"
+        values={appliedFilters.year_filter ?? []}
+        onChange={(values) => updateFilter('year_filter', values)}
+        onClear={() => updateFilter('year_filter', [])}
+        options={yearOptions}
+      />
+      <FilterDropdown
+        label="Status"
+        values={appliedFilters.status_filter ?? []}
+        onChange={(values) => updateFilter('status_filter', values)}
+        onClear={() => updateFilter('status_filter', [])}
+        options={statusOptions}
+      />
+      <FilterDropdown
+        label="Format"
+        values={appliedFilters.type_filter ?? []}
+        onChange={(values) => updateFilter('type_filter', values)}
+        onClear={() => updateFilter('type_filter', [])}
+        options={formatOptions}
+      />
+      <FilterDropdown
+        label="Studio"
+        values={appliedFilters.studio_filter ?? []}
+        onChange={(values) => updateFilter('studio_filter', values)}
+        onClear={() => updateFilter('studio_filter', [])}
+        options={studioOptions}
+      />
+      <FilterDropdown
+        label="Season"
+        values={appliedFilters.season_filter ?? []}
+        onChange={(values) => updateFilter('season_filter', values)}
+        onClear={() => updateFilter('season_filter', [])}
+        options={seasonOptions}
+      />
+      <FilterDropdown
+        label="Rating"
+        values={appliedFilters.rating_filter ?? []}
+        onChange={(values) => updateFilter('rating_filter', values)}
+        onClear={() => updateFilter('rating_filter', [])}
+        options={ratingOptions}
+      />
+      <FilterDropdown
+        label="Order By"
+        values={appliedFilters.order_by ?? []}
+        onChange={(values) => updateFilter('order_by', values)}
+        onClear={() => updateFilter('order_by', [])}
+        options={orderByOptions}
+      />
+      <button
+        type="button"
+        onClick={resetFilters}
+        className="button-primary text-s flex w-full items-center justify-center gap-2"
+      >
+        <svg
+          className="mr-2 h-4 w-4"
+          fill="none"
+          strokeWidth="2"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
         >
-          <svg
-            className="mr-2 h-4 w-4"
-            fill="none"
-            strokeWidth="2"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-          </svg>
-          Reset
-        </button>
-      </ul>
-    </section>
+          <path d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+        </svg>
+        Reset
+      </button>
+    </ul>
   )
 }
