a856d2ac1703ff138dd62aa65825e219f9a43c38
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-27 21:15:36 -0500

    add utility get filters of search params

---
+++ src/utils/get-filters-of-search-params.ts
@@ -0,0 +1,28 @@
+import { Filters } from 'types'
+import { generateQueryPattern } from './generate-query-pattern'
+
+export const getFilters = (filtersEnum: typeof Filters, url: URL) => {
+  return Object.values(filtersEnum).reduce(
+    (filters, filter) => {
+      const value = url.searchParams.get(filter)
+      if (
+        filter === Filters.parental_control ||
+        filter === Filters.banners_filter
+      ) {
+        filters[filter] = value !== 'false'
+      } else if (
+        filter === Filters.page_number ||
+        filter === Filters.limit_count
+      ) {
+        filters[filter] = value ?? null
+      } else if (filter === Filters.search_query) {
+        filters[filter] = generateQueryPattern(value ?? '')
+      } else {
+        filters[filter] = value ? value.split('_') : null
+      }
+
+      return filters
+    },
+    {} as Record<string, string | number | boolean | string[] | null>
+  )
+}
