4cd79e68f75e7441369aef8cc44bc8be6ab07abc
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-29 12:03:03 -0500

    feat: integrate Overlay component into LoadingCarousel for enhanced visual effect

--- src/components/index/carousel/carousel-loader.tsx
+++ src/components/index/carousel/carousel-loader.tsx
@@ -1,3 +1,5 @@
+import { Overlay } from '@components/overlay'
+
 /**
  * LoadingCarousel component displays a loading animation for the carousel.
  *
@@ -28,7 +30,7 @@ export const LoadingCarousel = () => (
       <div className="z-10 mx-auto -mt-14 flex h-full w-full max-w-2xl flex-col items-center justify-center gap-4 p-6 text-white md:mr-16 md:ml-8 md:h-auto md:items-start md:justify-normal">
         <div className="z-30 h-20 w-full animate-pulse rounded-lg bg-zinc-800 md:mt-4 md:mb-4"></div>
         <div className="z-30 hidden h-12 w-full animate-pulse rounded-lg bg-zinc-800 md:flex"></div>
-
+        <Overlay className="to-Primary-950/100 h-full w-full bg-gradient-to-b" />
         <div className="flex w-full flex-row items-center gap-4 md:mt-2">
           <div className="h-10 w-32 animate-pulse rounded-lg bg-zinc-800 md:flex"></div>
           <div className="h-10 w-32 animate-pulse rounded-lg bg-zinc-800"></div>
