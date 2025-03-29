af3e0d2b814641162ee7b5fe8ab78c44509041ee
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-29 07:49:58 -0500

    reorder imports in auth.config.js for better organization

--- auth.config.js
+++ auth.config.js
@@ -1,6 +1,6 @@
 import Google from '@auth/core/providers/google'
-import { defineConfig } from 'auth-astro'
 import { supabase } from '@libs/supabase'
+import { defineConfig } from 'auth-astro'
 
 export default defineConfig({
   providers: [
