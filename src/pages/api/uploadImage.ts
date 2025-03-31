5cce71a4ccaa90778acb04bdbcb9a44cd7114c45
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-30 22:53:47 -0500

    Add uploadImage API endpoint for image processing and storage

---
+++ src/pages/api/uploadImage.ts
@@ -0,0 +1,44 @@
+import { pinata } from '@libs/pinata'
+
+import type { APIRoute } from 'astro'
+import sharp from 'sharp'
+
+export const POST: APIRoute = async ({ request }) => {
+  const { image, filename } = await request.json()
+
+  if (!image) {
+    return new Response(JSON.stringify({ message: 'Missing image data' }), {
+      status: 400,
+    })
+  }
+
+  const base64Pattern = /^data:([A-Za-z-+/]+);base64,(.+)$/
+  let base64String = image
+  const matches = image.match(base64Pattern)
+  if (matches) {
+    base64String = matches[2]
+  }
+
+  const buffer = Buffer.from(base64String, 'base64')
+
+  const optimizedBuffer = await sharp(buffer)
+    .resize({ width: 150 })
+    .webp({ quality: 75 })
+    .toBuffer()
+
+  if (!optimizedBuffer) {
+    return new Response(
+      JSON.stringify({ error: 'Error processing image (empty buffer)' }),
+      { status: 500, headers: { 'Content-Type': 'application/json' } }
+    )
+  }
+
+  const newFile = new File([optimizedBuffer], filename || 'image.webp', {
+    type: 'image/webp',
+  })
+
+  const { cid } = await pinata.upload.public.file(newFile)
+  const url = await pinata.gateways.public.convert(cid)
+
+  return new Response(JSON.stringify({ data: url }), { status: 200 })
+}
