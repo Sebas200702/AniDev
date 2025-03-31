b0b500bb50dd4d2ff7e6cd405b54b0027c808db1
Sebas200702 <sebastorregroza6@gmail.com>
2025-03-31 18:16:25 -0500

    Add drag-and-drop functionality for image upload in UserInfo component

--- src/components/profile/user-info.tsx
+++ src/components/profile/user-info.tsx
@@ -1,14 +1,33 @@
 import { Picture } from '@components/picture'
 import { useGlobalUserPreferences } from '@store/global-user'
+import { useUploadImageStore } from '@store/upload-image'
 import { createImageUrlProxy } from '@utils/craete-imageurl-proxy'
 import { ImageEditor } from './image-editor'
 import { InputUserImage } from './input-user-image'
 
 export const UserInfo = () => {
   const { userInfo } = useGlobalUserPreferences()
+  const { setImage } = useUploadImageStore()
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    const droppedFile = e.dataTransfer.files[0]
+    if (!droppedFile) return
+    const reader = new FileReader()
+    reader.onload = () => {
+      setImage(reader.result as string)
+    }
+    reader.readAsDataURL(droppedFile)
+    const $imageEditor = document.querySelector('.image-editor')
+
+    $imageEditor?.classList.replace('hidden', 'flex')
+  }
   return (
     <article className="z-10 mt-24 flex w-full flex-row items-center gap-6 text-white md:gap-8 ">
-      <div className="relative flex h-26 w-26 items-center justify-center rounded-full md:h-40 md:w-40 group ">
+      <div
+        className="relative flex h-26 w-26 items-center justify-center rounded-full md:h-40 md:w-40 group "
+        onDrop={handleDrop}
+        onDragOver={(event) => event.preventDefault()}
+      >
         <Picture
           image={createImageUrlProxy(userInfo?.avatar ?? '')}
           styles="h-26 w-26 rounded-full md:h-36 md:w-36 relative"
