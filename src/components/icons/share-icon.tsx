1a7a66ea6c1958beffe806debd9af67299b180d4
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-04 23:48:23 -0500

    create share icon

---
+++ src/components/icons/share-icon.tsx
@@ -0,0 +1,18 @@
+import type { IconProps } from "types"
+export const  ShareIcon = ({ className }: IconProps): JSX.Element=> {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        viewBox="0 0 24 24"
+        className={className}
+      >
+        <path d="M0 0h24v24H0z" stroke="none" />
+        <path d="M13 4v4C6.425 9.028 3.98 14.788 3 20c-.037.206 5.384-5.962 10-6v4l8-7-8-7z" />
+      </svg>
+    )
+  }
