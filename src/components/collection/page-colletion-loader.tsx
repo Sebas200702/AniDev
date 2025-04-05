b0148f51a6d7057d1f03b523a8db252022a3e6ec
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-04 23:46:31 -0500

    ajust collection loader

--- src/components/collection/page-colletion-loader.tsx
+++ src/components/collection/page-colletion-loader.tsx
@@ -26,13 +26,13 @@ export const PageCollectionLoader = () => {
   return (
     <div className="mx-auto flex flex-col gap-4">
       <div className="mb-6 h-20 w-full animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out md:h-10 md:w-96"></div>
-      <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 xl:grid-cols-3">
+      <div className="grid grid-cols-1 gap-6 md:gap-10 md:grid-cols-2 xl:grid-cols-3">
         {Array(20)
           .fill(0)
           .map((_, i) => (
             <div
               key={i + 1}
-              className="bg-Primary-950 mx-auto flex aspect-[700/400] h-full w-full animate-pulse flex-row rounded-lg"
+              className="bg-Complementary mx-auto flex aspect-[100/28] h-full w-full animate-pulse flex-row rounded-lg"
             >
               <div className="aspect-[225/330] h-full animate-pulse rounded-l-lg bg-zinc-800 object-cover object-center transition-all ease-in-out"></div>
             </div>
