2fd2b49e00157f1c2d20dc66a332bb1fe34f2946
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-04 23:46:07 -0500

    fix styling chage spacing of elements of colection list

--- src/components/collection/page-collection-list.tsx
+++ src/components/collection/page-collection-list.tsx
@@ -64,17 +64,17 @@ export const PageColectionList = ({ title, id }: Props) => {
   if (!animes) return <PageCollectionLoader />
 
   return (
-    <div className="mx-auto flex flex-col gap-4">
+    <section className="mx-auto flex flex-col gap-4 md:mb-20 mb-10">
       <header className="flex flex-row justify-between">
         <h2 className="mb-6 h-20 overflow-hidden text-xl font-bold text-white md:h-auto md:text-3xl">
           {title}
         </h2>
       </header>
-      <ul className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 xl:grid-cols-3">
+      <ul className="grid grid-cols-1 md:gap-10 gap-6 md:grid-cols-2 xl:grid-cols-3">
         {animes.map((anime) => (
           <CollectionItem key={anime.mal_id} anime={anime} />
         ))}
       </ul>
-    </div>
+    </section>
   )
 }
