979db1a3dd41a1fa42e25a71d89405a9a3084585
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-04 23:44:57 -0500

    extrac share icon and add styles

--- src/components/buttons/share-button.tsx
+++ src/components/buttons/share-button.tsx
@@ -1,3 +1,4 @@
+import { ShareIcon } from '@components/icons/share-icon'
 interface Props {
   /**
    * The title of the anime to be shared.
@@ -11,24 +12,7 @@ interface Props {
    * The text to be shared along with the anime.
    */
   text: string
-}
-
-const ShareIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      viewBox="0 0 24 24"
-      className="h-3 w-3 xl:h-4 xl:w-4"
-    >
-      <path d="M0 0h24v24H0z" stroke="none" />
-      <path d="M13 4v4C6.425 9.028 3.98 14.788 3 20c-.037.206 5.384-5.962 10-6v4l8-7-8-7z" />
-    </svg>
-  )
+  className: string
 }
 
 /**
@@ -61,7 +45,7 @@ const ShareIcon = () => {
  *   text="Check out this amazing anime!"
  * />
  */
-export const ShareButton = ({ title, url, text }: Props) => {
+export const ShareButton = ({ title, url, text, className }: Props) => {
   const handleClick = () => {
     navigator.share({
       title,
@@ -71,8 +55,8 @@ export const ShareButton = ({ title, url, text }: Props) => {
   }
 
   return (
-    <button onClick={handleClick} className="button-secondary" title="Share">
-      <ShareIcon />
+    <button onClick={handleClick} className={className} title="Share">
+      <ShareIcon className="xl:w-5 w-4 h-4 xl:h-5" />
     </button>
   )
 }
