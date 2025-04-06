f9d7e740d329572d0cf5ab97d9d26e3201e7822c
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-06 10:49:21 -0500

    fix styles

--- src/components/nav-bar/category-menu.tsx
+++ src/components/nav-bar/category-menu.tsx
@@ -23,26 +23,26 @@ export const CategoryMenu = () => {
 
   return (
     <div className=" text-m" ref={menuRef}>
-    <button
-      className="flex items-center gap-4 text-white rounded-md p-2 duration-200"
-      onClick={() => setIsOpen(!isOpen)}
-      aria-expanded={isOpen}
-      aria-haspopup="true"
-    >
-      Categories
-      <svg
-        fill="none"
-        className={`h-4 w-4 transition-transform duration-200 ${isOpen ? 'rotate-180' : ''}`}
-        strokeWidth="2"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
+      <button
+        className="flex items-center gap-4 text-white rounded-md p-2 duration-200"
+        onClick={() => setIsOpen(!isOpen)}
+        aria-expanded={isOpen}
+        aria-haspopup="true"
       >
-        <path d="M19 9l-7 7-7-7" />
-      </svg>
-    </button>
+        Categories
+        <svg
+          fill="none"
+          className={`h-4 w-4 transition-transform duration-200 ${isOpen ? 'rotate-180' : ''}`}
+          strokeWidth="2"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          aria-hidden="true"
+        >
+          <path d="M19 9l-7 7-7-7" />
+        </svg>
+      </button>
       <ul
-        className={`absolute  w-48 max-h-72 overflow-y-auto top-16 flex flex-col gap-2 bg-Primary-950/40 backdrop-blur-sm custom-scrollbar rounded-b-md shadow-lg transition-all duration-200 ease-in-out ${
+        className={`absolute  w-48 max-h-72 overflow-y-auto top-16 flex flex-col gap-2 bg-Primary-950/60  custom-scrollbar rounded-b-md shadow-lg backdrop-blur-sm transition-all duration-200 ease-in-out ${
           isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
         }`}
         role="menu"
@@ -52,7 +52,7 @@ export const CategoryMenu = () => {
           <li key={genre} className="hover:bg-Complementary/20">
             <a
               href={`/search?genre_filter=${genre.toLowerCase()}`}
-              className="block px-4 py-2 text-white hover:bg-Complementary/20 transition-colors duration-200 focus:outline-none focus:bg-Complementary/30"
+              className="block px-6 py-2  text-white hover:bg-Complementary/20 hover:text-enfasisColor transition-colors duration-200 focus:outline-none focus:bg-Complementary/30"
               role="menuitem"
             >
               {genre}
