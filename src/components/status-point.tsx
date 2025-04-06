bb5ae354345d8b646268fb01d125d055716e0aa4
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-06 00:23:06 -0500

    fix status point

--- src/components/status-point.tsx
+++ src/components/status-point.tsx
@@ -24,16 +24,9 @@ interface StatusPointProps {
 }
 export const StatusPoint = ({ class: className, status }: StatusPointProps) => {
   return (
-    <div title={status}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        viewBox="0 0 24 24"
-        className={className}
-      >
-        <path d="M0 0h24v24H0z" fill="none" />
-        <path d="M12 7a5 5 0 1 1-4.995 5.217L7 12l.005-.217A5 5 0 0 1 12 7z" />
-      </svg>
-    </div>
+    <div
+      title={status}
+      className={`${className} ${status === 'Currently Airing' ? 'pulse' : ''}`}
+    ></div>
   )
 }
