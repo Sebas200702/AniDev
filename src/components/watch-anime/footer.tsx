b35003d12da204d12518ff2470b68f061aadf1ff
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-26 11:58:34 -0500

    Add Footer component for displaying episode details

---
+++ src/components/watch-anime/footer.tsx
@@ -0,0 +1,23 @@
+interface FooterProps {
+  episodeTitle?: string
+  episodeDescription?: string
+  title: string
+  episode: number
+}
+export const Footer = ({
+  episodeTitle,
+  title,
+  episode,
+  episodeDescription,
+}: FooterProps) => {
+  return (
+    <footer>
+      <h3 className="h-auto p-4 text-xl font-bold text-white">
+        {episodeTitle ?? '(Untitled)'} - {title} - Episode {episode}
+      </h3>
+      <p className="font-size-sm p-4 text-gray-400">
+        {episodeDescription ?? 'No description'}
+      </p>
+    </footer>
+  )
+}
