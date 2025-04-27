69818f4def7ce25c24249e65b3395b62218c2a67
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-27 15:23:38 -0500

    Add CalendarList component to display calendar sections

---
+++ src/components/calendar/calendar-list.tsx
@@ -0,0 +1,22 @@
+import { SectionList } from '@components/section-list'
+import { useCalendarListStore } from '@store/calendar-list-store'
+
+export const CalendarList = () => {
+  const { calendarList, setCalendarList } = useCalendarListStore()
+
+  return (
+    <ul className="flex flex-row gap-4 text-lx">
+      {calendarList.map((section) => (
+        <SectionList
+          key={section.label}
+          context='calendar'
+          section={section}
+          sections={{
+            list: calendarList,
+            set: setCalendarList,
+          }}
+        />
+      ))}
+    </ul>
+  )
+}
