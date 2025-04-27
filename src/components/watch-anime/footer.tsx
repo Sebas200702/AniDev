152fdbe2a110694a72c9d4cfaa86b714a74ba659
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-26 20:08:47 -0500

    Add loading state to Footer component for better user experience

--- src/components/watch-anime/footer.tsx
+++ src/components/watch-anime/footer.tsx
@@ -4,12 +4,21 @@ interface FooterProps {
   title: string
   episode: number
 }
+const FooterLoader = () => {
+  return (
+    <footer className="flex h-full w-full animate-pulse flex-col gap-8 rounded-lg p-4 transition-all duration-300 ease-in-out ">
+      <div className="xl:h-8 h-16 md:w-120 w-full animate-pulse rounded-md bg-zinc-800 transition-all  duration-200 ease-in-out"></div>
+      <div className="xl:h-20 h-32 w-full animate-pulse rounded-md bg-zinc-800 transition-all duration-200 ease-in-out"></div>
+    </footer>
+  )
+}
 export const Footer = ({
   episodeTitle,
   title,
   episode,
   episodeDescription,
 }: FooterProps) => {
+  if (!title || !episode) return <FooterLoader />
   return (
     <footer>
       <h3 className="h-auto p-4 text-xl font-bold text-white">
