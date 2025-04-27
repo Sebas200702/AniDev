568f5b9c035b698e077c6590b935f77dba0a35bc
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-27 15:22:48 -0500

    Refactor AnimeLoader component layout for improved spacing and responsiveness

--- src/components/anime-info/anime-loader.tsx
+++ src/components/anime-info/anime-loader.tsx
@@ -28,18 +28,25 @@ export const AnimeLoader = () => {
       <div className="relative aspect-[1080/600] h-[60dvh] w-full animate-pulse bg-zinc-800 duration-300">
         <div className="to-Primary-950/100 absolute right-0 bottom-0 left-0 h-full w-full bg-gradient-to-b from-transparent transition-all duration-300 ease-in-out" />
       </div>
-      <div className="z-10 -mt-96 grid grid-cols-1 gap-4 px-4 md:-mt-54 md:grid-cols-3 md:gap-15 md:px-20 xl:grid-cols-5">
-        <div className="row-span-2 p-8 md:p-0">
+      <div className="z-10 -mt-[60vh] grid grid-cols-1 gap-10 px-4 md:-mt-54 md:grid-cols-3 md:gap-15 md:px-20 xl:grid-cols-5 md:mb-20 mb-10">
+        <div className="row-span-2 flex flex-col px-8 md:gap-8 gap-6 md:p-0 mt-26 md:mt-0">
           <div className="aspect-[225/330] h-auto w-full animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out"></div>
+          <div className="animate-pulse rounded-lg xl:h-12 h-8 w-full bg-zinc-800 duration-300 ease-in-out"></div>
         </div>
-        <div className="flex h-full w-full flex-col justify-end gap-4 md:col-span-2 xl:col-span-4">
-          <div className="h-20 w-full animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out md:w-[80%]"></div>
+        <div className="flex h-full w-full flex-col justify-end md:col-span-2 xl:col-span-4 gap-6 md:gap-8">
+          <div className="xl:h-20 md:h-12 md:mx-0 mx-auto animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out w-[80%]"></div>
 
-          <div className="h-20 w-60 animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out md:h-15 md:w-90"></div>
+          <div className="h-10 w-90 md:mx-0 mx-auto animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out"></div>
         </div>
 
-        <div className="row-span-2 h-100 w-full animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out md:col-span-2 xl:col-span-3"></div>
-        <div className="h-200 w-full animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out"></div>
+        <div className="row-span-2  w-full  md:col-span-2 xl:col-span-3 flex flex-col gap-4">
+          <div className="animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out h-12 w-60"></div>
+          <div className="animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out h-100 w-full"></div>
+        </div>
+        <div className=" w-full px-10 md:px-0 animate-pulse ">
+          <div className="w-[80%] mx-auto xl:h-10 h-8  rounded-t-lg bg-zinc-700 "></div>
+          <div className="  h-200 animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out"></div>
+        </div>
       </div>
     </div>
   )
