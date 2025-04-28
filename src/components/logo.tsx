a4d94a658c77211159d94921e31198b8dbc17905
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-27 22:16:21 -0500

    Add Logo component for site branding

---
+++ src/components/logo.tsx
@@ -0,0 +1,9 @@
+export const Logo = () => {
+  return (
+    <a className="mr-4" href="/">
+      <h1>
+        Ani<span className="text-enfasisColor font-black">Dev</span>
+      </h1>
+    </a>
+  )
+}
