6acd79d2f34a8776a9fa61e150ad6ae4d5769ee3
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-30 16:23:52 -0500

    Refactor LoadingCarousel component: adjust height and width for improved responsiveness and layout

--- src/components/index/carousel/carousel-loader.tsx
+++ src/components/index/carousel/carousel-loader.tsx
@@ -25,15 +25,15 @@ import { Overlay } from '@components/overlay'
  * <LoadingCarousel />
  */
 export const LoadingCarousel = () => (
-  <div className="carousel-anime-banner relative h-[65vh] animate-pulse bg-zinc-900 md:h-[90vh]">
-    <div className="relative flex h-[65vh] w-full flex-shrink-0 flex-col items-center md:h-[90vh] md:flex-row">
+  <div className="carousel-anime-banner relative h-[85vh] md:h-[650px] animate-pulse bg-zinc-900 xl:h-[900px]">
+    <div className="relative flex h-[85vh] w-full flex-shrink-0 md:h-[650px] flex-col items-center xl:h-[900px] md:flex-row">
       <div className="z-10 mx-auto -mt-14 flex h-full w-full max-w-2xl flex-col items-center justify-center gap-4 p-6 text-white md:mr-16 md:ml-8 md:h-auto md:items-start md:justify-normal">
         <div className="z-30 h-20 w-full animate-pulse rounded-lg bg-zinc-800 md:mt-4 md:mb-4"></div>
         <div className="z-30 hidden h-12 w-full animate-pulse rounded-lg bg-zinc-800 md:flex"></div>
         <Overlay className="to-Primary-950/100 h-full w-full bg-gradient-to-b" />
-        <div className="flex w-full flex-row items-center gap-4 md:mt-2">
-          <div className="h-10 w-32 animate-pulse rounded-lg bg-zinc-800 md:flex"></div>
-          <div className="h-10 w-32 animate-pulse rounded-lg bg-zinc-800"></div>
+        <div className="flex w-full flex-row items-center justify-center md:justify-normal gap-4 md:mt-2">
+          <div className="h-12 w-34 animate-pulse rounded-lg bg-zinc-800 md:flex"></div>
+          <div className="h-12 w-34 animate-pulse rounded-lg bg-zinc-800"></div>
         </div>
       </div>
     </div>
