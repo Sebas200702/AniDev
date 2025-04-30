654375dc473c53db47a1d33320a2a43afbcf6db3
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-29 22:47:24 -0500

    Remove margin from logo link in Logo component

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,6 +1,6 @@
 export const Logo = () => {
   return (
-    <a className="mr-8" href="/">
+    <a  href="/">
       <h1>
         Ani<span className="text-enfasisColor font-black">Dev</span>
       </h1>
