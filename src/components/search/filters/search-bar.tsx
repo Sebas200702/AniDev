ff754b20c2b6a514b2739e1600a7bcd80b2662f8
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-30 06:34:27 -0500

    Refactor SearchBar styles for improved hover effects and border visibility

--- src/components/search/filters/search-bar.tsx
+++ src/components/search/filters/search-bar.tsx
@@ -4,7 +4,7 @@ export const SearchBar = () => {
   const { query, setQuery } = useSearchStoreResults()
 
   return (
-    <div className="hover:bg-enfasisColor/20 hover:border-enfasisColor flex justify-center rounded-md border border-gray-100/10 px-4 text-white transition-all duration-200 ease-in-out">
+    <div className="hover:bg-enfasisColor/5 hover:border-enfasisColor/40 flex justify-center rounded-md border border-gray-100/10 px-4 text-white transition-all duration-200 ease-in-out">
       <input
         type="text"
         className="min-w-[50px] flex-grow cursor-pointer bg-transparent focus:outline-none"
