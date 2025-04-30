3313a39170d04a3e20389e5578e76f62f2e7fddf
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-30 16:24:07 -0500

    Refactor Carousel component: adjust height for improved layout and responsiveness

--- src/components/index/carousel/carousel.tsx
+++ src/components/index/carousel/carousel.tsx
@@ -142,7 +142,7 @@ export const Carousel = (): JSX.Element => {
 
   return (
     <section
-      className={`fade-out relative right-0 left-0 h-[65vh] md:h-[90vh] ${fadeIn ? 'opacity-100 transition-all duration-200' : 'opacity-0'} `}
+      className={`fade-out relative right-0 left-0 h-[85vh] md:h-[650px] xl:h-[900px] ${fadeIn ? 'opacity-100 transition-all duration-200' : 'opacity-0'} `}
       data-carousel="slide"
       style={{ position: 'sticky' }}
       aria-label="Carousel of Animes"
