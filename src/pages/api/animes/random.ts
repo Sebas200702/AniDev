babbf29872c7efb9f13115a23ce57a382f6caeb6
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-30 19:08:29 -0500

    Add API route for fetching a random anime with rate limiting and error handling

---
+++ src/pages/api/animes/random.ts
@@ -0,0 +1,26 @@
+import { redis } from '@libs/redis'
+import { rateLimit } from '@middlewares/rate-limit'
+import { redisConnection } from '@middlewares/redis-connection'
+import { getRandomAnime } from '@utils/get-random-anime'
+import type { APIRoute } from 'astro'
+export const GET: APIRoute = rateLimit(
+  redisConnection(async () => {
+    try {
+      const result = await getRandomAnime()
+      return new Response(JSON.stringify(result), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+    } catch (error) {
+      return new Response(
+        JSON.stringify({ error: 'Ocurrió un error en el servidor.' }),
+        {
+          status: 500,
+          headers: { 'Content-Type': 'application/json' },
+        }
+      )
+    }
+  })
+)
