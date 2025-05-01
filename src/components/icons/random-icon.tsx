44a618f74b0dc557a4e0090266cf26e32e71799b
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-30 19:07:04 -0500

    Add RandomIcon component: create a new SVG icon for random functionality

---
+++ src/components/icons/random-icon.tsx
@@ -0,0 +1,19 @@
+import type { IconProps } from 'types'
+export const RandomIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="m18 4 3 3-3 3M18 20l3-3-3-3" />
+      <path d="M3 7h3a5 5 0 0 1 5 5 5 5 0 0 0 5 5h5M21 7h-5a4.978 4.978 0 0 0-3 1m-4 8a4.984 4.984 0 0 1-3 1H3" />
+    </svg>
+  )
+}
