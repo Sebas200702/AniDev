cf2c5a9350b73098e220953211f6b05ce16ef90c
Sebas200702 <sebastorregroza6@gmail.com>
2025-04-30 19:42:07 -0500

    Refactor getRandomAnime function to use RPC for fetching a weighted random anime entry

--- src/utils/get-random-anime.ts
+++ src/utils/get-random-anime.ts
@@ -1,11 +1,7 @@
 import { supabase } from '@libs/supabase'
 import type { RandomAnime } from 'types'
 export const getRandomAnime = async (): Promise<RandomAnime> => {
-  const randomId = Math.floor(Math.random() * (61483 - 1 + 1) + 1)
-  const { data, error } = await supabase
-    .from('old_anime')
-    .select('mal_id, title')
-    .eq('mal_id', randomId)
+  const { data, error } = await supabase.rpc('get_weighted_random_entry')
 
   if (error || !data) {
     return await getRandomAnime()
