efd8d7b90dd0e60051af95d905087e37100c0472
Sebastian Torregroza <143149476+Sebas200702@users.noreply.github.com>
2025-05-05 17:10:22 -0500

    Update astro-auth-extensions.d.ts

--- src/astro-auth-extensions.d.ts
+++ src/astro-auth-extensions.d.ts
@@ -1,7 +1,6 @@
-export {} // Esto asegura que el archivo sea tratado como un módulo
+export {}
 
 declare module '@auth-astro/client' {
-  // Se "fusiona" con la definición original de AstroSignInOptions
   interface AstroSignInOptions {
     callbackUrl?: string
   }
