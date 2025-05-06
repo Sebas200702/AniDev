2d666a1166c4d45587c850d610d99a62d76305fb
Sebas200702 <sebastorregroza6@gmail.com>
2025-05-06 10:39:23 -0500

    Update NavBarMenu button SVG to use viewBox for consistent sizing

--- src/components/nav-bar/nav-bar-menu.tsx
+++ src/components/nav-bar/nav-bar-menu.tsx
@@ -11,13 +11,13 @@ export const NavBarMenu = () => {
       <button className="cursor-pointer md:hidden" onClick={toggleMenu}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
+          viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth="2"
+          className="w-6 h-6"
         >
           <path stroke="none" d="M0 0h24v24H0z" />
           <path d="M4 6h16M4 12h16M4 18h16" />
