731369420d534f606a8614b354485ea9d446c0b0
Sebas200702 <sebastorregroza6@gmail.com>
2025-05-06 10:40:25 -0500

    Refactor GET API route to remove redis connection middleware for simplified error handling

--- src/pages/api/animes/random.ts
+++ src/pages/api/animes/random.ts
@@ -1,25 +1,23 @@
 import { rateLimit } from '@middlewares/rate-limit'
-import { redisConnection } from '@middlewares/redis-connection'
+
 import { getRandomAnime } from '@utils/get-random-anime'
 import type { APIRoute } from 'astro'
-export const GET: APIRoute = rateLimit(
-  redisConnection(async () => {
-    try {
-      const result = await getRandomAnime()
-      return new Response(JSON.stringify(result), {
-        status: 200,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-    } catch (error) {
-      return new Response(
-        JSON.stringify({ error: 'Ocurrió un error en el servidor.' }),
-        {
-          status: 500,
-          headers: { 'Content-Type': 'application/json' },
-        }
-      )
-    }
-  })
-)
+export const GET: APIRoute = rateLimit(async () => {
+  try {
+    const result = await getRandomAnime()
+    return new Response(JSON.stringify(result), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: 'Ocurrió un error en el servidor.' }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+  }
+})
