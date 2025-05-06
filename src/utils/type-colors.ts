5b8c1d46770ed074e2e1d67c532fb84aff99a8c5
Sebas200702 <sebastorregroza6@gmail.com>
2025-05-06 10:40:51 -0500

    Fix typeColors function to handle TV_SPECIAL in anime type classification

--- src/utils/type-colors.ts
+++ src/utils/type-colors.ts
@@ -22,7 +22,7 @@ export const typeColors = (type: string) => {
   if (type === AnimeTypes.TV) return 'text-emerald-700 '
   if (type === AnimeTypes.OVA) return 'text-green-500 '
   if (type === AnimeTypes.MOVIE) return 'text-purple-500 '
-  if (type === AnimeTypes.SPECIAL) return 'text-yellow-500 '
+  if (type === AnimeTypes.SPECIAL || type === AnimeTypes.TV_SPECIAL) return 'text-yellow-500 '
   if (type === AnimeTypes.ONA) return 'text-red-500 '
   if (type === AnimeTypes.PV) return 'text-orange-500 '
   if (type === AnimeTypes.CM) return 'text-indigo-600 '
