33cbd9f958f89a111474e4029c67bab39d48ddd1
Sebas200702 <sebastorregroza6@gmail.com>
2025-05-10 12:17:39 -0500

    Integrate parental control parameter into getRandomAnime function to enhance anime selection based on user preferences, improving the overall user experience.

--- src/utils/get-random-anime.ts
+++ src/utils/get-random-anime.ts
@@ -1,10 +1,14 @@
 import { supabase } from '@libs/supabase'
 import type { RandomAnime } from 'types'
-export const getRandomAnime = async (): Promise<RandomAnime> => {
-  const { data, error } = await supabase.rpc('get_weighted_random_entry')
+export const getRandomAnime = async (
+  parentalControl: boolean | null
+): Promise<RandomAnime> => {
+  const { data, error } = await supabase.rpc('get_weighted_random_entry', {
+    parental_control: parentalControl,
+  })
 
   if (error || !data) {
-    return await getRandomAnime()
+    return await getRandomAnime(parentalControl)
   }
 
   return data[0] as RandomAnime
