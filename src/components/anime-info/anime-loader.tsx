715dd4304ff51af95890cfc41d90f831b082efd2
Sebas200702 <sebastorregroza6@gmail.com>
2025-05-11 22:18:59 -0500

    Refactor AnimeLoader component layout by adjusting element heights and grid structure for improved visual consistency and responsiveness.

--- src/components/anime-info/anime-loader.tsx
+++ src/components/anime-info/anime-loader.tsx
@@ -28,19 +28,19 @@ export const AnimeLoader = () => {
       <div className="fixed aspect-[1080/600] h-[60dvh] w-full animate-pulse bg-zinc-800 duration-300">
         <div className="to-Primary-950/100 absolute right-0 bottom-0 left-0 h-full w-full bg-gradient-to-b from-transparent transition-all duration-300 ease-in-out" />
       </div>
-      <div className="z-10 pt-[35dvh] mb-10 grid grid-cols-1 gap-10 px-4 md:-mt-54 md:mb-20 md:grid-cols-3 md:gap-15 md:px-20 xl:grid-cols-5">
-        <div className="row-span-2 row-start-2 flex flex-col gap-6  md:mt-0 md:gap-8 md:p-0">
+      <div className="z-10 pt-[35dvh] mb-10 grid grid-cols-1 gap-10 px-4 md:mb-20 md:grid-cols-3 md:gap-15 md:px-20 xl:grid-cols-5">
+        <div className="row-span-2 row-start-2 md:row-start-1 flex flex-col gap-6 -mt-4  md:mt-0 md:gap-8 md:p-0">
           <div className="aspect-[225/330] md:h-auto w-full h-0 px-8 animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out"></div>
           <div className="h-10 w-full animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out xl:h-12"></div>
         </div>
         <div className="flex h-full w-full flex-col justify-end gap-6 md:col-span-2 md:gap-8 xl:col-span-4">
-          <div className="mx-auto h-12 w-[80%] animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out md:mx-0 md:h-12 xl:h-20"></div>
+          <div className="mx-auto h-8 w-[80%] animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out md:mx-0 md:h-12 xl:h-20"></div>
 
-          <div className="mx-auto h-10 w-90 animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out md:mx-0"></div>
+          <div className="mx-auto h-8 md:h-10 w-90 animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out md:mx-0"></div>
         </div>
 
         <div className="row-span-2 flex w-full flex-col gap-4 md:col-span-2 xl:col-span-3">
-          <div className="h-12 w-60 animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out"></div>
+          <div className="h-9 w-60 animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out"></div>
           <div className="h-100 w-full animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out"></div>
         </div>
         <div className="w-full animate-pulse px-10 md:px-0">
