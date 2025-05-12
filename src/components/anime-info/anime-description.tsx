a3ecd2f22f5dd63e026d40767573cbdaf2f6cee4
Sebas200702 <sebastorregroza6@gmail.com>
2025-05-11 21:57:22 -0500

    Enhance AnimeDescription component by adjusting z-index for improved layering in the UI.

--- src/components/anime-info/anime-description.tsx
+++ src/components/anime-info/anime-description.tsx
@@ -32,7 +32,7 @@ interface Props {
 
 export const AnimeDescription = ({ synopsis }: Props) => {
   return (
-    <div className="bg-Complementary text-Primary-50/90 text-m h-auto w-full space-y-2 rounded-md p-6">
+    <div className="z-10 bg-Complementary  text-Primary-50/90 text-m h-auto w-full space-y-2 rounded-md p-6">
       <p className="max-w-full">{splitTextOnP(synopsis ?? '')[0]}</p>
       <p className="max-w-full">{splitTextOnP(synopsis ?? '')[1]}</p>
     </div>
