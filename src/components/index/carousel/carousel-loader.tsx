bba852ca5fbd17322b6ecf084b5248dd1f7a7660
Sebas200702 <sebastorregroza6@gmail.com>
2025-05-11 21:58:32 -0500

    Refactor LoadingCarousel component layout by adjusting height, padding, and element sizes for improved responsiveness and visual consistency.

--- src/components/index/carousel/carousel-loader.tsx
+++ src/components/index/carousel/carousel-loader.tsx
@@ -25,18 +25,19 @@ import { Overlay } from '@components/overlay'
  * <LoadingCarousel />
  */
 export const LoadingCarousel = () => (
-  <div className="carousel-anime-banner relative h-[85vh] animate-pulse bg-zinc-900 md:h-[650px] xl:h-[90vh]">
-    <div className="relative flex h-[40vh] w-full flex-shrink-0 flex-col items-center md:h-[650px] md:flex-row xl:h-[90vh]">
-      <div className="z-10 mx-auto -mt-14 flex h-full w-full max-w-2xl flex-col items-center justify-center gap-4 p-6 text-white md:mr-16 md:ml-8 md:h-auto md:items-start md:justify-normal">
-        <div className="z-30 h-20 w-full animate-pulse rounded-lg bg-zinc-800 md:mt-4 md:mb-4"></div>
-        <div className="z-30 hidden h-12 w-full animate-pulse rounded-lg bg-zinc-800 md:flex"></div>
-        <Overlay className="to-Primary-950 mt-16 h-full w-full bg-gradient-to-b" />
-        <div className="flex w-full flex-row items-center justify-center gap-4 md:mt-2 md:justify-normal">
-          <div className="h-12 w-34 animate-pulse rounded-lg bg-zinc-800 md:flex"></div>
-          <div className="h-12 w-34 animate-pulse rounded-lg bg-zinc-800"></div>
+  <div className="carousel-anime-banner relative animate-pulse bg-zinc-900 h-[70vh] md:h-[650px] xl:h-[90vh]">
+    <div className="relative flex h-[70vh]  w-full flex-shrink-0 flex-col items-center md:h-[650px] md:flex-row xl:h-[90vh]">
+      <div className="z-10 mx-auto  flex h-full w-full max-w-2xl items-center flex-col justify-center gap-4 p-6 pt-36 md:-mt-4 md:pt-0 text-white md:mr-16 md:ml-8 md:h-auto md:items-start md:justify-normal">
+        <div className="z-30 h-12 w-[80%] animate-pulse rounded-lg bg-zinc-800  md:mb-4"></div>
+        <div className="z-30 h-12 mt-2  w-full animate-pulse rounded-lg bg-zinc-800 md:flex"></div>
+
+        <div className="flex w-full flex-row items-center justify-center gap-4 mt-5 md:mt-2 md:justify-normal">
+          <div className=" md:max-w-44 w-full h-10 animate-pulse rounded-lg bg-zinc-800 md:flex"></div>
+          <div className=" md:max-w-44 w-full h-10 animate-pulse rounded-lg bg-zinc-800"></div>
         </div>
       </div>
     </div>
-    <div className="absolute bottom-20 left-1/2 z-30 flex h-4 w-44 -translate-x-1/2 animate-pulse rounded-lg bg-zinc-800 md:bottom-16"></div>
+    <div className="absolute bottom-21 left-1/2 z-30 flex h-4 w-44 -translate-x-1/2 animate-pulse rounded-lg bg-zinc-800 md:bottom-16"></div>
+    <Overlay className="to-Primary-950 mt-16 h-full w-full bg-gradient-to-b" />
   </div>
 )
