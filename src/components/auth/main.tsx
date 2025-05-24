819174d126eca523e906ade3647c42465884cd4d
Sebas200702 <sebastorregroza6@gmail.com>
2025-05-24 08:49:06 -0500

    Refactor Main component to implement a step-based authentication flow using Step1, Step2, and Step3 components. Update state management to utilize useStepsStore and enhance UI with dynamic titles and descriptions for each step.

--- src/components/auth/main.tsx
+++ src/components/auth/main.tsx
@@ -1,90 +1,52 @@
-import { GoogleBtn } from '@components/auth/google-btn'
-import { Input } from '@components/auth/input'
-import { EmailIcon } from '@components/icons/email-icon'
-import { PasswordIcon } from '@components/icons/password-icon'
-import { UserIcon } from '@components/icons/user-icon'
-import { useAuthFormStore } from '@store/auth-form-store'
+import { Step1, Step2, Step3 } from '@components/auth/steps/index'
+import { useStepsStore } from '@store/steps-store'
 
 interface Props {
   isLoading: boolean
   isSignUp: boolean
   title: string
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void
 }
 
-export const Main = ({ isLoading, isSignUp, title, onSubmit }: Props) => {
-  const { currentStep, steps } = useAuthFormStore()
+export const Main = ({ isLoading, isSignUp, title }: Props) => {
+  const { currentStep, steps } = useStepsStore()
+
   return (
-    <section className="flex w-full flex-col items-center justify-center px-20">
-      {currentStep === 1 && (
+    <section className="flex w-full h-full flex-col items-center justify-center px-18 py-8 ">
+      {steps[currentStep - 1] && (
         <>
-          <h2 className="text-Primary-50 text-lx mb-6 font-bold">{title}</h2>
-
-          <form onSubmit={onSubmit} className="flex w-full flex-col gap-6">
-            {isSignUp && (
-              <Input
-                name="user_name"
-                type="text"
-                placeholder="Username"
-                required
-              >
-                <UserIcon className="h-5 w-5" />
-              </Input>
-            )}
-
-            <Input name="email" type="email" placeholder="Email" required>
-              <EmailIcon className="h-5 w-5" />
-            </Input>
-
-            <Input
-              name="password"
-              type="password"
-              placeholder="Password"
-              required
-            >
-              <PasswordIcon className="h-5 w-5" />
-            </Input>
-
-            <button
-              type="submit"
-              disabled={isLoading}
-              className="button-primary w-full focus:ring-2 focus:outline-none disabled:opacity-50"
-            >
-              {isLoading ? 'Procesando...' : title}
-            </button>
-
-            <div className="my-4 flex items-center">
-              <div className="flex-grow border-t border-gray-400"></div>
-              <span className="mx-4 text-gray-200">or</span>
-              <div className="flex-grow border-t border-gray-400"></div>
-            </div>
-          </form>
-
-          <GoogleBtn />
-
-          <footer className="text-Primary-200 mt-6 text-center text-sm">
-            {title === 'Sign In' ? (
-              <p>
-                <span>Don&apos;t have an account?</span>{' '}
-                <a href="/signup" className="text-blue-500 hover:underline">
-                  Sign up
-                </a>
-              </p>
-            ) : (
-              <p>
-                Already have an account?{' '}
-                <a href="/signin" className="text-blue-500 hover:underline">
-                  Sign in
-                </a>
-              </p>
-            )}
-          </footer>
+          <h2 className="text-Primary-50 text-lx mb-4 font-bold">
+            {steps[currentStep - 1].title}
+          </h2>
+          <p className="text-Primary-100 text-sm mb-6">
+            {steps[currentStep - 1].description}
+          </p>
+          {currentStep === 1 ? (
+            <Step1 isLoading={isLoading} title={title} />
+          ) : currentStep === 2 ? (
+            <Step2 isSignUp={isSignUp} />
+          ) : (
+            <Step3 />
+          )}
+          {currentStep === 1 && (
+            <footer className="text-Primary-200 mt-6 text-center text-sm">
+              {title === 'Sign In' ? (
+                <p>
+                  <span>Don&apos;t have an account?</span>{' '}
+                  <a href="/signup" className="text-blue-500 hover:underline">
+                    Sign up
+                  </a>
+                </p>
+              ) : (
+                <p>
+                  Already have an account?{' '}
+                  <a href="/signin" className="text-blue-500 hover:underline">
+                    Sign in
+                  </a>
+                </p>
+              )}
+            </footer>
+          )}
         </>
-      )}
-      {currentStep === 2 && (
-        <div>
-          <h2>{steps[currentStep - 1].title}</h2>
-        </div>
       )}
     </section>
   )
