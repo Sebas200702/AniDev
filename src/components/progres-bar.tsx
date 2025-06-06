20e397ce15d28caf94bd09a7fb811d9e79595b64
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-06 16:51:41 -0500

    Fix ProgressBar component className formatting for improved readability and consistency.

--- src/components/progres-bar.tsx
+++ src/components/progres-bar.tsx
@@ -74,7 +74,7 @@ export const ProgressBar = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 z-50 w-full rounded-md transition-opacity duration-300 ${
+      className={`fixed top-0 left-0 z-50 w-full  rounded-md transition-opacity duration-300 ${
         isLoading ? 'opacity-100' : 'opacity-0'
       }`}
     >
