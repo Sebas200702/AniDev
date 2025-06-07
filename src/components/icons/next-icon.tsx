88c973cb1a06a3cee4738ec6d7d8792474e9f7a0
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-07 10:56:47 -0500

    Add NextIcon component: create a new SVG icon for navigation functionality, expanding the icon library for enhanced UI representation.

---
+++ src/components/icons/next-icon.tsx
@@ -0,0 +1,19 @@
+import type { IconProps } from 'types'
+
+export const NextIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M3 5v14l8-7zM14 5v14l8-7z" />
+    </svg>
+  )
+}
