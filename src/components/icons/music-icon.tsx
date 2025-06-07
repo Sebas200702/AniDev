4735e2896e7d6b5e3d1a69123af55312344b5b72
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-07 10:56:39 -0500

    Add MusicIcon component: create a new SVG icon for music representation, expanding the icon library for enhanced UI functionality.

---
+++ src/components/icons/music-icon.tsx
@@ -0,0 +1,20 @@
+import type { IconProps } from 'types'
+
+export const MusicIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M3 17a3 3 0 1 0 6 0 3 3 0 0 0-6 0M13 17a3 3 0 1 0 6 0 3 3 0 0 0-6 0" />
+      <path d="M9 17V4h10v13M9 8h10" />
+    </svg>
+  )
+}
