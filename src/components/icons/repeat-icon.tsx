7fb391344d2f0b869bebc86806aea796685cb5d9
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-07 10:57:09 -0500

    Add RepeatIcon component: create a new SVG icon for repeat functionality, expanding the icon library for enhanced UI representation.

---
+++ src/components/icons/repeat-icon.tsx
@@ -0,0 +1,19 @@
+import type { IconProps } from 'types'
+
+export const RepeatIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M4 12V9a3 3 0 0 1 3-3h13m-3-3 3 3-3 3M20 12v3a3 3 0 0 1-3 3H4m3 3-3-3 3-3" />
+    </svg>
+  )
+}
