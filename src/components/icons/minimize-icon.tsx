92399e2145b9a9559acd59561bad8e384193c0d2
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-07 10:56:33 -0500

    Add MinimizeIcon component: create a new SVG icon for minimize functionality, expanding the icon library for enhanced UI representation.

---
+++ src/components/icons/minimize-icon.tsx
@@ -0,0 +1,19 @@
+import type { IconProps } from 'types'
+
+export const MinimizeIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M6 10h4V6M4 4l6 6M18 14h-4v4M14 14l6 6" />
+    </svg>
+  )
+}
