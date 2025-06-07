a7c4e490a538d8113c8961a5ff26a14835ef77dd
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-06 19:13:00 -0500

    Update LoadingCard component: change background color from zinc-700 to zinc-800 for improved visual consistency.

--- src/components/search/results/loading-card.tsx
+++ src/components/search/results/loading-card.tsx
@@ -5,6 +5,6 @@
  */
 export const LoadingCard = () => {
   return (
-    <div className="flex aspect-[225/330] h-full w-full animate-pulse flex-col rounded-lg bg-zinc-700 p-4 duration-200"></div>
+    <div className="flex aspect-[225/330] h-full w-full animate-pulse flex-col rounded-lg bg-zinc-800 p-4 duration-200"></div>
   )
 }
