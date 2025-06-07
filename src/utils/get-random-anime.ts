4271905498caf6c310171ca3ae9e056cbe57a705
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-06 19:28:41 -0500

    Refactor getRandomAnime utility: update Supabase RPC call for improved functionality and add error logging for better debugging.

--- src/utils/get-random-anime.ts
+++ src/utils/get-random-anime.ts
@@ -3,13 +3,16 @@ import type { RandomAnime } from 'types'
 export const getRandomAnime = async (
   parentalControl: boolean | null
 ): Promise<RandomAnime> => {
-  const { data, error } = await supabase.rpc('get_weighted_random_entry', {
+  const { data, error } = await supabase.rpc('get_random_anime', {
     parental_control: parentalControl,
   })
 
+  const randomAnime = data[0] as RandomAnime
+
   if (error || !data) {
+    console.log('Error al obtener el anime aleatorio:', error)
     return await getRandomAnime(parentalControl)
   }
 
-  return data[0] as RandomAnime
+  return randomAnime
 }
