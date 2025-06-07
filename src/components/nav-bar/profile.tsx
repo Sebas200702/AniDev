24b3d8a9e6b9f61aead2274e5e5cca8996d0a519
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-06 23:49:42 -0500

    Refactor Profile component: replace SVG icons with SettingsIcon and UserIcon components for improved maintainability and styling consistency.

--- src/components/nav-bar/profile.tsx
+++ src/components/nav-bar/profile.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useRef } from 'react'
 
+import { SettingsIcon } from '@components/icons/settings-icon'
 import { useGlobalUserPreferences } from '@store/global-user'
 import { signOut } from 'auth-astro/client'
+import { UserIcon } from '@components/icons/user-icon'
 
 /**
  * Profile component renders the user's profile information and a dropdown menu.
@@ -70,7 +72,7 @@ export const Profile = () => {
       </article>
       <div
         ref={dropdownRef}
-        className="bg-Primary-950/60 absolute top-16 right-0 z-50 hidden w-48 rounded-b-md p-4 text-base text-white shadow-lg backdrop-blur-sm md:right-4"
+        className="bg-Primary-950/60 absolute top-16 right-0 z-50 hidden w-48 md:rounded-lg rounded-b-md p-4 text-base text-white shadow-lg backdrop-blur-sm md:right-4"
       >
         <ul className="space-y-6">
           <li>
@@ -79,20 +81,7 @@ export const Profile = () => {
               className="text-Primary-50 md:hover:text-enfasisColor flex items-center justify-between gap-3 rounded-md p-3 text-sm transition-all duration-300 hover:bg-zinc-800/50"
             >
               <div className="flex items-center gap-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-                  />
-                </svg>
+                <SettingsIcon className="h-5 w-5" />
                 <span>Settings</span>
               </div>
               <svg
@@ -117,20 +106,7 @@ export const Profile = () => {
               className="text-Primary-50 md:hover:text-enfasisColor flex items-center justify-between gap-3 rounded-md p-3 text-sm transition-all duration-300 hover:bg-zinc-800/50"
             >
               <div className="flex items-center gap-3">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                  />
-                </svg>
+                <UserIcon className="h-5 w-5" />
                 <span>Profile</span>
               </div>
               <span className="text-gray-400 dark:text-gray-500"></span>
