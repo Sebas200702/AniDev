271684ae0abf00eb5aa8af3ba922e65ff8d8d72c
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-07 10:57:03 -0500

    Add PreviousIcon component: create a new SVG icon for previous navigation functionality, expanding the icon library for enhanced UI representation.

---
+++ src/components/icons/previous-icon.tsx
@@ -0,0 +1,19 @@
+import type { IconProps } from 'types'
+
+export const PreviousIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M21 5v14l-8-7zM10 5v14l-8-7z" />
+    </svg>
+  )
+}
