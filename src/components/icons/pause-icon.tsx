b00c243a04c77dcd9aa2a5bd0eb539f1856e5e4c
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-07 10:56:56 -0500

    Add PauseIcon component: create a new SVG icon for pause functionality, expanding the icon library for enhanced UI representation.

---
+++ src/components/icons/pause-icon.tsx
@@ -0,0 +1,19 @@
+import type { IconProps } from 'types'
+
+export const PauseIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M6 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1zM14 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1z" />
+    </svg>
+  )
+}
