9b77edbb3b9f9f5d39598506a5c41b2d380fa38f
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-11 18:00:55 -0500

    Implement music fetching logic in Music component: integrate music player store, fetch music info based on themeId, and manage playback state for improved user experience.

--- src/components/music-info/music.tsx
+++ src/components/music-info/music.tsx
@@ -1,3 +1,36 @@
-export const Music = () => {
-  return <div>Music</div>
+import { useMusicPlayerStore } from '@store/music-player-store'
+import { useEffect } from 'react'
+export const Music = ({ themeId }: { themeId: string }) => {
+  const {
+    currentSong,
+    setCurrentSong,
+    setIsPlaying,
+    setCurrentTime,
+    setDuration,
+    setIsLoading,
+    setError,
+  } = useMusicPlayerStore()
+  useEffect(() => {
+    setIsPlaying(false)
+    const fetchMusic = async () => {
+      const response = await fetch(`/api/getMusicInfo?themeId=${themeId}`)
+      const data = await response.json()
+
+      if (data[0].song_id === currentSong?.song_id) {
+        console.log('song is the same')
+        return
+      }
+
+      setCurrentSong(data[0])
+
+      setCurrentTime(0)
+      setDuration(0)
+      setIsLoading(false)
+      setError(null)
+      setIsPlaying(true)
+    }
+    fetchMusic()
+  }, [])
+
+  return null
 }
