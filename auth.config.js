cb5f7e877937aa6593785613972f8b7a87453e85
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-13 14:10:55 -0500

    refactor auth.config.js for improved readability and structure

--- auth.config.js
+++ auth.config.js
@@ -4,41 +4,42 @@ import { supabaseAdmin } from '@libs/supabase'
 import { defineConfig } from 'auth-astro'
 
 export default defineConfig({
-  providers: [
-    Google({
-      clientId: import.meta.env.GOOGLE_CLIENT_ID,
-      clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
-      authorization: { params: { scope: 'openid email profile' } },
-    }),
-  ],
-  callbacks: {
-    async signIn({ user }) {
-      const { data: pub, error: e1 } = await supabase
-        .from('public_users')
-        .select('id')
-        .eq('name', user.name)
-      if (e1) {
-        console.error(e1)
-        return false
-      }
+    secret: import.meta.env.AUTH_SECRET,
+    providers: [
+        Google({
+            clientId: import.meta.env.GOOGLE_CLIENT_ID,
+            clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
+            authorization: { params: { scope: 'openid email profile' } },
+        }),
+    ],
+    callbacks: {
+        async signIn({ user }) {
+            const { data: pub, error: e1 } = await supabase
+                .from('public_users')
+                .select('id')
+                .eq('name', user.name)
+            if (e1) {
+                console.error(e1)
+                return false
+            }
 
-      if (pub.length === 0) {
-        const { data: createRes, error: createErr } =
-          await supabaseAdmin.auth.admin.createUser({
-            email: user.email,
-            email_confirm: true,
-            user_metadata: {
-              user_name: user.name,
-              avatar_url: user.image,
-            },
-          })
-        if (createErr) {
-          console.error('Error creando usuario admin:', createErr)
-          return false
-        }
-      }
+            if (pub.length === 0) {
+                const { data: createRes, error: createErr } =
+                    await supabaseAdmin.auth.admin.createUser({
+                        email: user.email,
+                        email_confirm: true,
+                        user_metadata: {
+                            user_name: user.name,
+                            avatar_url: user.image,
+                        },
+                    })
+                if (createErr) {
+                    console.error('Error creando usuario admin:', createErr)
+                    return false
+                }
+            }
 
-      return true
+            return true
+        },
     },
-  },
 })
