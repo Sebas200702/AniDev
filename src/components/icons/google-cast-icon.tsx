93f10dc45f999d9ce8979e7a04e242d81ccdd174
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-19 14:19:01 -0500

    feat: add GoogleCastIcon component for casting functionality

---
+++ src/components/icons/google-cast-icon.tsx
@@ -0,0 +1,20 @@
+import type { IconProps } from 'types'
+
+export const GoogleCastIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M3 19h.01M7 19a4 4 0 0 0-4-4M11 19a8 8 0 0 0-8-8" />
+      <path d="M15 19h3a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6a3 3 0 0 0-2.8 2" />
+    </svg>
+  )
+}
