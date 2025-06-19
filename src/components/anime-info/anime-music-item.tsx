17fb94d09ad161e1e0b9100d7f607c3bd3e58c63
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-18 21:32:02 -0500

    refactor AnimeMusicItem component to enhance interactivity and playlist management features

--- src/components/anime-info/anime-music-item.tsx
+++ src/components/anime-info/anime-music-item.tsx
@@ -1,7 +1,14 @@
+import { navigate } from 'astro:transitions/client'
+import { AddToPlayList } from '@components/icons/add-to-play-list-icon'
+import { DeleteIcon } from '@components/icons/delete-icon'
+import { PauseIcon } from '@components/icons/pause-icon'
+import { PlayIcon } from '@components/icons/play-icon'
 import { Overlay } from '@components/overlay'
 import { Picture } from '@components/picture'
+import { useMusicPlayerStore } from '@store/music-player-store'
 import { normalizeString } from '@utils/normalize-string'
-import type { AnimeSong } from 'types'
+import { type PointerEvent, useEffect, useState } from 'react'
+import type { AnimeSong, AnimeSongWithImage } from 'types'
 
 /**
  * AnimeMusicItem component displays a music track item for an anime series.
@@ -42,11 +49,76 @@ export const AnimeMusicItem = ({
   song,
   image,
   placeholder,
+  banner_image,
+  anime_title,
 }: {
   song: AnimeSong
   image: string
   placeholder: string
+  banner_image: string
+  anime_title: string
 }) => {
+  const [heights, setHeights] = useState([0, 0, 0, 0])
+
+  const { isPlaying, currentSong, list, setList, playerRef, canPlay } =
+    useMusicPlayerStore()
+  const isInPlaylist = list.find(
+    (songList) => songList.song_id === song.song_id
+  )
+
+  const isCurrentSong = currentSong?.song_id === song.song_id
+  const handlePlay = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    isPlaying && canPlay
+      ? playerRef.current?.pause()
+      : playerRef.current?.play()
+  }
+
+  const handleClick = () => {
+    if (isCurrentSong) return
+    navigate(`/music/${normalizeString(song.song_title)}_${song.theme_id}`)
+  }
+
+  const handleAddTolist = (newSong: AnimeSongWithImage) => {
+    setList([...list, newSong])
+  }
+  const handleClickList = () => {
+    if (!isInPlaylist) {
+      const newSong = {
+        image,
+        placeholder,
+        banner_image,
+        anime_title,
+        ...song,
+      }
+      handleAddTolist(newSong)
+      return
+    }
+    const index = list.findIndex((item) => item.song_id === song.song_id)
+
+    handleRemoveTolist(index)
+  }
+  const handleRemoveTolist = (indexToDelete: number) => {
+    setList([...list].filter((_, index) => index !== indexToDelete))
+  }
+
+  useEffect(() => {
+    let interval: string | number | NodeJS.Timeout | undefined
+
+    if (isPlaying) {
+      interval = setInterval(() => {
+        const newHeights = Array.from(
+          { length: 4 },
+          () => Math.floor(Math.random() * 24) + 8
+        )
+        setHeights(newHeights)
+      }, 150)
+    } else {
+      setHeights([0, 0, 0, 0])
+    }
+
+    return () => clearInterval(interval)
+  }, [isPlaying])
   const getTypeColor = (type: string) => {
     switch (type?.toLowerCase()) {
       case 'opening':
@@ -62,36 +134,75 @@ export const AnimeMusicItem = ({
     }
   }
   return (
-    <a
-      href={`/music/${normalizeString(song.song_title)}_${song.theme_id}`}
+    <article
+      onClick={() => handleClick()}
       title={song.song_title}
-      className="hover:bg-Primary-900 border-enfasisColor relative group flex max-h-28 flex-row md:gap-2 overflow-hidden rounded-lg border-l-2 bg-zinc-800 transition-colors duration-300 ease-in-out"
+      className="hover:bg-Primary-900 cursor-pointer group border-enfasisColor group relative flex max-h-28 flex-row overflow-hidden rounded-lg border-l-2 bg-zinc-800 transition-colors duration-300 ease-in-out md:gap-2 items-start"
     >
       <Picture
         image={placeholder}
         styles="aspect-[225/330] w-full  overflow-hidden  relative max-w-20"
       >
+        {isCurrentSong && (
+          <div className="absolute inset-0 z-10 flex items-center gap-[3px] justify-center bg-Complementary/30">
+            {heights.map((height, index) => (
+              <div
+                key={index}
+                className="w-[3px] group-hover:opacity-0 bg-enfasisColor transition-all duration-150 ease-out rounded-md"
+                style={{ height: `${height}px` }}
+              />
+            ))}
+
+            <button
+              className="absolute inset-0 opacity-0 pointer-events-none z-20 flex p-4 cursor-pointer group-hover:pointer-events-auto group-hover:opacity-90 transition-all disabled:pointer-events-none  duration-150 w-full h-full items-center justify-center mx-auto"
+              onClick={(e) => handlePlay(e)}
+              disabled={!canPlay}
+            >
+              {isPlaying ? (
+                <PauseIcon className="w-6 h-6" />
+              ) : (
+                <PlayIcon className="w-6 h-6" />
+              )}
+            </button>
+          </div>
+        )}
+
         <img
           src={image}
           alt={song.song_title}
-          className="relative aspect-[225/330] h-full w-full  object-cover object-center transition-all ease-in-out group-hover:scale-105"
+          className="relative aspect-[225/330] h-full w-full object-cover object-center transition-all ease-in-out group-hover:scale-105"
         />
         <Overlay className="to-Primary-950/60 h-full w-full bg-gradient-to-b from-transparent" />
       </Picture>
 
       {song.type && (
         <span
-          className={`absolute top-2 right-2 flex-shrink-0 rounded-full border md:px-2 md:py-1 p-1 text-xs font-medium ${getTypeColor(song.type)}`}
+          className={`absolute top-2 right-2 flex-shrink-0 rounded-full border p-1 text-xs font-medium md:px-2 md:py-1 ${getTypeColor(song.type)}`}
         >
           {song.type.toUpperCase()}
         </span>
       )}
-      <footer className="flex flex-col gap-2 md:p-4 p-2 max-w-[60%] w-full">
-        <h3 className="md:text-lg text-md font-bold text-pretty  group-hover:text-enfasisColor/80 transition-colors duration-300 ease-in-out">
+
+      <button
+        className="absolute bottom-2 right-2 flex-shrink-0 p-2 cursor-pointer hover:bg-Primary-950 rounded-full transition-colors duration-300 z-10"
+        title={`Add to playlist ${song.song_title}`}
+        onClick={(e) => {
+          e.stopPropagation()
+          handleClickList()
+        }}
+      >
+        {isInPlaylist ? (
+          <DeleteIcon className="w-6 h-6" />
+        ) : (
+          <AddToPlayList className="w-6 h-6" />
+        )}
+      </button>
+      <footer className="flex w-full max-w-[60%] flex-col gap-2 p-2 md:p-4 items-start">
+        <h3 className="text-md group-hover:text-enfasisColor/80 font-bold text-pretty select-none transition-colors duration-300 ease-in-out md:text-lg">
           {song.song_title}
         </h3>
-        <p className="md:text-sm text-xs text-gray-500">{song.artist_name}</p>
+        <p className="text-xs text-gray-500 md:text-sm select-none">{song.artist_name}</p>
       </footer>
-    </a>
+    </article>
   )
 }
