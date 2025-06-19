7338e98750ea74a3dfc3e2d853412967c8a02a94
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-18 23:18:22 -0500

    fix: update ExpandIcon component to use correct SVG attributes and paths

--- src/components/icons/expand-icon.tsx
+++ src/components/icons/expand-icon.tsx
@@ -4,16 +4,20 @@ export const ExpandIcon = ({ className }: IconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
       className={className}
     >
-      <path stroke="none" d="M0 0h24v24H0z" />
-      <path d="M4 8V6a2 2 0 0 1 2-2h2M4 16v2a2 2 0 0 0 2 2h2M16 4h2a2 2 0 0 1 2 2v2M16 20h2a2 2 0 0 0 2-2v-2" />
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+      <path d="M11 13l9 -9" />
+      <path d="M15 4h5v5" />
     </svg>
   )
 }
