bbc16a88e31a4489cc8eea580467b5c33cace598
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-18 21:34:27 -0500

    add className to search section for full width styling

--- src/components/search/search-section.tsx
+++ src/components/search/search-section.tsx
@@ -124,7 +124,7 @@ export const SearchComponent = () => {
   }, [url])
 
   return (
-    <section id="search-section">
+    <section id="search-section" className='w-full'>
       <aside className="[grid-area:aside]">
         <FilterSection />
         <AppliedFiltersComponent />
