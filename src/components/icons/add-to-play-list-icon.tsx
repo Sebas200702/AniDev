489cff0fc10dbc50d994ecac11c196fa90d81bd6
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-18 21:32:40 -0500

    add AddToPlayList icon component for playlist functionality

---
+++ src/components/icons/add-to-play-list-icon.tsx
@@ -0,0 +1,18 @@
+import type { IconProps } from 'types'
+export const AddToPlayList = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M19 8H5M5 12h9M11 16H5M15 16h6M18 13v6" />
+    </svg>
+  )
+}
