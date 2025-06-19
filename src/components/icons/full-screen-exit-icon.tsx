e5c3f313975176bfe7d2e1dc24ceeb4fb14e5939
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-19 14:18:50 -0500

    fix: correct SVG attributes to use kebab-case in FullScreenExitIcon component

--- src/components/icons/full-screen-exit-icon.tsx
+++ src/components/icons/full-screen-exit-icon.tsx
@@ -6,13 +6,13 @@ export const FullScreenExitIcon = ({ className }: IconProps) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
       className={className}
     >
       <path stroke="none" d="M0 0h24v24H0z" />
-      <path d="M5 9h4V5M3 3l6 6M5 15h4v4M3 21l6-6M19 9h-4V5M15 9l6-6M19 15h-4v4M15 15l6 6" />
+      <path d="M15 19v-2a2 2 0 0 1 2-2h2M15 5v2a2 2 0 0 0 2 2h2M5 15h2a2 2 0 0 1 2 2v2M5 9h2a2 2 0 0 0 2-2V5" />
     </svg>
   )
 }
