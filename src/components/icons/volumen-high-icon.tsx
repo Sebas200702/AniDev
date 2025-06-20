009ba8383e391807827f538a4b0fb77294523d44
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-20 16:04:00 -0500

    fix: standardize stroke properties in VolumeHighIcon component

--- src/components/icons/volumen-high-icon.tsx
+++ src/components/icons/volumen-high-icon.tsx
@@ -3,14 +3,12 @@ export const VolumeHighIcon = ({ className }: IconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={className}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
