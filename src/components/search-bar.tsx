55b39f701b75169f586fd991857d52f7c43d715d
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-20 09:39:53 -0500

    feat: integrate filter dropdown for search type selection and enhance API query handling

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,5 +1,6 @@
 import { navigate } from 'astro:transitions/client'
 import { AnimeDetailCard } from '@components/anime-detail-card'
+import { FilterDropdown } from '@components/search/filters/filter-dropdown'
 import { useDebounce } from '@hooks/useDebounce'
 import { useFetch } from '@hooks/useFetch'
 import { useShortcuts } from '@hooks/useShortCuts'
@@ -12,8 +13,9 @@ import { loadSearchHistory } from '@utils/load-search-history'
 import { normalizeString } from '@utils/normalize-string'
 import { saveSearchHistory } from '@utils/save-search-history'
 import { useCallback, useEffect, useMemo, useRef } from 'react'
-import { ToastType, shortCuts } from 'types'
-import type { AnimeCardInfo, AnimeDetail } from 'types'
+import { ToastType, shortCuts, type AnimeSongWithImage } from 'types'
+import { type AnimeCardInfo, type AnimeDetail, typeSearchOptions } from 'types'
+import { AnimeMusicItem } from '@components/anime-info/anime-music-item'
 
 export const SearchBar = () => {
   const {
@@ -29,7 +31,9 @@ export const SearchBar = () => {
     setTotalResults,
     addSearchHistory,
     setSearchHistoryIsOpen,
+    setType,
     searchHistory,
+    type,
     clearSearchHistory,
     setSearchHistory,
   } = useSearchStoreResults()
@@ -44,11 +48,15 @@ export const SearchBar = () => {
   )
 
   const url = useMemo(() => {
-    const baseQuery = `/api/animes?limit_count=30&banners_filter=false&format=search&parental_control=${parentalControl}`
+    const defautlFilters =
+      type === 'animes'
+        ? 'limit_count=30&banners_filter=false&format=search&parental_control=false'
+        : 'limit_count=30'
+    const baseQuery = `/api/${type}?${defautlFilters}`
     const searchQuery = debouncedQuery ? `&search_query=${debouncedQuery}` : ''
     const filterQuery = filtersToApply ? `&${filtersToApply}` : ''
     return `${baseQuery}${searchQuery}${filterQuery}`
-  }, [debouncedQuery, filtersToApply, parentalControl])
+  }, [debouncedQuery, filtersToApply, parentalControl, type])
 
   const actionMap = {
     'close-search': () => setSearchIsOpen(false),
@@ -93,11 +101,11 @@ export const SearchBar = () => {
   useShortcuts(shortCuts, actionMap)
 
   const {
-    data: animes,
+    data,
     loading: isLoading,
     total,
     error: fetchError,
-  } = useFetch<AnimeCardInfo[]>({
+  } = useFetch<AnimeCardInfo[] | AnimeSongWithImage[]>({
     url,
     skip: !url || (!filtersToApply && !debouncedQuery),
   })
@@ -105,7 +113,7 @@ export const SearchBar = () => {
     data: animesFull,
     loading: isLoadingFull,
     error: fetchErrorFull,
-  } = useFetch<AnimeDetail[]>({
+  } = useFetch<AnimeDetail[] | AnimeSongWithImage[]>({
     url: `${url.replace('format=search', 'format=anime-detail').replace('30', '7')}`,
     skip: !url || (!filtersToApply && !debouncedQuery),
   })
@@ -152,12 +160,12 @@ export const SearchBar = () => {
     [setQuery, setLoading]
   )
   useEffect(() => {
-    if (animes?.length === 0 && (query || appliedFilters) && !isLoading) {
+    if (data?.length === 0 && (query || appliedFilters) && !isLoading) {
       toast[ToastType.Warning]({
         text: `No results found for "${query}" with the selected filters.`,
       })
     }
-  }, [animes, appliedFilters, isLoading])
+  }, [data, appliedFilters, isLoading])
 
   useEffect(() => {
     if (!query) {
@@ -167,8 +175,8 @@ export const SearchBar = () => {
 
   useEffect(() => {
     setLoading(isLoading)
-    if (isLoading || (!query && !appliedFilters) || !animes) return
-    setResults(animes, false, fetchError)
+    if (isLoading || (!query && !appliedFilters) || !data) return
+    setResults(data, false, fetchError)
     setTotalResults(total)
     const newHistory = {
       query,
@@ -177,7 +185,7 @@ export const SearchBar = () => {
     }
     addSearchHistory(newHistory)
   }, [
-    animes,
+    data,
     isLoading,
     fetchError,
     setResults,
@@ -197,12 +205,15 @@ export const SearchBar = () => {
     saveSearchHistory(searchHistory, userInfo)
   }, [searchHistory, trackSearchHistory, query, appliedFilters])
 
+
   useEffect(() => {
     if (!trackSearchHistory || (!query && !appliedFilters)) return
     loadSearchHistory(userInfo).then((history) => {
       setSearchHistory(history)
     })
   }, [trackSearchHistory, query, appliedFilters])
+
+
 
   return (
     <div
@@ -215,7 +226,7 @@ export const SearchBar = () => {
         id="search-bar"
         role="search"
         onSubmit={handleSubmit}
-        className="relative mt-24 flex w-full max-w-xl flex-col gap-6 overflow-hidden shadow-lg"
+        className="relative mt-24 flex w-full max-w-xl flex-col gap-6  shadow-lg"
       >
         <header className="flex items-center justify-between">
           <div className="hidden gap-4 text-gray-300 select-none md:flex">
@@ -231,6 +242,18 @@ export const SearchBar = () => {
               applied
             </strong>
           )}
+
+          <FilterDropdown
+            options={typeSearchOptions}
+            label="Type"
+            values={[type]}
+            onChange={(values) => {
+              setType(values[0] as 'animes' | 'music')
+            }}
+            onClear={() => {}}
+            styles="max-w-32"
+            singleSelect={true}
+          />
         </header>
 
         <div className="bg-Primary-950 flex items-center rounded-md px-4 py-2">
@@ -268,7 +291,7 @@ export const SearchBar = () => {
       </form>
 
       <ul
-        className={`no-scrollbar max-h-96 w-full max-w-xl overflow-y-auto transition-all duration-300 ${isLoading || results ? 'h-full opacity-100' : 'h-0 opacity-0'} bg-Primary-950 flex flex-col gap-4 rounded-md p-4 shadow-lg`}
+        className={`no-scrollbar max-h-96 w-full max-w-xl overflow-y-auto transition-all duration-300 ${(isLoading || results) && query ? 'h-full opacity-100' : 'h-0 opacity-0'} bg-Primary-950 flex flex-col gap-4 rounded-md p-4 shadow-lg`}
       >
         {isLoadingFull &&
           Array.from({ length: 7 }, (_, i) => (
@@ -286,16 +309,26 @@ export const SearchBar = () => {
           </div>
         )}
 
-        {!isLoading &&
-          animesFull?.map((result) => (
+        {!isLoading && type === 'animes' ? (
+          (animesFull as AnimeDetail[])?.map((result) => (
             <AnimeDetailCard
               key={result.mal_id}
-              anime={{
-                ...result,
-              }}
+              anime={result}
             />
-          ))}
-        {results && results?.length > 7 && !isLoading && !isLoadingFull && (
+          ))
+        ) : (
+          (animesFull as AnimeSongWithImage[])?.map((result) => (
+            <AnimeMusicItem
+              key={result.song_id}
+              song={result}
+              image={result.image}
+              placeholder={result.placeholder}
+              banner_image={result.banner_image}
+              anime_title={result.anime_title}
+            />
+          ))
+        )}
+        {results && results?.length > 7 && !isLoading && !isLoadingFull && query && (
           <a
             href={`/search?q=${encodeURIComponent(query)}`}
             onClick={() => setSearchIsOpen(false)}
