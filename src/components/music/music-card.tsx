d9522332d6c5177e4f0205f354413d77bb2058f0
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-20 12:21:14 -0500

    feat: reorganize imports and enhance MusicCard component layout

--- src/components/music/music-card.tsx
+++ src/components/music/music-card.tsx
@@ -1,9 +1,10 @@
-import { PauseIcon } from '@components/icons/pause-icon'
-import { PlayIcon } from '@components/icons/play-icon'
+import { navigate } from 'astro:transitions/client'
 import { Picture } from '@components/picture'
+import { MoreOptionsIcon } from '@icons/more-options-icon'
+import { PauseIcon } from '@icons/pause-icon'
+import { PlayIcon } from '@icons/play-icon'
 import { useMusicPlayerStore } from '@store/music-player-store'
 import { normalizeString } from '@utils/normalize-string'
-import { navigate } from 'astro:transitions/client'
 import type { AnimeSongWithImage } from 'types'
 
 export const MusicCard = ({ song }: { song: AnimeSongWithImage }) => {
@@ -19,45 +20,49 @@ export const MusicCard = ({ song }: { song: AnimeSongWithImage }) => {
     isPlaying ? playerRef.current?.pause() : playerRef.current?.play()
   }
   return (
-    <article className="group relative bg-Complementary hover:bg-zinc-800 rounded-lg p-4 transition-all duration-300 hover:shadow-xl aspect-[225/330]"
-    onClick={() => navigate(`/music/${normalizeString(song.song_title)}_${song.theme_id}`)}
+    <article
+      className="group relative bg-Complementary hover:bg-zinc-800 rounded-lg p-4 transition-all duration-300 hover:shadow-xl aspect-[225/330]"
+      onClick={() =>
+        navigate(`/music/${normalizeString(song.song_title)}_${song.theme_id}`)
+      }
     >
+      <div className="relative mb-4">
+        <Picture
+          image={song.placeholder}
+          styles="relative aspect-square rounded-md overflow-hidden shadow-lg aspect-square"
+        >
+          <img
+            src={song.image}
+            alt={song.song_title}
+            className="w-full  object-cover transition-transform duration-300 aspect-square rounded-md"
+          />
+        </Picture>
 
-        <div className="relative mb-4">
-          <Picture
-            image={song.placeholder}
-            styles="relative aspect-square rounded-md overflow-hidden shadow-lg aspect-square"
+        <div className="absolute bottom-2 right-2 opacity-0 group-hover:opacity-100 transform translate-y-2 group-hover:translate-y-0 transition-all duration-300 z-20">
+          <button
+            className="bg-enfasisColor cursor-pointer text-Primary-50 rounded-full p-3 shadow-2xl hover:scale-105 transition-all duration-200"
+            onClick={(e) => handleClick(e)}
           >
-            <img
-              src={song.image}
-              alt={song.song_title}
-              className="w-full  object-cover transition-transform duration-300 aspect-square rounded-md"
-            />
-          </Picture>
-
-          <div className="absolute bottom-2 right-2 opacity-0 group-hover:opacity-100 transform translate-y-2 group-hover:translate-y-0 transition-all duration-300 z-20">
-            <button
-              className="bg-enfasisColor cursor-pointer text-Primary-50 rounded-full p-3 shadow-2xl hover:scale-105 transition-all duration-200"
-              onClick={(e) => handleClick(e)}
-            >
-              {isPlaying && currentSong?.song_id === song.song_id ? (
-                <PauseIcon className="w-5 h-5 " />
-              ) : (
-                <PlayIcon className="w-5 h-5 " />
-              )}
-            </button>
-          </div>
+            {isPlaying && currentSong?.song_id === song.song_id ? (
+              <PauseIcon className="w-5 h-5 " />
+            ) : (
+              <PlayIcon className="w-5 h-5 " />
+            )}
+          </button>
         </div>
+      </div>
 
-        <footer className="space-y-1 mt-2">
-          <h3 className="font-semibold text-white text-lg line-clamp-2 group-hover:underline">
-            {song.song_title}
-          </h3>
-          <p className="text-neutral-400 text-sm line-clamp-1">
-            {song.artist_name || 'Unknown Artist'}
-          </p>
-        </footer>
-
+      <footer className="space-y-2 mt-2 w-full">
+        <h3 className="font-semibold text-white text-lg line-clamp-2 group-hover:underline">
+          {song.song_title}
+        </h3>
+        <p className="text-neutral-400 text-sm line-clamp-1">
+          {song.artist_name || 'Unknown Artist'}
+        </p>
+        <button className='flex justify-end'>
+          <MoreOptionsIcon className='h-4 w-4' />
+        </button>
+      </footer>
     </article>
   )
 }
