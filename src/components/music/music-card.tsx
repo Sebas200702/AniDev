2731bc4a80025593056e2fb13043cc7ea6c2f65f
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-20 16:04:28 -0500

    feat: add DownloadButton and AddToPlayListButton to MusicCard for enhanced song management

--- src/components/music/music-card.tsx
+++ src/components/music/music-card.tsx
@@ -1,15 +1,24 @@
 import { navigate } from 'astro:transitions/client'
+import { DownloadButton } from '@components/dowload-button'
+import { AddToPlayListButton } from '@components/buttons/add-to-playlist-button'
 import { Picture } from '@components/picture'
 import { MoreOptionsIcon } from '@icons/more-options-icon'
 import { PauseIcon } from '@icons/pause-icon'
 import { PlayIcon } from '@icons/play-icon'
 import { useMusicPlayerStore } from '@store/music-player-store'
 import { normalizeString } from '@utils/normalize-string'
+import { useRef } from 'react'
+import { useEffect } from 'react'
 import type { AnimeSongWithImage } from 'types'
 
 export const MusicCard = ({ song }: { song: AnimeSongWithImage }) => {
-  const { currentSong, setCurrentSong, playerRef, isPlaying } =
+  const { currentSong, setCurrentSong, playerRef, isPlaying , list } =
     useMusicPlayerStore()
+
+  const menuRef = useRef<HTMLDivElement>(null)
+  const isInPlaylist = list.some(
+    (songList) => songList.song_id === song.song_id
+  )
 
   const handleClick = (e: React.MouseEvent) => {
     e.stopPropagation()
@@ -18,6 +27,23 @@ export const MusicCard = ({ song }: { song: AnimeSongWithImage }) => {
     }
 
     isPlaying ? playerRef.current?.pause() : playerRef.current?.play()
+  }
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
+        menuRef.current.classList.replace('flex', 'hidden')
+      }
+    }
+
+    document.addEventListener('mousedown', handleOutsideClick)
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick)
+    }
+  }, [])
+
+  const handleMenuClick = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    menuRef.current?.classList.replace('hidden', 'flex')
   }
   return (
     <article
@@ -59,10 +85,31 @@ export const MusicCard = ({ song }: { song: AnimeSongWithImage }) => {
         <p className="text-neutral-400 text-sm line-clamp-1">
           {song.artist_name || 'Unknown Artist'}
         </p>
-        <button className='flex justify-end'>
-          <MoreOptionsIcon className='h-4 w-4' />
+        <button
+          className="absolute right-3 bottom-2 p-2 cursor-poiter z-10"
+          onClick={(e) => handleMenuClick(e)}
+        >
+          <MoreOptionsIcon className="h-4 w-4 " />
         </button>
       </footer>
+
+      <div
+        ref={menuRef}
+        className="absolute z-50 bg-zinc-900/95 backdrop-blur-md shadow-2xl border border-zinc-700/50 rounded-lg min-w-[180px] max-w-[220px] p-1 right-8 translate-y-1/2 translate-x-full flex-col hidden transition-all duration-200 ease-out"
+      >
+
+        <AddToPlayListButton song={song} isInPlayList={isInPlaylist} clasName='w-full flex items-center gap-3 hover:text-enfasisColor px-3 py-2.5 text-sm hover:bg-zinc-800/80 rounded-md transition-all duration-150 group' label={`${!isInPlaylist ? 'Add' : 'Remove'} `}/>
+        <DownloadButton
+          styles="w-full flex items-center gap-3 hover:text-enfasisColor px-3 py-2.5 text-sm hover:bg-zinc-800/80 rounded-md transition-all duration-150 group"
+          url={song.audio_url}
+          title={song.song_title}
+          metadata={{
+            type: 'audio',
+            coverUrl: song.image,
+            artist: song.artist_name ?? 'Unknown Artist',
+          }}
+        />
+      </div>
     </article>
   )
 }
