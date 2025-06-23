5617c2533f93801e9b7b736b7b825a55fb6c3aa0
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-23 10:11:12 -0500

    fix metadata error

---
+++ src/utils/sycronize-player-metadata.ts
@@ -0,0 +1,10 @@
+import { baseTitle } from '@utils/base-url'
+
+interface Props {
+  url: string
+  title: string
+}
+export const SyncronizePlayerMetadata = ({ url, title }: Props) => {
+  window.history.replaceState(null, '', url)
+  document.title = `${title}e} - ${baseTitle}`
+}
