1821f1c1a52abd9756443b5984c42efd94fbf677
Sebas200702 <sebastorregroza6@gmail.com>
2025-06-23 08:13:45 -0500

    Add CheckIcon component for SVG checkmark icon

---
+++ src/components/icons/check-icon.tsx
@@ -0,0 +1,18 @@
+import type { IconProps } from 'types'
+export const CheckIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M5 12l5 5l10 -10" />
+    </svg>
+  )
+}
