5d6310db0f4c67922df2d05642bad82c37f07824
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-15 10:31:26 -0500

    Add gradient overlay to AnimeInfo component for improved visual depth

--- src/components/anime-info/anime.tsx
+++ src/components/anime-info/anime.tsx
@@ -69,6 +69,7 @@ export const AnimeInfo = ({ slug }: Props) => {
       />
 
       <article className="relative z-10 mb-10 grid grid-cols-1 gap-10 px-4 pt-[35dvh] md:mb-20 md:grid-cols-3 md:gap-15 md:px-20 xl:grid-cols-5">
+        <Overlay className="to-Primary-950 via-Primary-950/20 absolute inset-0 bg-gradient-to-l via-60%" />
         <Overlay className="to-Primary-950/100 via-Primary-950 z-0 h-full w-full bg-gradient-to-b via-[38dvh] md:via-[55dvh]" />
         <AnimeAside
           animeData={animeData}
