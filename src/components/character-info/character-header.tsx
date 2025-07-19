afce6ccdabbe8153405d5ac51ae172a4ac4756a4
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-19 15:28:56 -0500

    Refactor CharacterHeader component to improve layout and readability of character information

--- src/components/character-info/character-header.tsx
+++ src/components/character-info/character-header.tsx
@@ -6,12 +6,18 @@ interface Props {
 
 export const CharacterHeader = ({ character }: Props) => {
   return (
-    <header className="character-header row-span-2 z-20 flex h-full w-full flex-col gap-4 md:col-span-2 xl:col-span-4">
+    <header className="character-header z-20 row-span-2 flex h-full w-full flex-col gap-4 md:col-span-2 xl:col-span-4">
       <h1 className="title text-white">{character.character_name}</h1>
-      <h2 className="text-l text-Primary-200">{character.character_name_kanji}</h2>
+      <h2 className="text-l text-Primary-200">
+        {character.character_name_kanji}
+      </h2>
       <h3>Nicknames</h3>
-      <p className="text-m max-w-[80ch] text-Primary-200">{character.character_nicknames?.join(', ')}</p>
-      <p className="text-m max-w-[80ch] text-Primary-200">{character.character_about}</p>
+      <p className="text-m text-Primary-200 max-w-[80ch]">
+        {character.character_nicknames?.join(', ')}
+      </p>
+      <p className="text-m text-Primary-200 max-w-[80ch]">
+        {character.character_about}
+      </p>
     </header>
   )
 }
