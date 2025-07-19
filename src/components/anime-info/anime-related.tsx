6479dc5da2929927dabca3baab2636b4c1f4fd85
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-19 15:13:08 -0500

    Refactor AnimeRelated component to enhance loading state and update anime card rendering
    This update modifies the AnimeRelated component by replacing the AnimeCard with AnimeDetailCard for improved detail presentation. The loading state layout has been adjusted for better responsiveness, and class names have been updated for consistency and readability.

--- src/components/anime-info/anime-related.tsx
+++ src/components/anime-info/anime-related.tsx
@@ -1,7 +1,7 @@
-import { AnimeCard } from '@components/anime-card'
+import { AnimeDetailCard } from '@components/anime-detail-card'
 import { LoadingCard } from '@components/search/results/loading-card'
 import { useFetch } from '@hooks/useFetch'
-import type { AnimeCardInfo } from 'types'
+import type { AnimeDetail } from 'types'
 
 /**
  * AnimeRelated component displays a grid of related anime recommendations.
@@ -32,37 +32,38 @@ import type { AnimeCardInfo } from 'types'
  */
 
 export const AnimeRelated = ({ animeId }: { animeId: number }) => {
-  const { data, loading } = useFetch<AnimeCardInfo[]>({
+  const { data, loading } = useFetch<AnimeDetail[]>({
     url: `/api/getAnimeRelations?animeId=${animeId}`,
   })
 
   if (loading)
     return (
-      <div className="bg-Complementary relative z-10 flex flex-col gap-4 rounded-lg p-4">
-        <header className="flex items-center justify-between px-10">
-          <h2 className="text-2xl font-bold">Related</h2>
+      <div className="relative z-10 flex flex-col gap-4 rounded-lg">
+        <header className="flex items-center justify-between">
+          <h2 className="text-lxx font-bold">Related</h2>
         </header>
-        <ul className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
-          {Array.from({ length: 4 }).map((_, index) => (
-            <li key={index}>
-              <LoadingCard />
-            </li>
+        <ul className="grid grid-cols-1 gap-4 md:gap-6 xl:grid-cols-2">
+          {Array.from({ length: 4 }).map((_, i) => (
+            <div
+              key={i + 1}
+              className="mx-auto flex aspect-[100/30] h-full w-full animate-pulse flex-row rounded-lg bg-zinc-800 md:max-h-36"
+            >
+              <div className="aspect-[225/330] h-full animate-pulse rounded-l-lg bg-zinc-700 object-cover object-center transition-all ease-in-out md:max-h-36"></div>
+            </div>
           ))}
         </ul>
       </div>
     )
 
   return (
-    <section className="bg-Complementary relative z-10 flex flex-col gap-4 rounded-lg p-4">
-      <header className="flex items-center justify-between px-10">
-        <h2 className="text-2xl font-bold">Related</h2>
+    <section className="relative z-10 flex flex-col gap-4 rounded-lg">
+      <header className="flex items-center justify-between">
+        <h2 className="text-lxx font-bold">Related</h2>
       </header>
 
-      <ul className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
+      <ul className="grid grid-cols-1 gap-4 md:gap-6 xl:grid-cols-2">
         {data?.map((anime) => (
-          <li key={anime.mal_id}>
-            <AnimeCard anime={anime} />
-          </li>
+          <AnimeDetailCard anime={anime} key={anime.mal_id} />
         ))}
       </ul>
     </section>
