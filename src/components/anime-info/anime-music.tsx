47d5d221cbc74883305d357beb297320b030f6a4
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-19 15:12:40 -0500

    Refactor AnimeMusic component to enhance layout and improve loading state presentation
    This update modifies the AnimeMusic component by adjusting the layout to use a grid system for better responsiveness and visual structure. The loading state has been improved with updated class names and aspect ratios for a more polished appearance.

--- src/components/anime-info/anime-music.tsx
+++ src/components/anime-info/anime-music.tsx
@@ -92,15 +92,15 @@ export const AnimeMusic = ({
 
   if (loading)
     return (
-      <div className="bg-Complementary z-10 flex flex-col gap-4 rounded-lg p-4">
-        <h2 className="text-2xl font-bold">Anime Music</h2>
-        <ul className="flex flex-col gap-4">
+      <div className="z-10 flex flex-col gap-8 rounded-lg">
+        <h2 className="text-lxx font-bold">Anime Music</h2>
+        <ul className="grid grid-cols-1 gap-4 md:gap-6 xl:grid-cols-2">
           {Array.from({ length: 4 }, (_, i) => (
             <div
               key={i + 1}
-              className="mx-auto flex aspect-[100/28] h-full w-full animate-pulse flex-row rounded-lg bg-zinc-700 md:max-h-36"
+              className="mx-auto flex aspect-[100/30] h-full w-full animate-pulse flex-row rounded-lg bg-zinc-800 md:max-h-36"
             >
-              <div className="aspect-[225/330] h-full animate-pulse rounded-l-lg bg-zinc-800 object-cover object-center transition-all ease-in-out md:max-h-36"></div>
+              <div className="aspect-[225/330] h-full animate-pulse rounded-l-lg bg-zinc-700 object-cover object-center transition-all ease-in-out md:max-h-36"></div>
             </div>
           ))}
         </ul>
@@ -108,9 +108,9 @@ export const AnimeMusic = ({
     )
   if (error) return <div>Error: {error}</div>
   return (
-    <div className="bg-Complementary z-10 flex flex-col gap-4 rounded-lg p-4">
-      <h2 className="text-2xl font-bold">Anime Music</h2>
-      <ul className="flex flex-col gap-4">
+    <div className="z-10 flex flex-col gap-8 rounded-lg">
+      <h2 className="text-lxx font-bold">Anime Music</h2>
+      <ul className="grid grid-cols-1 gap-4 md:gap-6 xl:grid-cols-2">
         {songs.length === 0 && !loading && (
           <div className="text-center text-2xl font-bold">No music found</div>
         )}
