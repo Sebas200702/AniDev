8ac1725b36aa64aa6a2e48e8bda0e335aceaab32
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-19 15:29:57 -0500

    Introduce ExpandIconV2 component with enhanced SVG properties and remove fixed dimensions from ExpandIcon

--- src/components/icons/expand-icon.tsx
+++ src/components/icons/expand-icon.tsx
@@ -4,8 +4,6 @@ export const ExpandIcon = ({ className }: IconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -18,6 +16,24 @@ export const ExpandIcon = ({ className }: IconProps) => {
       <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
       <path d="M11 13l9 -9" />
       <path d="M15 4h5v5" />
+    </svg>
+  )
+}
+
+export const ExpandIconV2 = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M16 4h4v4M14 10l6-6M8 20H4v-4M4 20l6-6M16 20h4v-4M14 14l6 6M8 4H4v4M4 4l6 6" />
     </svg>
   )
 }
