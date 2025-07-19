32caee73a731d897b7a8571bc997055fe5ac59fb
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-19 18:56:30 -0500

    Update AnimeMusic component to increase loading placeholders and add padding for improved layout

--- src/components/anime-info/anime-music.tsx
+++ src/components/anime-info/anime-music.tsx
@@ -92,10 +92,10 @@ export const AnimeMusic = ({
 
   if (loading)
     return (
-      <div className="z-10 flex flex-col gap-8 rounded-lg">
+      <div className="z-10 flex flex-col gap-8 rounded-lg p-4 md:p-6">
         <h2 className="text-lxx font-bold">Anime Music</h2>
         <ul className="grid grid-cols-1 gap-4 md:gap-6 xl:grid-cols-2">
-          {Array.from({ length: 4 }, (_, i) => (
+          {Array.from({ length: 8 }, (_, i) => (
             <div
               key={i + 1}
               className="mx-auto flex aspect-[100/30] h-full w-full animate-pulse flex-row rounded-lg bg-zinc-800 md:max-h-36"
@@ -108,7 +108,7 @@ export const AnimeMusic = ({
     )
   if (error) return <div>Error: {error}</div>
   return (
-    <div className="z-10 flex flex-col gap-8 rounded-lg">
+    <div className="z-10 flex flex-col gap-8 rounded-lg p-4 md:p-6">
       <h2 className="text-lxx font-bold">Anime Music</h2>
       <ul className="grid grid-cols-1 gap-4 md:gap-6 xl:grid-cols-2">
         {songs.length === 0 && !loading && (
