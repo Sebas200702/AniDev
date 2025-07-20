d27c1362d92ae3e99fb178178d53249537ea3296
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-19 19:20:31 -0500

    Add RotateLeftIcon component for enhanced UI functionality
    This update introduces a new RotateLeftIcon component, providing a customizable SVG icon for use in the application. The component accepts a className prop for styling, improving the overall user interface and interaction options.

---
+++ src/components/icons/rotate-left-icon.tsx
@@ -0,0 +1,21 @@
+interface IconProps {
+  className?: string
+}
+
+export const RotateLeftIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M19.95 11a8 8 0 1 0 -.5 4m.5 5v-5h-5" />
+    </svg>
+  )
+}
