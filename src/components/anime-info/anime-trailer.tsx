f4f69336a7fe84fc72b2e7abf42fa862d7727190
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-19 19:07:40 -0500

    Replace Lite YouTube integration with Vidsstack MediaPlayer for anime trailer playback. This update includes new styles and a custom layout, enhancing the video display and user experience.

--- src/components/anime-info/anime-trailer.tsx
+++ src/components/anime-info/anime-trailer.tsx
@@ -1,4 +1,8 @@
-import { useEffect, useState } from 'react'
+import '@vidstack/react/player/styles/default/theme.css'
+import '@styles/video.css'
+import '@styles/player.css'
+import { CustomLayout } from '@components/music-player/custom-layout'
+import { MediaPlayer, MediaProvider, Poster } from '@vidstack/react'
 
 import { Picture } from '@components/picture'
 import { baseUrl } from '@utils/base-url'
@@ -56,36 +60,25 @@ export const AnimeTrailer = ({
   image_large_webp,
   title,
 }: Props) => {
-  const [isLiteYouTubeLoaded, setIsLiteYouTubeLoaded] = useState(false)
   const videoId = trailer_url?.split('v=')[1]?.split('&')[0]
-
-  useEffect(() => {
-    if (typeof window !== 'undefined' && trailer_url) {
-      import('@justinribeiro/lite-youtube').then((module) => {
-        const LiteYouTube = (module as any).default
-
-        if (!customElements.get('lite-youtube')) {
-          customElements.define('lite-youtube', LiteYouTube)
-        }
-        setIsLiteYouTubeLoaded(true)
-      })
-    }
-  }, [trailer_url])
 
   return (
     <>
       {trailer_url ? (
-        <div className="relative z-10 flex h-min w-full flex-col">
-          {isLiteYouTubeLoaded && videoId ? (
-            <lite-youtube
-              videoid={videoId}
-              style={{ aspectRatio: '16/9' }}
-              class="w-full rounded-lg"
-            />
-          ) : (
-            <div className="aspect-video w-full animate-pulse rounded-sm bg-zinc-700" />
-          )}
-        </div>
+        <MediaPlayer
+          src={trailer_url}
+          aspectRatio="16/9"
+          viewType="video"
+          streamType="on-demand"
+          logLevel="error"
+          playsInline
+          title={`${title} Trailer`}
+        >
+          <MediaProvider>
+            <Poster className="vds-poster" />
+          </MediaProvider>
+          <CustomLayout />
+        </MediaPlayer>
       ) : (
         <div className="flex w-full flex-col rounded-md md:max-w-5xl">
           <Picture
