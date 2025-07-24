2a7b6046dcc8c9a8478af50107b8d3ed466c4064
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-24 10:44:37 -0500

    Add WarningIcon component for improved user feedback. Introduced a new SVG icon to enhance visual representation of warning messages in the interface.

---
+++ src/components/icons/warning-icon.tsx
@@ -0,0 +1,19 @@
+import type { IconProps } from 'types'
+
+export const WarningIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9-9 9-9-1.8-9-9 1.8-9 9-9zM12 8v4M12 16h.01" />
+    </svg>
+  )
+}
