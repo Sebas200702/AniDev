b3b044fed30c9c1e68eed7570318fc491344990b
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-24 10:45:10 -0500

    Enhance AnimeMusicItem component with drag-and-drop functionality and improved UI elements. Added ShareButton and MoreOptions for better user interaction, and refactored layout for clearer presentation of song details.

--- src/components/music/anime-music-item.tsx
+++ src/components/music/anime-music-item.tsx
@@ -1,11 +1,17 @@
 import { navigate } from 'astro:transitions/client'
 import { AddToPlayListButton } from '@components/buttons/add-to-playlist-button'
+import { ShareButton } from '@components/buttons/share-button'
 import { PauseIcon } from '@components/icons/pause-icon'
 import { PlayIcon } from '@components/icons/play-icon'
+import { MoreOptions } from '@components/more-options'
 import { Overlay } from '@components/overlay'
 import { Picture } from '@components/picture'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 import { useMusicPlayerStore } from '@store/music-player-store'
 import { useSearchStoreResults } from '@store/search-results-store'
+import { createImageUrlProxy } from '@utils/create-imageurl-proxy'
+import { getTypeMusicColor } from '@utils/get-type-music-color'
 import { normalizeString } from '@utils/normalize-string'
 import { useEffect, useState } from 'react'
 import type { AnimeSong } from 'types'
@@ -16,18 +22,43 @@ export const AnimeMusicItem = ({
   placeholder,
   banner_image,
   anime_title,
+  isInMusicPlayer = false,
 }: {
   song: AnimeSong
   image: string
   placeholder: string
   banner_image: string
   anime_title: string
+  isInMusicPlayer?: boolean
 }) => {
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transition,
+    transform,
+    isDragging,
+  } = useSortable({ id: song.song_id })
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    opacity: isDragging ? 0.9 : 1,
+  }
+  const handleProps = { ...attributes, ...listeners }
+
   const [heights, setHeights] = useState([0, 0, 0, 0])
   const { setSearchIsOpen } = useSearchStoreResults()
 
-  const { isPlaying, currentSong, list, playerRef, canPlay, isMinimized } =
-    useMusicPlayerStore()
+  const {
+    isPlaying,
+    currentSong,
+    list,
+    playerRef,
+    canPlay,
+    isMinimized,
+    setCurrentSong,
+  } = useMusicPlayerStore()
   const isInPlaylist = list.some(
     (songList) => songList.song_id === song.song_id
   )
@@ -43,6 +74,13 @@ export const AnimeMusicItem = ({
   const handleClick = () => {
     if (isCurrentSong && isMinimized) return
     setSearchIsOpen(false)
+    setCurrentSong({
+      ...song,
+      image,
+      placeholder,
+      banner_image,
+      anime_title,
+    })
     navigate(`/music/${normalizeString(song.song_title)}_${song.theme_id}`)
   }
 
@@ -64,27 +102,43 @@ export const AnimeMusicItem = ({
     return () => clearInterval(interval)
   }, [isPlaying])
 
-  const getTypeColor = (type: string) => {
-    switch (type?.toLowerCase()) {
-      case 'opening':
-      case 'op':
-        return 'bg-blue-500/20 text-blue-300 border-blue-500/30'
-      case 'ending':
-      case 'ed':
-        return 'bg-purple-500/20 text-purple-300 border-purple-500/30'
-      case 'insert':
-        return 'bg-green-500/20 text-green-300 border-green-500/30'
-      default:
-        return 'bg-gray-500/20 text-gray-300 border-gray-500/30'
-    }
-  }
-
   return (
     <article
+      ref={setNodeRef}
+      style={style}
+      className={`relative transition-all duration-200 ease-out ${isDragging ? 'z-50' : ''} hover:bg-Primary-900 group group border-enfasisColor group relative flex aspect-[100/30] h-full w-full cursor-pointer flex-row items-start rounded-lg border-l-4 bg-zinc-800 transition-all duration-300 ease-in-out md:max-h-36 md:gap-2 md:hover:translate-x-1 overflow-hidden`}
       onClick={handleClick}
       title={song.song_title}
-      className="hover:bg-Primary-900 group group border-enfasisColor group relative flex aspect-[100/30] h-full w-full cursor-pointer flex-row items-start rounded-lg border-l-2 bg-zinc-800 transition-all duration-300 ease-in-out md:max-h-36 md:gap-2 md:hover:translate-x-2"
     >
+      <div className="absolute top-0 left-0 h-full w-full">
+        <img
+          src={createImageUrlProxy(banner_image || image, '100', '70', 'webp')}
+          alt={song.song_title}
+          className="h-full w-full object-cover object-center"
+        />
+        <Overlay className="bg-Primary-950/90 h-full w-full backdrop-blur-sm" />
+      </div>
+      {isInMusicPlayer && (
+        <div
+          {...handleProps}
+          className="bg-enfasisColor/50 absolute top-1/2 right-0 z-10 flex h-full -translate-y-1/2 cursor-grab touch-none items-center justify-center rounded-r-md p-1 shadow-lg backdrop-blur-sm transition-all duration-200 select-none active:cursor-grabbing"
+          style={{ touchAction: 'none' }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="pointer-events-none h-4 w-4 text-white"
+          >
+            <circle cx="8" cy="6" r="1.5" />
+            <circle cx="8" cy="12" r="1.5" />
+            <circle cx="8" cy="18" r="1.5" />
+            <circle cx="16" cy="6" r="1.5" />
+            <circle cx="16" cy="12" r="1.5" />
+            <circle cx="16" cy="18" r="1.5" />
+          </svg>
+        </div>
+      )}
       <Picture
         image={placeholder}
         styles="aspect-[225/330] h-full overflow-hidden rounded-l-lg relative"
@@ -118,36 +172,70 @@ export const AnimeMusicItem = ({
           alt={song.song_title}
           className="relative aspect-[225/330] h-full rounded-l-lg object-cover object-center"
         />
-        <Overlay className="to-Primary-950/60 h-full w-full bg-gradient-to-b from-transparent" />
+        <Overlay className="group-hover:bg-Primary-950/40 bg-Primary-950/0 h-full w-full" />
       </Picture>
 
       {song.type && (
         <span
-          className={`absolute top-2 right-2 flex-shrink-0 rounded-full border p-1 text-xs font-medium md:px-2 md:py-1 ${getTypeColor(song.type)}`}
+          className={`absolute top-2 flex-shrink-0 rounded-full border p-1 text-xs font-medium md:px-2 md:py-1 ${getTypeMusicColor(song.type)} ${isInMusicPlayer ? 'right-8 md:right-10' : 'right-2 md:right-3'}`}
         >
           {song.type.toUpperCase()}
         </span>
       )}
 
-      <AddToPlayListButton
-        song={{
-          image,
-          banner_image,
-          anime_title,
-          placeholder,
-          ...song,
-        }}
-        isInPlayList={isInPlaylist}
-        clasName="hover:bg-Primary-950 absolute right-2 bottom-2 z-10 flex-shrink-0 cursor-pointer rounded-full p-2 transition-colors duration-300"
-      />
+      <MoreOptions
+        className={`absolute md:bottom-3 ${isInMusicPlayer ? 'right-8 md:right-10' : 'right-2 md:right-3'} bottom-2 z-10 md:flex`}
+      >
+        <AddToPlayListButton
+          song={{
+            image,
+            banner_image,
+            anime_title,
+            placeholder,
+            ...song,
+          }}
+          isInPlayList={isInPlaylist}
+          clasName="hover:text-enfasisColor group  cursor-pointer rounded-md  p-1 text-sm transition-all duration-300  disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+        />
 
-      <footer className="flex h-full w-full max-w-[60%] flex-col items-start gap-2 p-2 md:p-4">
-        <h3 className="text-l group-hover:text-enfasisColor/80 g text-pretty text-white transition-colors duration-300 ease-in-out select-none">
-          {song.song_title}
-        </h3>
-        <p className="text-s line-clamp-1 text-gray-400 select-none">
-          {song.artist_name}
-        </p>
+        <ShareButton
+          className="hover:text-enfasisColor group cursor-pointer rounded-md p-1 text-sm transition-all duration-300 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+          url={`/music/${normalizeString(song.song_title)}_${song.theme_id}`}
+          title={song.song_title}
+          text={`Listen ${song.song_title} on AniDev`}
+        />
+        <a
+          href={`/music/${normalizeString(song.song_title)}_${song.theme_id}`}
+          title={`Listen ${song.song_title}`}
+          className="hover:text-enfasisColor group cursor-pointer rounded-md p-1 text-sm transition-all duration-300 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
+        >
+          <PlayIcon className="h-4 w-4" />
+        </a>
+      </MoreOptions>
+
+      <footer className="z-10 flex h-full w-full max-w-[60%] flex-col items-start justify-center gap-8 p-2 md:p-4">
+        <div className="text-pretty ease-in-out select-none">
+          <span className="text-l text-white">{song.song_title}</span>
+
+          {song.artist_name && (
+            <>
+              <span className="text-sx"> By</span>{' '}
+              <span className="text-l hover:text-enfasisColor text-gray-300 transition-all duration-300">
+                {song.artist_name}
+              </span>
+            </>
+          )}
+        </div>
+
+        <button
+          onClick={(e) => {
+            e.stopPropagation()
+            navigate(`/anime/${normalizeString(anime_title)}_${song.anime_id}`)
+          }}
+          className="text-s text-gray-300 transition-all duration-300 select-none hover:underline"
+        >
+          {anime_title}
+        </button>
       </footer>
     </article>
   )
