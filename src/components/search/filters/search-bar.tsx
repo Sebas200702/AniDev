0e4603f55e17d54172da75a9946bb2269ea65f81
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-24 10:46:54 -0500

    Enhance SearchBar component by adding an Overlay for improved UI interaction and integrating a clear button functionality. Updated styling for better responsiveness and user experience.

--- src/components/search/filters/search-bar.tsx
+++ src/components/search/filters/search-bar.tsx
@@ -1,17 +1,33 @@
+import { CloseIcon } from '@components/icons/close-icon'
+import { SearchIcon } from '@components/icons/search-icon'
+import { Overlay } from '@components/overlay'
 import { useSearchStoreResults } from '@store/search-results-store'
-
 export const SearchBar = () => {
   const { query, setQuery } = useSearchStoreResults()
 
   return (
-    <div className="hover:bg-enfasisColor/5 hover:border-enfasisColor/40 flex justify-center rounded-md border border-gray-100/10 px-4 text-white transition-all duration-200 ease-in-out">
+    <div className="group hover:border-enfasisColor/50 bg-Complementary relative flex h-full w-full items-center justify-center rounded-lg border border-gray-100/10 px-3 py-2 text-white transition-all duration-300 ease-in-out md:w-2/3">
+      <Overlay className="bg-enfasisColor/5 group-hover:bg-enfasisColor/10 h-full w-full" />
+      <SearchIcon className="mr-3 h-4.5 w-4.5 text-gray-400" />
       <input
         type="text"
         className="min-w-[50px] flex-grow cursor-pointer bg-transparent focus:outline-none"
-        placeholder="Search"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
+      {query && (
+        <button
+          type="button"
+          onClick={(e) => {
+            e.stopPropagation()
+            setQuery('')
+          }}
+          className="hover:bg-enfasisColor/10 cursor-pointer rounded-full p-1"
+          aria-label="Clear selection"
+        >
+          <CloseIcon className="h-3 w-3 text-gray-400" />
+        </button>
+      )}
     </div>
   )
 }
