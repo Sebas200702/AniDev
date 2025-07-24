a08bf0d5320e2cd48587d762dd89558d0c260595
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-24 10:45:39 -0500

    Refactor Cover component to update animation class for paused state. Changed class name from 'animate-[animation-play-state: paused;]' to 'animation-pause' for improved clarity and consistency in animation handling.

--- src/components/music-player/cover.tsx
+++ src/components/music-player/cover.tsx
@@ -55,7 +55,7 @@ export const Cover = () => {
       <div className="disk flex items-center justify-center rounded-full p-6">
         <figure
           className={`animate-spin-slow relative h-32 w-32 overflow-hidden rounded-full border border-gray-100/20 shadow-lg transition-all duration-300 ease-in-out ${
-            isPlaying ? '' : 'animate-[animation-play-state: paused;]'
+            isPlaying ? '' : 'animation-pause'
           }`}
         >
           <Picture
