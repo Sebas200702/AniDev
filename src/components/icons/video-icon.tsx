33488fc5ac551a8b9500092c2cdbdc9f096eaf41
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-24 10:44:28 -0500

    Add VideoIcon component for enhanced visual representation of video features. Introduced a new SVG icon to improve user interface consistency.

---
+++ src/components/icons/video-icon.tsx
@@ -0,0 +1,18 @@
+import type { IconProps } from 'types'
+export const VideoIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M4 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zM8 4v16M16 4v16M4 8h4M4 16h4M4 12h16M16 8h4M16 16h4" />
+    </svg>
+  )
+}
