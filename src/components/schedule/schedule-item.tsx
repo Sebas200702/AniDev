227add5593af1863f21ffb161b522335a540fe06
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-25 13:47:22 -0500

    Refactor import path for Picture component in ScheduleItem to improve modularity and organization.

--- src/components/schedule/schedule-item.tsx
+++ src/components/schedule/schedule-item.tsx
@@ -1,4 +1,4 @@
-import { Picture } from '@components/picture'
+import { Picture } from '@components/media/picture'
 import { useDynamicBorder } from '@hooks/useDynamicBorder'
 import { useGlobalUserPreferences } from '@store/global-user'
 import { baseUrl } from '@utils/base-url'
