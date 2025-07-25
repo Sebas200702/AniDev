b777c64205303ab63338388b7a4866ffc4d4b325
Sebas200702 <sebastorregroza6@gmail.com>
2025-07-25 13:42:02 -0500

    Refactor import paths for AnimeCard and SectionList components in UserList to improve modularity and organization.

--- src/components/profile/user-tabs/user-list.tsx
+++ src/components/profile/user-tabs/user-list.tsx
@@ -1,7 +1,7 @@
-import { AnimeCard } from '@components/anime-card'
+import { AnimeCard } from '@components/anime-info/anime-card'
 import { UserListOptions } from '@components/profile/user-tabs/user-list-options'
 import { LoadingCard } from '@components/search/results/loading-card'
-import { SectionList } from '@components/section-list'
+import { SectionList } from '@components/common/section-list'
 import { useGlobalUserPreferences } from '@store/global-user'
 import { useUserListsStore } from '@store/user-list-store'
 /**
