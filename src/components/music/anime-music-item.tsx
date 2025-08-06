dceed30389e74605e073aecdc42812e0014215f9
Sebas200702 <sebastorregroza6@gmail.com>
2025-08-05 20:31:37 -0500

    Refactor AnimeMusicItem component to improve click handling and enhance layout. Replaced anchor tags with onClick navigation for better user experience, streamlined JSX structure, and ensured consistent styling for song details and images.

--- src/components/music/anime-music-item.tsx
+++ src/components/music/anime-music-item.tsx
@@ -88,129 +88,108 @@ export const AnimeMusicItem = ({
     <article
       ref={setNodeRef}
       style={style}
-      className={`transition-all duration-200 ease-out ${isDragging ? 'z-50' : ''} group group border-enfasisColor group relative flex h-full w-full cursor-pointer bg-zinc-800 hover:bg-Primary-900  mx-auto  aspect-[100/30]  overflow-hidden flex-row items-start rounded-lg border-l-4 transition-all duration-300 ease-in-out md:max-h-36 md:gap-2 md:hover:translate-x-1`}
+      onClick={() => {
+        if (isInMusicPlayer) return
+        navigate(`/music/${normalizeString(song.song_title)}_${song.theme_id}`)
+      }}
+      className={` link transition-all duration-200 ease-out ${isDragging ? 'z-50' : ''} group group border-enfasisColor group relative flex h-full w-full cursor-pointer flex-row items-start rounded-lg border-l-4 transition-all duration-300 ease-in-out md:max-h-36 md:gap-2 md:hover:translate-x-1`}
     >
-      <a
-        href={`/music/${normalizeString(song.song_title)}_${song.theme_id}`}
-        className="w-full h-full"
-        title={song.song_title}
-      >
-        <div className="flex aspect-[100/30] h-full w-full flex-row overflow-hidden rounded-lg md:max-h-36">
-          <div className="absolute top-0 left-0 h-full w-full overflow-hidden rounded-lg">
-            <img
-              src={createImageUrlProxy(
-                banner_image || image,
-                '100',
-                '70',
-                'webp'
-              )}
-              alt={song.song_title}
-              className="aspect-[100/30] w-full object-cover object-center"
-            />
-            <Overlay className="bg-Primary-950/90 h-full w-full overflow-hidden rounded-lg backdrop-blur-sm" />
-          </div>
-
-
-
-          {song.type && (
-            <span
-              className={`absolute top-2 flex-shrink-0 rounded-full border p-1 text-xs font-medium md:px-2 md:py-1 ${getTypeMusicColor(song.type)} ${isInMusicPlayer ? 'right-8 md:right-10' : 'right-2 md:right-3'}`}
-            >
-              {song.type.toUpperCase()}
-            </span>
-          )}
-
-
-        </div>
-      </a>
-      <footer className="z-10 flex w-full h-full flex-row absolute items-start">
-        <Picture
-            image={createImageUrlProxy(placeholder, '0', '0', 'webp')}
-            styles="aspect-[225/330] h-full overflow-hidden rounded-l-lg relative"
-          >
-            {isCurrentSong && (
-              <div className="bg-Complementary/30 absolute inset-0 z-10 flex items-center justify-center gap-[3px]">
-                {heights.map((height, index) => (
-                  <div
-                    key={index}
-                    className="bg-enfasisColor w-[3px] rounded-md transition-all duration-150 ease-out group-hover:opacity-0"
-                    style={{ height: `${height}px` }}
-                  />
-                ))}
-              </div>
+      <div className="flex aspect-[100/30] h-full w-full flex-row overflow-hidden rounded-lg md:max-h-36">
+        <div className="absolute top-0 left-0 h-full w-full overflow-hidden rounded-lg">
+          <img
+            src={createImageUrlProxy(
+              banner_image || image,
+              '100',
+              '70',
+              'webp'
             )}
-            <button
-              className="text-enfasisColor pointer-events-none absolute inset-0 z-20 mx-auto flex h-full w-full cursor-pointer items-center justify-center p-4 opacity-0 transition-all duration-150 group-hover:pointer-events-auto group-hover:opacity-90 disabled:pointer-events-none"
-              onClick={(e) => handlePlay(e)}
-              disabled={!canPlay}
-            >
-              {isPlaying && isCurrentSong ? (
-                <PauseIcon className="h-6 w-6" />
-              ) : (
-                <PlayIcon className="h-6 w-6" />
-              )}
-            </button>
+            alt={song.song_title}
+            className="aspect-[100/30] w-full object-cover object-center"
+          />
+          <Overlay className="bg-Primary-950/90 h-full w-full overflow-hidden rounded-lg backdrop-blur-sm" />
+        </div>
 
-            <img
-              src={createImageUrlProxy(image, '0', '70', 'webp')}
-              alt={song.song_title}
-              className="relative aspect-[225/330] h-full rounded-l-lg object-cover object-center"
-            />
-            <Overlay className="group-hover:bg-Primary-950/40 bg-Primary-950/0 h-full w-full" />
+        <Picture
+          image={createImageUrlProxy(placeholder, '0', '0', 'webp')}
+          styles="aspect-[225/330] h-full overflow-hidden rounded-l-lg relative"
+        >
+          {isCurrentSong && (
+            <div className="bg-Complementary/30 absolute inset-0 z-10 flex items-center justify-center gap-[3px]">
+              {heights.map((height, index) => (
+                <div
+                  key={index}
+                  className="bg-enfasisColor w-[3px] rounded-md transition-all duration-150 ease-out group-hover:opacity-0"
+                  style={{ height: `${height}px` }}
+                />
+              ))}
 
-          </Picture>
-
-          {song.type && (
-            <span
-              className={`absolute top-2 flex-shrink-0 rounded-full border p-1 text-xs font-medium md:px-2 md:py-1 ${getTypeMusicColor(song.type)} ${isInMusicPlayer ? 'right-8 md:right-10' : 'right-2 md:right-3'}`}
-            >
-              {song.type.toUpperCase()}
-            </span>
+              <button
+                className="text-enfasisColor pointer-events-none absolute inset-0 z-20 mx-auto flex h-full w-full cursor-pointer items-center justify-center p-4 opacity-0 transition-all duration-150 group-hover:pointer-events-auto group-hover:opacity-90 disabled:pointer-events-none"
+                onClick={(e) => handlePlay(e)}
+                disabled={!canPlay}
+              >
+                {isPlaying ? (
+                  <PauseIcon className="h-6 w-6" />
+                ) : (
+                  <PlayIcon className="h-6 w-6" />
+                )}
+              </button>
+            </div>
           )}
 
-          <div className="z-10 flex w-full max-w-[65%] flex-col items-start justify-between px-4 py-2 md:justify-center md:gap-4 md:p-4 h-full">
+          <img
+            src={createImageUrlProxy(image, '0', '70', 'webp')}
+            alt={song.song_title}
+            className="relative aspect-[225/330] h-full rounded-l-lg object-cover object-center"
+          />
+          <Overlay className="group-hover:bg-Primary-950/40 bg-Primary-950/0 h-full w-full" />
+        </Picture>
 
+        {song.type && (
+          <span
+            className={`absolute top-2 flex-shrink-0 rounded-full border p-1 text-xs font-medium md:px-2 md:py-1 ${getTypeMusicColor(song.type)} ${isInMusicPlayer ? 'right-8 md:right-10' : 'right-2 md:right-3'}`}
+          >
+            {song.type.toUpperCase()}
+          </span>
+        )}
 
-            <header className='flex flex-row space-x-1'>
-                <h3 className="text-m truncate text-white">
-                {song.song_title}
-              </h3>
+        <footer className="z-10 flex w-full max-w-[65%] flex-col items-start justify-between px-4 py-2 md:justify-center md:gap-4 md:p-4">
+          <div className="flex w-full flex-col items-start space-x-1 truncate text-pretty ease-in-out select-none md:flex-row">
+            <span className="text-m truncate text-white">
+              {song.song_title}
+            </span>
 
-              {song.artist_name && (
-                <a
-                  title={`View ${song.artist_name} profile`}
-                  href={ `/artist/${normalizeString(song.artist_name || '')}`}
-                  className="text-sxx text-Primary-300 flex w-full flex-row items-end gap-1 truncate md:w-auto"
-                >
-                  By
-                  <strong className="text-Primary-100 text-m hover:text-enfasisColor cursor-pointer truncate transition-all duration-300">
-                    {song.artist_name}
-                  </strong>
-                </a>
-              )}
-
-            </header>
-
-
-
-            <span className="text-sxx text-Primary-300 flex w-full flex-row items-end truncate">
-              From
+            {song.artist_name && (
               <a
-              href={`/anime/${normalizeString(anime_title)}_${song.anime_id}`}
-                title={`View ${anime_title} details`}
-                className="ml-1 cursor-pointer truncate"
+                title={`View ${song.artist_name} profile`}
+                href={`/artist/${normalizeString(song.artist_name || '')}`}
+                className="text-sxx text-Primary-300 flex w-full flex-row items-end gap-1 truncate md:w-auto"
               >
-                <strong className="text-sx text-Primary-100 truncate hover:underline">
-                  {anime_title}
+                By
+                <strong className="text-Primary-100 text-m hover:text-enfasisColor cursor-pointer truncate transition-all duration-300">
+                  {' '}
+                  {song.artist_name}
                 </strong>
               </a>
-            </span>
-
+            )}
           </div>
+          <span className="text-sxx text-Primary-300 flex w-full flex-row items-end truncate">
+            From
+            <a
+              href={`/anime/${normalizeString(anime_title)}_${song.anime_id}`}
 
 
+              title={`View ${anime_title} details`}
+              className="ml-1 cursor-pointer truncate"
+            >
+              <strong className="text-sx text-Primary-100 truncate hover:underline">
+                {anime_title}
+              </strong>
+            </a>
+          </span>
+        </footer>
+      </div>
 
-          </footer>
       <MoreOptions
         className={`absolute md:bottom-3 ${isInMusicPlayer ? 'right-8 md:right-10' : 'right-2 md:right-3'} bottom-2 md:flex`}
       >
