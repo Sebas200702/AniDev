350eea81e15bc9ca9853aea259be07afe987e703
Sebas200702 <sebastorregroza6@gmail.com>
2025-08-06 08:13:32 -0500

    Update

--- src/components/layout/aside.tsx
+++ src/components/layout/aside.tsx
@@ -38,7 +38,7 @@ export const AsideNav = () => {
 
   return (
     <>
-      <aside className={`fixed top-0 left-0 z-50 hidden h-full flex-col text-white md:flex`}>
+      <aside className={`fixed top-0 left-0 z-50 hidden h-full w-20 flex-col text-white md:flex`}>
         <nav className=" mt-10 flex h-full w-full flex-col gap-6 items-center">
           <Logo />
           {menuItems.map((item) => (
