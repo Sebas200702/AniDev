9f99245030c7b34804884e06b5795a4c0e929481
Sebas200702 <sebastorregroza6@gmail.com>
2025-08-22 17:58:03 -0500

    Refactor Artist component: remove redundant fetch calls and simplify structure for improved readability.

--- src/components/artist/artist.tsx
+++ src/components/artist/artist.tsx
@@ -1,35 +1,9 @@
-import type {AnimeSong} from 'types'
+import type { AnimeSong, ArtistInfo } from 'types'
+
+import { useEffect } from 'react'
 import { useFetch } from '@hooks/useFetch'
 
 export const Artist = ({ name, id }: { name: string; id: string }) => {
-  const {
-    data: dataArtistMusic,
-    loading,
-    error,
-  } = useFetch<AnimeSong[]>({
-    url: `/api/music?artist_filter=${name}`,
-  })
 
-  const {
-    data: dataArtist,
-    loading: loadingArtist,
-    error: errorArtist,
-  } = useFetch({
-    url: `/api/artist?artistName=${name}`,
-  })
-
-  if (loading || !dataArtistMusic) return <p>Loading...</p>
-
-  return (
-    <section>
-      <ul>
-        {dataArtistMusic.map((song) => (
-          <li key={song.song_id}>{song.song_title}</li>
-        ))}
-      </ul>
-      
-    </section>
-  )
-
-
+  
 }
