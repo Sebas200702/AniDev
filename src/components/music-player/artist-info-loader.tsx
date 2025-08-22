54b42667c6754641a146f19bc40c2eb9cf815e47
Sebas200702 <sebastorregroza6@gmail.com>
2025-08-22 18:48:30 -0500

    Refactor ArtistInfoLoader component: update layout classes for improved responsiveness and consistency.

--- src/components/music-player/artist-info-loader.tsx
+++ src/components/music-player/artist-info-loader.tsx
@@ -2,7 +2,7 @@ export const ArtistInfoLoader = () => {
   return (
     <div
 
-    className="relative flex flex-col gap-6 p-6 row-start-2 bg-Complementary animate-pulse duration-300 rounded-3xl"
+    className="relative flex flex-col gap-6 md:p-6  p-4 row-start-2 bg-Complementary animate-pulse duration-300 md:rounded-3xl"
   >
      <h3 className="text-lx z-20 ">About the artist </h3>
     <div className="relative z-20 flex w-full md:max-w-100 max-w-80 flex-row items-center gap-6 rounded-full p-2">
@@ -13,10 +13,10 @@ export const ArtistInfoLoader = () => {
         </div>
     </div>
 
-    <div className="flex flex-col gap-3">
-        <div className="h-5 w-[90%] bg-Primary-800 animate-pulse rounded-md duration-300"></div>
-        <div className="h-5 w-[70%] bg-Primary-800 animate-pulse rounded-md duration-300"></div>
-        <div className="h-5 w-[80%] bg-Primary-800 animate-pulse rounded-md duration-300"></div>
+    <div className="flex flex-col md:gap-3 gap-2">
+        <div className="md:h-5 w-[90%] h-4 bg-Primary-800 animate-pulse rounded-md duration-300"></div>
+        <div className="md:h-5 w-[70%] h-4 bg-Primary-800 animate-pulse rounded-md duration-300"></div>
+        <div className="md:h-5 w-[80%] h-4 bg-Primary-800 animate-pulse rounded-md duration-300"></div>
     </div>
     </div>
   )
