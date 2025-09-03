45828dea74c8f725ec9e1c2ede0da9b3c9447c08
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-03 12:36:48 -0500

    Enhance CharacterHeader component to display character name and kanji together, and conditionally render nicknames for improved character profile presentation.

--- src/components/character-info/character-header.tsx
+++ src/components/character-info/character-header.tsx
@@ -1,23 +1,28 @@
-import type { CharacterDetails } from 'types'
+import type { CharacterDetails, PersonAbout } from 'types'
 
 interface Props {
   character: CharacterDetails
+
 }
 
 export const CharacterHeader = ({ character }: Props) => {
+
+  const nicknames = character.character_nicknames ?? []
+
   return (
-    <header className="character-header z-20 row-span-2 flex h-full w-full flex-col gap-4 md:col-span-2 xl:col-span-4">
-      <h1 className="title text-white">{character.character_name}</h1>
-      <h2 className="text-l text-Primary-200">
-        {character.character_name_kanji}
-      </h2>
-      <h3>Nicknames</h3>
-      <p className="text-m text-Primary-200 max-w-[80ch]">
-        {character.character_nicknames?.join(', ')}
-      </p>
-      <p className="text-m text-Primary-200 max-w-[80ch]">
-        {character.character_about}
-      </p>
+    <header className="character-header z-20 px-4 flex w-full flex-col items-center md:items-start rounded-2xl col-span-1 md:col-span-2 xl:col-span-4">
+
+      <h1 className="title font-bold">
+        {character.character_name} - {character?.character_name_kanji}
+      </h1>
+
+      {nicknames.length > 0 && (
+        <p className="mt-2 text-l text-Primary-200">
+          <span className="font-semibol text-Primary-50">Nicknames:</span>{'  '}
+          {nicknames.join(', ')}
+        </p>
+      )}
+
     </header>
   )
 }
