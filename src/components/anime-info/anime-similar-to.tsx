34665b07ba306dc4dc44d10b0a32ae7e88f168ef
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-04 10:59:47 -0500

    Refactor SimilarToComponent to utilize MainInfo for improved layout structure and readability, while enhancing loading state handling.

--- src/components/anime-info/anime-similar-to.tsx
+++ src/components/anime-info/anime-similar-to.tsx
@@ -1,6 +1,7 @@
 import { AnimeCard } from '@components/anime-info/anime-card'
-import { useFetch } from '@hooks/useFetch'
 import { LoadingCard } from '@components/search/results/loading-card'
+import { MainInfo } from '@components/shared/MainInfo'
+import { useFetch } from '@hooks/useFetch'
 import type { AnimeCardInfo } from 'types'
 
 interface props {
@@ -12,29 +13,22 @@ export const SimilarToComponet = ({ title, mal_id }: props) => {
     url: `/api/getRecomendations?similar_to=${title}&currentAnime=${mal_id}`,
   })
 
-
-
   return (
-    <section
-      className='flex flex-col md:row-span-2 md:col-span-3 md:col-start-2 border-Primary-800/30 from-Complementary via-Primary-950 to-Complementary/95  relative z-10 overflow-hidden md:rounded-xl border bg-gradient-to-br shadow-2xl backdrop-blur-sm transition-all duration-500 ease-in-out hover:shadow-xl
-     '
-    >
+    <MainInfo>
       <div className="from-Primary-950/20 to-Primary-900/10 pointer-events-none absolute inset-0 bg-gradient-to-t via-transparent" />
 
       <div className="border-Primary-800/30 from-Primary-950/80 to-Complementary/80 relative z-20 flex-shrink-0 border-b bg-gradient-to-r p-6 backdrop-blur-md">
-        <h3 className='text-lx'>Similar To</h3>
+        <h3 className="text-lx">Similar To</h3>
       </div>
-      <ul className='grid xl:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8 p-6'>
-        {loading || !data ? (
-          Array(10).fill(null).map((_, idx) => (
-            <LoadingCard key={idx} />
-          ))
-        ) : (
-          data?.map((anime) => (
-            <AnimeCard anime={anime} key={anime.mal_id} />
-          ))
-        )}
+      <ul className="grid xl:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8 p-6">
+        {loading || !data
+          ? Array(10)
+              .fill(null)
+              .map((_, idx) => <LoadingCard key={idx} />)
+          : data?.map((anime) => (
+              <AnimeCard anime={anime} key={anime.mal_id} />
+            ))}
       </ul>
-    </section>
+    </MainInfo>
   )
 }
