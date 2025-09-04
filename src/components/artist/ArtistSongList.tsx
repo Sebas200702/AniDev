432f816595701656249093fe12e235e4280b7155
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-04 11:00:08 -0500

    Refactor ArtistSongList component to replace AnimeMusicItem with MusicCard for improved layout and consistency, while simplifying the rendering of song items.

--- src/components/artist/ArtistSongList.tsx
+++ src/components/artist/ArtistSongList.tsx
@@ -1,27 +1,16 @@
-import type { AnimeSongWithImage } from 'types'
+import { MusicCard } from '@components/music/music-card'
 import { InfoSection } from '@components/shared/InfoSection'
-import { AnimeMusicItem } from '@components/music/anime-music-item'
+import type { AnimeSongWithImage } from 'types'
 
 interface Props {
   songs: AnimeSongWithImage[]
 }
 export const ArtistSongList = ({ songs }: Props) => {
   return (
-    <InfoSection title='Songs'>
-      
-        <ul className="flex flex-col gap-4 w-full">
-          {songs.map((song) => (
-            <AnimeMusicItem 
-            key={song.song_id}
-            song={song} 
-            image={song.image} 
-            anime_title={song.anime_title}
-            banner_image={song.banner_image}
-            />
-          ))}
-        </ul>
-     
-      </InfoSection>
- 
+    <InfoSection title="Songs" ulClassName='w-full'>
+        {songs.map((song) => (
+          <MusicCard key={song.song_id} song={song} isMini />
+        ))}
+    </InfoSection>
   )
 }
