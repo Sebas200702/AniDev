4ace475dcab663de9e568fa2f853bc23fb70d06a
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-06 11:51:51 -0500

    Integrate parental control settings into anime recommendations API call in SimilarToComponent for enhanced user experience and content filtering.

--- src/components/anime-info/anime-similar-to.tsx
+++ src/components/anime-info/anime-similar-to.tsx
@@ -2,6 +2,7 @@ import { AnimeCard } from '@components/anime-info/anime-card'
 import { LoadingCard } from '@components/search/results/loading-card'
 import { MainInfo } from '@components/shared/MainInfo'
 import { useFetch } from '@hooks/useFetch'
+import { useGlobalUserPreferences } from '@store/global-user'
 import type { AnimeCardInfo } from 'types'
 
 interface props {
@@ -9,8 +10,9 @@ interface props {
   mal_id: number
 }
 export const SimilarToComponet = ({ title, mal_id }: props) => {
+  const { parentalControl } = useGlobalUserPreferences()
   const { data, loading } = useFetch<AnimeCardInfo[]>({
-    url: `/api/getRecomendations?similar_to=${title}&currentAnime=${mal_id}`,
+    url: `/api/animes/getRecomendations?similar_to=${title}&currentAnime=${mal_id}&parental_control=${parentalControl}`,
   })
 
   return (
@@ -20,7 +22,7 @@ export const SimilarToComponet = ({ title, mal_id }: props) => {
       <div className="border-Primary-800/30 from-Primary-950/80 to-Complementary/80 relative z-20 flex-shrink-0 border-b bg-gradient-to-r p-6 backdrop-blur-md">
         <h3 className="text-lx">Similar To</h3>
       </div>
-      <ul className="grid xl:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8 p-6">
+      <ul className="grid xl:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8 max-h-[600px] overflow-y-scroll no-scrollbar p-6">
         {loading || !data
           ? Array(10)
               .fill(null)
