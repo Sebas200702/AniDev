8807f6f7c4a46486a8473556277a0c1cac838618
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-06 11:53:59 -0500

    Fix formatting in MainInfo component by removing unnecessary whitespace for cleaner code.

--- src/components/shared/MainInfo.tsx
+++ src/components/shared/MainInfo.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 export const MainInfo = ({ children }: Props) => {
   return (
-    <section className="xl:col-span-3 col- col-span-1 md:col-span-2 border-Primary-800/30 from-Complementary via-Primary-950 to-Complementary/95  relative z-10 flex flex-col overflow-hidden rounded-xl border bg-gradient-to-br shadow-2xl transition-all duration-500 ease-in-out hover:shadow-xl">
+    <section className="xl:col-span-3  col-span-1 md:col-span-2 border-Primary-800/30 from-Complementary via-Primary-950 to-Complementary/95  relative z-10 flex flex-col overflow-hidden rounded-xl border bg-gradient-to-br shadow-2xl transition-all duration-500 ease-in-out hover:shadow-xl">
       <div className="from-Primary-950/20 to-Primary-900/10 pointer-events-none absolute inset-0 bg-gradient-to-t via-transparent" />
       {children}
     </section>
