50ccdd59930cac0b835740b53c445480ca0d6bbf
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-08 20:29:37 -0500

    Refactor SeiyuuList component to improve code formatting by standardizing whitespace and adjusting class name quotes, enhancing readability and maintainability.

--- src/components/character-info/seiyuu-list.tsx
+++ src/components/character-info/seiyuu-list.tsx
@@ -1,14 +1,14 @@
 import { SeiyuuInfo } from '@components/character-info/seiyuu'
-import type { VoiceActor } from 'types'
 import { InfoSection } from '@components/shared/InfoSection'
+import type { VoiceActor } from 'types'
 
 interface Props {
-    seiyuus: VoiceActor[]
+  seiyuus: VoiceActor[]
 }
 
 export const SeiyuuList = ({ seiyuus }: Props) => {
   return (
-    <InfoSection title="Characters" ulClassName='w-full'>
+    <InfoSection title="Characters" ulClassName="w-full">
       {seiyuus.map((seiyuu) => (
         <SeiyuuInfo key={seiyuu.voice_actor_id} seiyuu={seiyuu} />
       ))}
