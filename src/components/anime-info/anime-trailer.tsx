1a08e15e1d3f7783f43a40474ce767cab8f11c04
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-08 20:27:02 -0500

    Remove unused import statements in AnimeTrailer component to enhance code clarity and maintainability.

--- src/components/anime-info/anime-trailer.tsx
+++ src/components/anime-info/anime-trailer.tsx
@@ -1,10 +1,8 @@
 import '@vidstack/react/player/styles/default/theme.css'
 import '@styles/video.css'
 import '@styles/player.css'
-
 import { Picture } from '@components/media/picture'
 import { CustomLayout } from '@components/music-player/custom-layout'
-import { baseUrl } from '@utils/base-url'
 import { createImageUrlProxy } from '@utils/create-image-url-proxy'
 import { MediaPlayer, MediaProvider, Poster } from '@vidstack/react'
 
