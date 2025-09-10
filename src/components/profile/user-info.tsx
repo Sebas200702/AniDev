6ad8ea8de556aa2073f6e91418908efb9ed81969
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-10 08:51:48 -0500

    Refactor UserInfo component to enhance image handling by replacing img tags with Picture component, utilizing logical OR for fallback sources, and improving overall code clarity and consistency.

--- src/components/profile/user-info.tsx
+++ src/components/profile/user-info.tsx
@@ -5,50 +5,38 @@ import { InputUserImage } from '@components/profile/edit-profile/input-user-imag
 
 import { useGlobalUserPreferences } from '@store/global-user'
 
-import { baseUrl } from '@utils/base-url'
-import { createImageUrlProxy } from '@utils/create-image-url-proxy'
-import { useRef } from 'react'
 
 export const UserInfo = ({
   isSignUp,
 }: {
   isSignUp?: boolean
-  styles?: string
 }) => {
   const { userInfo } = useGlobalUserPreferences()
 
-  const imageRef = useRef<HTMLImageElement | null>(null)
 
   return (
     <section className={`${isSignUp ? 'relative w-full md:mb-16 mb-6' : ''}`}>
       <Picture
         styles={` ${isSignUp ? 'w-full h-30 rounded' : 'w-full h-100'} object-cover object-center  absolute top-0 left-0 overflow-hidden `}
-        image={createImageUrlProxy(
-          `${userInfo?.banner_image ?? 'https://media.kitsu.app/anime/cover_images/3936/original.jpg'}`,
-          '100',
-          '0',
-          'webp'
-        )}
-      >
-        <img
-          className="absolute inset-0 h-full w-full object-cover object-center"
-          src={createImageUrlProxy(
-            `${userInfo?.banner_image ?? 'https://media.kitsu.app/anime/cover_images/3936/original.jpg'}`,
-            '1080',
-            '75',
-            'webp'
-          )}
-          alt=""
-          loading="lazy"
-        />
-        <Overlay className="to-Primary-950 absolute inset-0 h-full w-full bg-gradient-to-b" />
-        <Overlay className="md:to-Primary-950 absolute top-0 left-0 h-full w-1/4 bg-gradient-to-l" />
-      </Picture>
+        image={
+          userInfo?.banner_image ||
+          'https://media.kitsu.app/anime/cover_images/3936/original.jpg'
+        }
+        placeholder={
+          userInfo?.banner_image ||
+          'https://media.kitsu.app/anime/cover_images/3936/original.jpg'
+        }
+        isBanner
+        alt={`Banner of ${userInfo?.name}`}
+      />
+
+      <Overlay className="to-Primary-950 absolute inset-0 h-full w-full bg-gradient-to-b" />
+      <Overlay className="md:to-Primary-950 absolute top-0 left-0 h-full w-1/4 bg-gradient-to-l" />
       <article
         className={`z-10 flex w-full flex-row items-center ${isSignUp ? 'justify-center' : 'mt-24'} gap-4 text-white md:gap-8`}
       >
         <div
-          className={`group relative flex h-full w-full aspect-square items-center justify-center rounded-full ${isSignUp ? 'md:max-h-24 md:max-w-24 max-w-20 max-h-20 md:translate-y-1/2 mt-4 ' : 'max-h-26 max-w-26 md:max-h-[150px] md:max-w-[150px]'}`}
+          className={`group absolute top-1/2 flex h-full w-full aspect-square items-center justify-center rounded-full ${isSignUp ? 'md:max-h-24 md:max-w-24 max-w-20 max-h-20 ' : 'max-h-26 max-w-26 md:max-h-[150px] md:max-w-[150px]'}`}
         >
           <div
             className={`bg-enfasisColor absolute inset-0 flex w-full h-full items-center justify-center rounded-full transition-opacity duration-200 `}
@@ -59,26 +47,11 @@ export const UserInfo = ({
             </span>
           </div>
           <Picture
-            image={createImageUrlProxy(
-              userInfo?.avatar || `${baseUrl}/placeholder.webp`,
-              '0',
-              '0',
-              'webp'
-            )}
+            image={userInfo?.avatar || ''}
+            placeholder={userInfo?.avatar || ''}
             styles="relative rounded-full overflow-hidden w-full h-full object-cover object-center"
-          >
-            <img
-              ref={imageRef}
-              src={createImageUrlProxy(
-                userInfo?.avatar || `${baseUrl}/placeholder.webp`,
-                '0',
-                '75',
-                'webp'
-              )}
-              alt={`${userInfo?.name} Avatar`}
-              className="relative h-full w-full rounded-full transition-all duration-200 object-cover object-center"
-            />
-          </Picture>
+            alt={`${userInfo?.name} Avatar`}
+          />
 
           {(isSignUp || userInfo?.name) && <InputUserImage />}
         </div>
