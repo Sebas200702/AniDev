31cef4e018c577345fd76307733db503446dc581
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-09 21:52:18 -0500

    Refactor SeiyuuInfo component to enhance image handling by consolidating image source logic and improving accessibility with updated alt text for better user experience.

--- src/components/character-info/seiyuu.tsx
+++ src/components/character-info/seiyuu.tsx
@@ -1,6 +1,4 @@
 import { Picture } from '@components/media/picture'
-import { baseUrl } from '@utils/base-url'
-import { createImageUrlProxy } from '@utils/create-image-url-proxy'
 import { normalizeString } from '@utils/normalize-string'
 import type { VoiceActor } from 'types'
 
@@ -17,26 +15,11 @@ export const SeiyuuInfo = ({ seiyuu }: Props) => {
       >
         <div className="relative h-16 w-16 flex-shrink-0 overflow-hidden rounded-full">
           <Picture
-            image={createImageUrlProxy(
-              seiyuu.image_url ?? `${baseUrl}/placeholder.webp`,
-              '0',
-              '0',
-              'webp'
-            )}
-            styles="h-full w-full relative overflow-hidden"
-          >
-            <img
-              src={createImageUrlProxy(
-                seiyuu.image_url ?? `${baseUrl}/placeholder.webp`,
-                '0',
-                '70',
-                'webp'
-              )}
-              alt={seiyuu.name}
-              className="relative h-full w-full object-cover transition-transform duration-300 ease-in-out group-hover/actor:scale-105"
-              loading="lazy"
-            />
-          </Picture>
+            image={seiyuu.image_url ?? ''}
+            placeholder={seiyuu.image_url ?? ''}
+            styles="h-full w-full object-cover transition-transform duration-300 ease-in-out group-hover/actor:scale-105"
+            alt={seiyuu.name}
+          />
         </div>
 
         <div className="flex flex-1 flex-col justify-center">
