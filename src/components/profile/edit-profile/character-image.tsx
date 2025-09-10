566e5b9bb0b0c0fd2b68b19d45a86bfe32049490
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-10 08:52:54 -0500

    Refactor CharacterImage component to enhance image handling by replacing img tags with Picture component, removing proxy functions, and improving overall code clarity and consistency.

--- src/components/profile/edit-profile/character-image.tsx
+++ src/components/profile/edit-profile/character-image.tsx
@@ -1,7 +1,7 @@
 import { Overlay } from '@components/layout/overlay'
 import { Picture } from '@components/media/picture'
 import { useUpdateProfile } from '@store/update-profile'
-import { createImageUrlProxy } from '@utils/create-image-url-proxy'
+
 import type { CharacterImages } from 'types'
 interface Props {
   character: CharacterImages
@@ -15,30 +15,17 @@ export const CharacterImage = ({ character }: Props) => {
   return (
     <button
       key={character.character_id}
-      className={` h-full w-full aspect-square rounded-full border-3 ${isSelected ? ' border-enfasisColor' : 'border-enfasisColor/0'} transition-colors duration-300 ease-in-out  overflow-hidden aspect-square cursor-pointer group`}
+      className={`relative h-full w-full aspect-square rounded-full border-3 ${isSelected ? ' border-enfasisColor' : 'border-enfasisColor/0'} transition-colors duration-300 ease-in-out  overflow-hidden aspect-square cursor-pointer group`}
       onClick={handleClick}
     >
       <Picture
-        styles="relative w-full h-full  "
-        image={createImageUrlProxy(
-          character.character_small_image_url,
-          '0',
-          '0',
-          'webp'
-        )}
-      >
-        <img
-          src={createImageUrlProxy(
-            character.character_image_url,
-            '240',
-            '75',
-            'webp'
-          )}
-          alt={character.character_name}
-          className="relative object-cover object-center w-full h-full aspect-square "
-        />
-        <Overlay className="group-hover:opacity-100 opacity-0 h-full w-full bg-enfasisColor/40 transition-opacity duration-200" />
-      </Picture>
+        styles="relative object-cover object-center w-full h-full aspect-square "
+        placeholder={character.character_small_image_url}
+        image={character.character_image_url}
+        alt={character.character_name}
+      />
+
+      <Overlay className="group-hover:opacity-100 opacity-0 h-full w-full bg-enfasisColor/40 transition-opacity duration-200" />
     </button>
   )
 }
