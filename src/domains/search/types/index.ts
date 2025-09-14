b0bdaccbaa8e3a8e5a4799bcf3a12f7d1d3c5749
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-14 14:41:10 -0500

    Add search-related types and interfaces to index.ts for improved type safety and organization

---
+++ src/domains/search/types/index.ts
@@ -0,0 +1,26 @@
+export interface FilterOption {
+  value: string
+  label: string
+}
+
+export interface AppliedFilters {
+  [category: string]: string[]
+}
+
+export enum SearchType {
+  ANIMES = 'animes',
+  MUSIC = 'music',
+  CHARACTERS = 'characters',
+}
+
+export interface SearchHistory {
+  query: string
+  appliedFilters: AppliedFilters
+  totalResults: number
+}
+
+export type SearchQueryOptions = {
+  query: string
+  filters?: AppliedFilters
+  type?: SearchType
+}
