7456700aec7ea9f5e0da2844ef87baf6a10ec550
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-14 17:51:49 -0500

    Update type import in watch-anime component for improved accuracy and consistency across the application.

--- src/domains/watch/components/watch-anime.tsx
+++ src/domains/watch/components/watch-anime.tsx
@@ -1,8 +1,9 @@
+import type { Anime } from '@anime/types'
 import { EpisodesList } from 'domains/watch/components/episodes-list'
 import { Footer } from 'domains/watch/components/footer'
 import { Player } from 'domains/watch/components/player'
 import { useState } from 'react'
-import type { Anime, AnimeEpisode } from 'types'
+import type {  AnimeEpisode } from '@watch/types'
 
 interface Props {
   slug: string
