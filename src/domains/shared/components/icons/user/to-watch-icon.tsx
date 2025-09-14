88dd0ff7914db061e345a74b948eccbb1ce0d88f
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-14 17:22:48 -0500

    Update type import in ToWatchIcon component for improved accuracy and consistency across the application.

--- src/domains/shared/components/icons/user/to-watch-icon.tsx
+++ src/domains/shared/components/icons/user/to-watch-icon.tsx
@@ -1,4 +1,5 @@
-import type { IconProps } from 'types'
+import type { IconProps } from '@shared/types'
+
 
 /**
  * ToWatchIcon component renders an icon representing a bookmark or "to watch" functionality.
