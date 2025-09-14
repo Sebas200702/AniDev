7e715c513f281aaa7b0205f14b90e6c3ea013f8c
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-14 14:44:15 -0500

    Rename VoiceActorHeader component to SeiyuuHeader for consistency with updated naming conventions.

--- src/domains/seiyuu/components/seiyuu-info/voice-actor-header.tsx
+++ src/domains/seiyuu/components/seiyuu-info/voice-actor-header.tsx
@@ -2,7 +2,7 @@ interface Props {
   name: string
 }
 
-export const VoiceActorHeader = ({ name }: Props) => {
+export const SeiyuuHeader = ({ name }: Props) => {
   return (
     <div className="flex flex-col gap-4 z-20">
       <h1 className="text-3xl font-bold text-white">{name}</h1>
