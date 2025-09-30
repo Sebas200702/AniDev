ab54d5ed98677338c5450e311851d35810a943eb
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-30 14:41:24 -0500

    Add useCharacterList hook to fetch and manage anime character data

---
+++ src/domains/anime/hooks/anime-charater/useCharacterList.ts
@@ -0,0 +1,17 @@
+import type { Character } from '@character/types'
+import { useFetch } from '@shared/hooks/useFetch'
+import { useState } from 'react'
+
+export const useCharacterList = ({ animeId }: { animeId: number }) => {
+  const [language, setLanguage] = useState('Japanese')
+  const { data, loading } = useFetch<Character[]>({
+    url: `/api/getAnimeCharacters?animeId=${animeId}&language=${language}`,
+  })
+
+  return {
+    language,
+    setLanguage,
+    characters: data,
+    isLoading: loading,
+  }
+}
