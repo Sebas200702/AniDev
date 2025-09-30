45c8c8f4bc84621984e9b57c245f573a025aca23
Sebas200702 <sebastorregroza6@gmail.com>
2025-09-30 14:41:33 -0500

    Add createGroups utility function to manage item grouping based on window width and context

---
+++ src/domains/anime/utils/create-groups.ts
@@ -0,0 +1,22 @@
+export const createGroups = (
+  items: any[],
+  windowWidth: number,
+  context?: string
+) => {
+  let itemsPerGroup = 2
+
+  if (windowWidth >= 1280) {
+    itemsPerGroup = context === 'anime-info' ? 4 : 6
+  } else if (windowWidth >= 768) {
+    itemsPerGroup = context === 'anime-info' ? 3 : 4
+  }
+
+  return Array.from({ length: Math.ceil(items.length / itemsPerGroup) }).map(
+    (_, groupIndex) => {
+      return items.slice(
+        groupIndex * itemsPerGroup,
+        (groupIndex + 1) * itemsPerGroup
+      )
+    }
+  )
+}
