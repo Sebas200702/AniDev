6f4f8aaaa9dada656be741f2345496acb74f23ca
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-06 20:13:37 -0500

    Refactor CharacterSection to directly use AnimeCharacterList component, simplifying the structure and enhancing character display functionality

--- src/domains/anime/components/anime-charcaters/anime-characters.tsx
+++ src/domains/anime/components/anime-charcaters/anime-characters.tsx
@@ -1,8 +1,5 @@
-import { AnimeCharacterLoader } from '@anime/components/anime-charcaters/anime-character-loader'
-import { AnimeCharacterList } from '@anime/components/anime-charcaters/anime-chatcater-list'
+import { AnimeCharacterList } from '@anime/components/anime-charcaters/anime-characters-list'
 import { useCharacterList } from '@anime/hooks/anime-charater/useCharacterList'
-import { FilterDropdown } from '@search/components/search-filters/filter-dropdown'
-import { languageOptions } from '@search/utils/constants'
 
 interface Props {
   animeId: number
@@ -14,32 +11,11 @@ export const CharacterSection = ({ animeId }: Props) => {
   })
 
   return (
-    <section className="relative z-10 flex flex-col gap-8 rounded-lg p-4 md:p-6">
-      <header className="flex items-center justify-between">
-        <h2 className="text-lxx font-bold text-white">Characters</h2>
-        <FilterDropdown
-          label="Language"
-          styles="max-w-44 mx-0"
-          values={[language]}
-          onChange={(values) => {
-            if (values.length > 0) {
-              setLanguage(values[0])
-            } else {
-              setLanguage('')
-            }
-          }}
-          onClear={() => setLanguage('')}
-          options={languageOptions}
-          singleSelect={true}
-        />
-      </header>
-      <ul className="grid grid-cols-1 gap-4 md:gap-6 xl:grid-cols-2">
-        {!characters || (isLoading && <AnimeCharacterLoader />)}
-
-        {characters && !isLoading && (
-          <AnimeCharacterList characters={characters} />
-        )}
-      </ul>
-    </section>
+    <AnimeCharacterList
+      characters={characters}
+      language={language}
+      setLanguage={setLanguage}
+      isLoading={isLoading}
+    />
   )
 }
