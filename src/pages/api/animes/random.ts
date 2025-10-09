8f103ab059d64f575ed6aed6c0583bb618d60c33
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-09 10:34:02 -0500

    Update random anime API to accept user_id parameter and improve error handling for no results, enhancing response reliability.

--- src/pages/api/animes/random.ts
+++ src/pages/api/animes/random.ts
@@ -5,8 +5,21 @@ import type { APIRoute } from 'astro'
 export const GET: APIRoute = rateLimit(async ({ url }) => {
   const parentalControl =
     url.searchParams.get('parental_control') === 'false' ? false : true
+
+  const userId = url.searchParams.get('user_id')
+
   try {
-    const result = await getRandomAnime(parentalControl)
+    const result = await getRandomAnime(userId, parentalControl)
+    console.log(result)
+    if (!result) {
+      return new Response(
+        JSON.stringify({ error: 'No se encontró un anime aleatorio.' }),
+        {
+          status: 404,
+          headers: { 'Content-Type': 'application/json' },
+        }
+      )
+    }
     return new Response(JSON.stringify(result), {
       status: 200,
       headers: {
@@ -14,6 +27,7 @@ export const GET: APIRoute = rateLimit(async ({ url }) => {
       },
     })
   } catch (_error) {
+    console.error(_error)
     return new Response(
       JSON.stringify({ error: 'Ocurrió un error en el servidor.' }),
       {
