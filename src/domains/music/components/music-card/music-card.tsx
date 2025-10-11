84c693cdcc7a8ae2ce4a8f7d7c116f7bdbc946fc
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-11 16:56:24 -0500

    Refactor className in AddToPlayListButton within MusicCard component to improve code clarity by rearranging class order.

--- src/domains/music/components/music-card/music-card.tsx
+++ src/domains/music/components/music-card/music-card.tsx
@@ -144,7 +144,7 @@ export const MusicCard = ({
           <AddToPlayListButton
             song={song}
             isInPlayList={isInPlaylist}
-            className={`hover:text-enfasisColor group cursor-pointer rounded-md transition-all duration-300 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 p-1 text-sm`}
+            className={`hover:text-enfasisColor group cursor-pointer rounded-md p-1 text-sm transition-all duration-300 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50`}
           />
 
           <DownloadButton
