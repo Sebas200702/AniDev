38121d87cd9f4b0d7b1b5b2e29323d60d48609e9
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-11 17:03:36 -0500

    Add useSearchHistory hook to manage search history functionality, enabling saving and loading of search history based on user interactions and applied filters for improved user experience.

---
+++ src/domains/search/hooks/useSearchHistory.ts
@@ -0,0 +1,34 @@
+import type { AppliedFilters, SearchHistory } from '@search/types'
+import type { UserInfo } from '@user/types'
+import { loadSearchHistory } from '@utils/load-search-history'
+import { saveSearchHistory } from '@utils/save-search-history'
+import { useEffect } from 'react'
+
+export const useSearchHistory = (
+  searchHistory: SearchHistory[],
+  trackSearchHistory: boolean,
+  query: string,
+  appliedFilters: AppliedFilters,
+  userInfo: UserInfo | null,
+  setSearchHistory: (history: any[]) => void
+) => {
+  useEffect(() => {
+    if (
+      !searchHistory ||
+      !trackSearchHistory ||
+      (!query && Object.keys(appliedFilters).length === 0)
+    )
+      return
+    saveSearchHistory(searchHistory, userInfo)
+  }, [searchHistory, trackSearchHistory, query, appliedFilters, userInfo])
+
+  // Cargar historial
+  useEffect(() => {
+    if (
+      !trackSearchHistory ||
+      (!query && Object.keys(appliedFilters).length === 0)
+    )
+      return
+    loadSearchHistory(userInfo).then(setSearchHistory)
+  }, [trackSearchHistory, query, appliedFilters, userInfo, setSearchHistory])
+}
