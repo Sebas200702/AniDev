c7315febde563c63cca29d49c2e3e94d06bfbdf5
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-11 17:05:16 -0500

    Remove unnecessary line break in ModalDefaultContainer component for improved code clarity.

--- src/domains/shared/components/modal/modal-default-container.tsx
+++ src/domains/shared/components/modal/modal-default-container.tsx
@@ -4,9 +4,7 @@ export const ModalDefaultContainer = ({
   children: React.ReactNode
 }) => {
   return (
-    <div
-      className="bg-Complementary border-enfasisColor/30 relative flex max-w-[90vw] flex-col items-center justify-center gap-6 rounded-lg border p-6 shadow-2xl backdrop-blur-sm"
-    >
+    <div className="bg-Complementary border-enfasisColor/30 relative flex max-w-[90vw] flex-col items-center justify-center gap-6 rounded-lg border p-6 shadow-2xl backdrop-blur-sm">
       {children}
     </div>
   )
