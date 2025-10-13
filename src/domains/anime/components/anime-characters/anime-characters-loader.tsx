6093ceb9165c91bd236545e986417a4351c7054f
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-13 09:04:13 -0500

    Refactor AnimeCharacterLoader to use Loader component for improved loading animation and code consistency.

--- src/domains/anime/components/anime-characters/anime-characters-loader.tsx
+++ src/domains/anime/components/anime-characters/anime-characters-loader.tsx
@@ -1,13 +1,15 @@
+import { Loader } from '@shared/components/loader'
 export const AnimeCharacterLoader = () => {
   return (
     <>
       {Array.from({ length: 20 }).map((_, index) => (
-        <li key={index}>
-          <div className="flex aspect-[100/30] h-full w-full animate-pulse flex-row justify-between overflow-hidden rounded-md bg-zinc-800 duration-300">
-            <div className="aspect-[225/330] h-full animate-pulse bg-zinc-700 object-cover object-center transition-all ease-in-out md:max-h-36"></div>
-            <div className="aspect-[225/330] h-full animate-pulse bg-zinc-700 object-cover object-center transition-all ease-in-out md:max-h-36"></div>
-          </div>
-        </li>
+        <Loader
+          key={index}
+          className="flex aspect-[100/30] h-full w-full  flex-row justify-between overflow-hidden  bg-zinc-800 "
+        >
+          <Loader className="aspect-[225/330] h-full  bg-zinc-700 object-cover object-center  md:max-h-36" />
+          <Loader className="aspect-[225/330] h-full bg-zinc-700 object-cover object-center  md:max-h-36" />
+        </Loader>
       ))}
     </>
   )
