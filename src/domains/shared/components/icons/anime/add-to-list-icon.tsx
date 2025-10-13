e6566f602eec83167ba00602ad0e6e7a6a2a7f9e
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-13 09:30:01 -0500

    Adjust stroke width and simplify SVG paths in AddToListIcon component for improved visual consistency and clarity.

--- src/domains/shared/components/icons/anime/add-to-list-icon.tsx
+++ src/domains/shared/components/icons/anime/add-to-list-icon.tsx
@@ -25,15 +25,10 @@ export const AddToListIcon = ({ className }: IconProps) => (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    strokeWidth="2"
+    strokeWidth="1.75"
     strokeLinecap="round"
     strokeLinejoin="round"
     className={className}
   >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M7 3m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
-    <path d="M4.012 7.26a2.005 2.005 0 0 0 -1.012 1.737v10c0 1.1 .9 2 2 2h10c.75 0 1.158 -.385 1.5 -1" />
-    <path d="M11 10h6" />
-    <path d="M14 7v6" />
-  </svg>
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
 )
