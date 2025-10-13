e23de4797db80c821666cb2192717b01aa94242b
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-13 09:03:56 -0500

    Refactor AnimeCharacterList component to utilize DataWrapper for improved data handling, loading states, and error management, enhancing user experience.

--- src/domains/anime/components/anime-characters/anime-characters-list.tsx
+++ src/domains/anime/components/anime-characters/anime-characters-list.tsx
@@ -3,76 +3,61 @@ import { AnimeCharacterCard } from '@character/components/character-card/detail-
 import type { Character } from '@character/types'
 import { FilterDropdown } from '@search/components/search-filters/filter-dropdown'
 import { languageOptions } from '@search/utils/constants'
+import { DataWrapper } from '@shared/components/data-wrapper'
 
 interface Props {
-  characters: Character[] | null
   language: string
-  setLanguage: (language: string) => void
-  isLoading?: boolean
+  setLanguage: (lang: string) => void
+  characters: Character[]
+  isLoading: boolean
+  error?: Error | null
 }
 
-const Container = ({
-  children,
-  actions,
-}: {
-  children: React.ReactNode
-  actions?: React.ReactNode
-}) => (
-  <section className="relative z-10 flex flex-col gap-8 rounded-lg p-4 md:p-6">
-    <header className="flex items-center justify-between">
-      <h2 className="text-lxx font-bold text-white">Characters</h2>
-      {actions}
-    </header>
-    <div className="grid grid-cols-1 gap-4 md:gap-6 xl:grid-cols-2">
-      {children}
-    </div>
-  </section>
-)
-
 export const AnimeCharacterList = ({
-  characters,
   language,
   setLanguage,
+  characters,
   isLoading,
+  error,
 }: Props) => {
-  const filter = (
-    <FilterDropdown
-      label="Language"
-      styles="max-w-44 mx-0"
-      values={[language]}
-      onChange={(values) => setLanguage(values[0] ?? '')}
-      onClear={() => setLanguage('')}
-      options={languageOptions}
-      singleSelect
-    />
-  )
-
-  if (characters?.length === 0) {
-    return (
-      <Container actions={filter}>
-        <p className="text-md text-center font-medium text-white">
-          No characters found.
-        </p>
-      </Container>
-    )
-  }
-
-  if (isLoading || !characters) {
-    return (
-      <Container actions={filter}>
-        <AnimeCharacterLoader />
-      </Container>
-    )
-  }
-
   return (
-    <Container actions={filter}>
-      {characters.map((character) => (
-        <AnimeCharacterCard
-          character={character}
-          key={character.character_id}
+    <section className="relative z-10 flex flex-col gap-8 rounded-lg p-4 md:p-6">
+      <header className="flex items-center justify-between">
+        <h2 className="text-lxx font-bold text-white">Characters</h2>
+        <FilterDropdown
+          label="Language"
+          styles="max-w-44 mx-0"
+          values={[language]}
+          onChange={(values) => setLanguage(values[0] ?? '')}
+          onClear={() => setLanguage('')}
+          options={languageOptions}
+          singleSelect
         />
-      ))}
-    </Container>
+      </header>
+      <div className="grid grid-cols-1 gap-4 md:gap-6 xl:grid-cols-2">
+        <DataWrapper<Character[]>
+          data={characters}
+          loading={isLoading}
+          error={error}
+          noDataFallback={
+            <div className="text-gray-400 p-6 text-center">
+              No characters found
+            </div>
+          }
+          loadingFallback={<AnimeCharacterLoader />}
+        >
+          {(characters) => (
+            <>
+              {characters.map((character) => (
+                <AnimeCharacterCard
+                  key={character.character_id}
+                  character={character}
+                />
+              ))}
+            </>
+          )}
+        </DataWrapper>
+      </div>
+    </section>
   )
 }
