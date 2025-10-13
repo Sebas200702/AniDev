b48fe5c425f86f2399c824f4c6870d16596d46a1
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-13 09:06:13 -0500

    Add AnimeCollectionLoader component to display loading state for anime collections, enhancing user experience with a consistent loading animation.

---
+++ src/domains/anime/components/anime-collection/anime-coletion-loader.tsx
@@ -0,0 +1,7 @@
+import { Loader } from "@shared/components/loader"
+
+export const AnimeCollectionLoader = () => {
+  return (
+    <Loader className="flex h-54 w-full  items-center justify-center  bg-zinc-800" />
+  )
+}
