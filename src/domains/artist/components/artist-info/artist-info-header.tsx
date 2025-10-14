182c7be1b3ce838ef793953e556312e17ce907af
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-14 08:32:46 -0500

    Add ArtistHeader component to display artist name, enhancing the artist profile experience with a dedicated header for better visibility.

---
+++ src/domains/artist/components/artist-info/artist-info-header.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  name: string
+}
+
+export const ArtistHeader = ({ name }: Props) => {
+  return (
+    <div className="z-20 flex flex-col gap-4">
+      <h1 className="text-3xl font-bold text-white">{name}</h1>
+    </div>
+  )
+}
