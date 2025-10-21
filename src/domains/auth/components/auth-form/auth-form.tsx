cfe474839d25a9bbd5c7ef3a92b9455b8b0ff893
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-21 15:33:12 -0500

    Refactor AuthForm component: Update imports and remove unused props for improved clarity and maintainability in the authentication form layout.

--- src/domains/auth/components/auth-form/auth-form.tsx
+++ src/domains/auth/components/auth-form/auth-form.tsx
@@ -1,24 +1,20 @@
-import { Aside } from '@auth/components/auth-layout/aside'
-import { Main } from '@auth/components/auth-layout/main'
+import { Aside } from '@auth/components/auth-layout/auth-layout-aside'
+import { MainContainer } from '@auth/components/auth-layout/auth-layout-main-container'
 
 interface Props {
   bgImage: string
-  isLoading: boolean
   isSignUp: boolean
-  title: string
 }
 
 export const AuthFormulary = ({
   bgImage,
-  isLoading,
-  title,
   isSignUp,
 }: Props) => {
   return (
-    <section className="flex h-full max-h-[75vh] w-full items-center justify-center px-4 text-white md:mt-20 md:px-20">
+    <section className="flex h-full max-h-[75vh] w-full items-center justify-center  text-white md:mt-20 md:px-20">
       <article className="bg-Complementary border-enfasisColor/30 relative flex h-full w-full max-w-7xl overflow-hidden rounded-lg border-1">
         <Aside bgImage={bgImage} isSignUp={isSignUp} />
-        <Main isLoading={isLoading} isSignUp={isSignUp} title={title} />
+        <MainContainer  />
       </article>
     </section>
   )
