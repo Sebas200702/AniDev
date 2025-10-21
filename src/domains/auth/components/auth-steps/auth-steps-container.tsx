d18b5ea505ea6031834c01f063a217d8de896b0f
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-21 15:54:18 -0500

    Add StepsContainer component for managing authentication steps: Introduce a new StepsContainer component that utilizes the auth form store to dynamically render authentication steps, improving the user experience during the sign-up and login processes.

---
+++ src/domains/auth/components/auth-steps/auth-steps-container.tsx
@@ -0,0 +1,19 @@
+import { useAuthFormStore } from '@auth/stores/auth-store'
+import { Step } from '@auth/components/auth-steps/auth-step'
+export const StepsContainer = () => {
+  const { steps, currentStep, mode } = useAuthFormStore()
+  const isSignUp = mode === 'signUp'
+
+  return (
+    <ul className='flex gap-4'>
+      {steps.map((step) => (
+        <Step
+          key={step.id}
+          currentStep={currentStep}
+          step={step}
+          isSignUp={isSignUp}
+        />
+      ))}
+    </ul>
+  )
+}
