4efcf8db6a48f4299118e140fa4151eb38c0d6e4
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-27 18:13:15 -0500

    Refactor sign-up schema validation to improve clarity and maintainability: Simplify password validation messages and remove unused fields from the signUpSchema, enhancing the overall structure and readability of the authentication schema.

--- src/domains/auth/schemas/signup.ts
+++ src/domains/auth/schemas/signup.ts
@@ -1,3 +1,4 @@
+// @auth/schemas/signup.ts
 import { z } from 'zod'
 
 export const signUpSchema = z.object({
@@ -5,48 +6,13 @@ export const signUpSchema = z.object({
   password: z
     .string()
     .min(6, 'The password must be at least 6 characters long')
-    .regex(
-      /(?=.*[a-z])/,
-      'The password must contain at least one lowercase letter'
-    )
-    .regex(
-      /(?=.*[A-Z])/,
-      'The password must contain at least one uppercase letter'
-    )
-    .regex(/(?=.*\d)/, 'The password must contain at least one number')
-    .regex(/(?=.*[!@#$%^&*])/, 'The password must contain at least one symbol')
-    .max(20, 'The password cannot have more than 20 characters'),
+    .regex(/(?=.*[a-z])/, 'Must contain at least one lowercase letter')
+    .regex(/(?=.*[A-Z])/, 'Must contain at least one uppercase letter')
+    .regex(/(?=.*\d)/, 'Must contain at least one number')
+    .regex(/(?=.*[!@#$%^&*])/, 'Must contain at least one symbol')
+    .max(20, 'The password cannot exceed 20 characters'),
   user_name: z.string().min(3),
-  avatar: z.string().url().optional(),
-  name: z.string().min(1),
-  last_name: z.string().min(1),
-  birthday: z.string().optional(),
-  gender: z.enum(['male', 'female', 'other']),
-  favorite_animes: z.array(z.string()).optional(),
-  favorite_genres: z.array(z.string()).optional(),
-  favorite_studios: z.array(z.string()).optional(),
-  frequency: z.string().optional(),
-  fanatic_level: z.string().optional(),
-  preferred_format: z.string().optional(),
-  watched_animes: z.array(z.string()).optional(),
 })
 
 export type SignUpSchema = z.infer<typeof signUpSchema>
 
-// Schema for Google signup (omits step 1 fields)
-export const signUpGoogleSchema = z.object({
-  avatar: z.string().url().optional(),
-  name: z.string().min(1),
-  last_name: z.string().min(1),
-  birthday: z.string().optional(),
-  gender: z.enum(['male', 'female', 'other']),
-  favorite_animes: z.array(z.string()).optional(),
-  favorite_genres: z.array(z.string()).optional(),
-  favorite_studios: z.array(z.string()).optional(),
-  frequency: z.string().optional(),
-  fanatic_level: z.string().optional(),
-  preferred_format: z.string().optional(),
-  watched_animes: z.array(z.string()).optional(),
-})
-
-export type SignUpGoogleSchema = z.infer<typeof signUpGoogleSchema>
