30bc43126315a6c9ca4a158a9e86b51a26a1b027
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-27 18:13:38 -0500

    Add Zustand store for authentication form management: Implement useAuthFormStore to handle sign-in and sign-up processes, including state management for steps, values, and submission logic, enhancing user experience and maintainability.

---
+++ src/domains/auth/stores/auth-store.ts
@@ -0,0 +1,101 @@
+import { authService } from '@auth/services/auth-service'
+import type { AuthStep } from '@auth/types'
+import { stepsSignIn, stepsSignUp } from '@auth/utils/steps'
+import { userProfileService } from '@user/services/user-profile-service'
+import { z } from 'zod'
+import { create } from 'zustand'
+
+type AuthMode = 'signIn' | 'signUp'
+
+interface AuthFormState {
+  mode: AuthMode | null
+  currentStep: number
+  steps: AuthStep[]
+  values: Record<string, any>
+  error: string | null
+  loading: boolean
+  isGoogleAuth: boolean
+  setMode: (mode: AuthMode) => void
+  setCurrentStep: (step: number) => void
+  setField: (field: string, value: any) => void
+  setGoogleAuth: (isGoogle: boolean) => void
+  nextStep: () => void
+  prevStep: () => void
+  submit: () => Promise<{ success : boolean, message: string }>
+  reset: () => void
+}
+
+export const useAuthFormStore = create<AuthFormState>((set, get) => ({
+  mode: null,
+  currentStep: 1,
+  steps: stepsSignIn,
+  values: {},
+  error: null,
+  loading: false,
+  isGoogleAuth: false,
+
+  setMode: (mode) => {
+    set({
+      mode,
+      currentStep: 1,
+      steps: mode === 'signUp' ? stepsSignUp : stepsSignIn,
+      values: {},
+      error: null,
+    })
+  },
+  setCurrentStep(step) {
+    set({
+      currentStep: step,
+    })
+  },
+
+  setField: (field, value) =>
+    set((state) => ({
+      values: { ...state.values, [field]: value },
+    })),
+
+  setGoogleAuth: (isGoogle) => set({ isGoogleAuth: isGoogle }),
+
+  nextStep: () => {
+    const { currentStep, steps } = get()
+    if (currentStep < steps.length) set({ currentStep: currentStep + 1 })
+  },
+
+  prevStep: () => {
+    const { currentStep } = get()
+    if (currentStep > 1) set({ currentStep: currentStep - 1 })
+  },
+
+  submit: async () => {
+    const { mode, values, isGoogleAuth } = get()
+    try {
+      set({ loading: true, error: null })
+
+      if (mode === 'signUp') {
+        const { email, password, user_name, ...profileData } = values
+
+        if (!isGoogleAuth) {
+          await authService.signUp({ email, password, user_name })
+        }
+
+        await userProfileService.createProfile(profileData)
+        return { success: true, message: 'Account created successfully 🎉' }
+      } else {
+        const { email, password } = values
+        await authService.signIn({ email, password })
+        return { success: true, message: 'Login successful 🎉' }
+      }
+    } catch (err: any) {
+        const message =
+        err instanceof z.ZodError
+          ? err.errors[0]?.message
+          : err.message 
+      set({ error: message })
+      return { success: false, message }
+    } finally {
+      set({ loading: false })
+    }
+  },
+
+  reset: () => set({ values: {}, currentStep: 1, error: null }),
+}))
