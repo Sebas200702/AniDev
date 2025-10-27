2518d6fa64c206bbb8e93d91ba09a0c7505c22d3
Sebas200702 <sebastorregroza6@gmail.com>
2025-10-27 18:13:56 -0500

    Update API endpoint in useCharacterList hook: Change the fetch URL to a more concise path, improving code clarity and maintainability.

--- src/domains/character/hooks/useCharacterList.ts
+++ src/domains/character/hooks/useCharacterList.ts
@@ -5,7 +5,7 @@ import { useState } from 'react'
 export const useCharacterList = ({ animeId }: { animeId: number }) => {
   const [language, setLanguage] = useState('Japanese')
   const { data, loading, error } = useFetch<Character[]>({
-    url: `/api/getAnimeCharacters?animeId=${animeId}&language=${language}`,
+    url: `getAnimeCharacters?animeId=${animeId}&language=${language}`,
   })
 
   return {
