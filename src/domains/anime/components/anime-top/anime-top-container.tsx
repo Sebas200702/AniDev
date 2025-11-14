4ce91ac57e779ff315e19421d6f425d2b2385902
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-13 22:43:30 -0500

    Add refetch functionality in AnimeTopContainer: Enhance data handling by allowing users to retry fetching data on error, improving overall user experience.

--- src/domains/anime/components/anime-top/anime-top-container.tsx
+++ src/domains/anime/components/anime-top/anime-top-container.tsx
@@ -5,7 +5,7 @@ import { DataWrapper } from '@shared/components/data-wrapper'
 import { useFetch } from '@shared/hooks/useFetch'
 
 export const AnimeTopContainer = () => {
-  const { data, loading, error } = useFetch<AnimeTopInfo[]>({
+  const { data, loading, error, refetch } = useFetch<AnimeTopInfo[]>({
     url: '/animes?order_by=score&limit_count=10&type_filter=tv&banner_filter=false&format=top-anime',
   })
 
@@ -14,6 +14,7 @@ export const AnimeTopContainer = () => {
       data={data}
       loading={loading}
       error={error}
+      onRetry={refetch}
       loadingFallback={<AnimeTopLoader />}
       noDataFallback={<AnimeTopLoader />}
     >
