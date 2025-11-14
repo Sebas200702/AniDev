0008de35984e6dbdc4b801173d025a3ed1637671
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-13 23:39:38 -0500

    Add pickAnimeForJikan utility: Implement function to randomly select a favorite anime from the user's profile, enhancing the recommendation system's ability to provide personalized suggestions based on user preferences.

---
+++ src/domains/recomendations/utils/pick-anime-for-jikan.ts
@@ -0,0 +1,14 @@
+import type { UserProfile } from '@user/types'
+import { getFavoriteAnimeIds } from '@utils/get-favorite-anime-ids'
+
+export async function pickAnimeForJikan(userProfile: UserProfile) {
+  if (!userProfile.favorite_animes?.length) return {}
+  const favs = await getFavoriteAnimeIds(userProfile.favorite_animes)
+  if (favs.error || !favs.mal_ids.length) return {}
+
+  const idx = Math.floor(Math.random() * favs.mal_ids.length)
+  return {
+    selectedFavoriteTitle: favs.matchedTitles[idx],
+    selectedFavoriteId: favs.mal_ids[idx]
+  }
+}
