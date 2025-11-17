b411e1d3d1016d6f8bf2b99161cb457582a1b76f
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-16 23:14:19 -0500

    feat: implement authApiClient for user sign up and sign in functionality

---
+++ src/domains/auth/helpers/api-client.ts
@@ -0,0 +1,25 @@
+import { type SignInSchema } from '@auth/schemas/sigin'
+import { type SignUpSchema } from '@auth/schemas/signup'
+import { api } from '@libs/api'
+
+export const authApiClient = {
+  async signUp(values: SignUpSchema) {
+    const res = await api.post('/auth/signup', values)
+    
+    if (res.status !== 200) {
+      throw new Error(res.error?.message || 'Error en el registro')
+    }
+
+    return res.data
+  },
+
+  async signIn(values: SignInSchema) {
+    const res = await api.post('/auth/signin', values)
+    
+    if (res.status !== 200) {
+      throw new Error(res.error?.message || 'Error al iniciar sesión')
+    }
+
+    return res.data
+  },
+}
