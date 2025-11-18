bb44ac3fb73fadac4d4fb18b93abcdcad9595b27
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-18 16:34:17 -0500

    fix: reorder import statements for improved organization

--- src/domains/anime/components/anime-carousel/anime-carousel-container.tsx
+++ src/domains/anime/components/anime-carousel/anime-carousel-container.tsx
@@ -1,24 +1,25 @@
 import { useCallback, useEffect, useMemo } from 'react'
 
 import { AnimeCarousel } from '@anime/components/anime-carousel/anime-carousel'
+import { LoadingCarousel } from '@anime/components/anime-carousel/anime-carousel-loader'
 import { useCarouselScroll } from '@anime/hooks/useCarouselScroll'
 import { useCarouselStore } from '@anime/stores/carousel-store'
 import type { AnimeBannerInfo } from '@anime/types'
 import { createDynamicUrl } from '@anime/utils/create-dynamic-url'
 import { DataWrapper } from '@shared/components/data-wrapper'
 import { useFetchWithCache } from '@shared/hooks/useFetchWithCache'
-import { LoadingCarousel } from '@anime/components/anime-carousel/anime-carousel-loader'
 
 export const AnimeCarouselContainer = () => {
   const { banners, setBanners, currentIndex, setCurrentIndex } =
     useCarouselStore()
   const { url } = useMemo(() => createDynamicUrl(6), [])
 
-  const { data, loading, error, refetch, retryCount, maxRetries } = useFetchWithCache<AnimeBannerInfo[]>({
-    url: `${url}&banners_filter=true&format=anime-banner`,
-    sectionId: 'carousel',
-    limit: 6,
-  })
+  const { data, loading, error, refetch, retryCount, maxRetries } =
+    useFetchWithCache<AnimeBannerInfo[]>({
+      url: `${url}&banners_filter=true&format=anime-banner`,
+      sectionId: 'carousel',
+      limit: 6,
+    })
 
   useEffect(() => {
     if (data) setBanners(data)
