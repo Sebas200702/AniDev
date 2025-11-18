57bd983cadf85341252985d8af6e9a1f2e8d43dd
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-18 16:35:30 -0500

    fix: update AnimeCollectionContainer to handle custom URLs more effectively

--- src/domains/anime/components/anime-collection/anime-collection-container.tsx
+++ src/domains/anime/components/anime-collection/anime-collection-container.tsx
@@ -3,18 +3,23 @@ import { AnimeCollection } from '@anime/components/anime-collection/anime-collec
 import type { AnimeCollectionInfo } from '@anime/types'
 import { createDynamicUrl } from '@anime/utils/create-dynamic-url'
 import { DataWrapper } from '@shared/components/data-wrapper'
-import { useFetch } from '@shared/hooks/useFetch'
+import { useFetchWithCache } from '@shared/hooks/useFetchWithCache'
 import { useMemo } from 'react'
 
 interface Props {
   id: number
+  url?: string
 }
 
-export const AnimeCollectionContainer = ({ id }: Props) => {
-  const { url, title } = useMemo(() => createDynamicUrl(3), [])
+export const AnimeCollectionContainer = ({ id, url: customUrl }: Props) => {
+  const { url: fallbackUrl, title } = useMemo(() => createDynamicUrl(3), [])
 
-  const { data, loading, error } = useFetch<AnimeCollectionInfo[]>({
-    url: `${url}&format=anime-collection`,
+  const { data, loading, error, retryCount, maxRetries } = useFetchWithCache<
+    AnimeCollectionInfo[]
+  >({
+    url: `${customUrl || fallbackUrl}&format=anime-collection`,
+    sectionId: `collection-${id}`,
+    limit: 3,
   })
 
   return (
@@ -22,6 +27,7 @@ export const AnimeCollectionContainer = ({ id }: Props) => {
       data={data}
       loading={loading}
       error={error}
+      retryCount={maxRetries - retryCount}
       loadingFallback={<AnimeCollectionLoader />}
       noDataFallback={<AnimeCollectionLoader />}
     >
