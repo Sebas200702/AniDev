cb3a63284e1a2c5ebec69328227a314d94d9f972
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-18 01:04:08 -0500

    refactor: streamline anime search logic by integrating AnimeController

--- src/pages/api/animes/index.ts
+++ src/pages/api/animes/index.ts
@@ -1,9 +1,7 @@
-import { AnimeService } from '@anime/services'
+import { AnimeController } from '@anime/controlers'
 import { rateLimit } from '@middlewares/rate-limit'
 import { redisConnection } from '@middlewares/redis-connection'
-import { Filters } from '@shared/types'
 import { CacheTTL, CacheUtils } from '@utils/cache-utils'
-import { getFilters } from '@utils/get-filters-of-search-params'
 import { ResponseBuilder } from '@utils/response-builder'
 import type { APIRoute } from 'astro'
 
@@ -15,38 +13,9 @@ export const GET: APIRoute = rateLimit(
         url.searchParams
       )
 
-      const format = url.searchParams.get('format') ?? 'anime-card'
-      const limit = Number.parseInt(url.searchParams.get('limit_count') ?? '10')
-      const page = Number.parseInt(url.searchParams.get('page_number') ?? '1')
-
-      const CountFilters = Object.keys(Filters).filter(
-        (key) =>
-          key !== 'limit_count' && key !== 'page_number' && key !== 'order_by'
-      )
-
-      const filters = getFilters(Object.values(Filters), url, true)
-      const countFilters = getFilters(CountFilters, url, false)
-
       const result = await CacheUtils.withCache(
         cacheKey,
-        async () => {
-          const { data, total } = await AnimeService.searchAnime({
-            format,
-            filters,
-            countFilters,
-          })
-
-          if (!data || data.length === 0) {
-            throw new Error('No se encontraron animes.')
-          }
-
-          return {
-            total_items: total,
-            data,
-            current_page: page,
-            last_page: Math.ceil(total / limit),
-          }
-        },
+        () => AnimeController.handleSearchAnime(url),
         { ttl: CacheTTL.ONE_HOUR * 2 }
       )
 
