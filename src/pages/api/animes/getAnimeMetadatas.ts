e5792e1235232e29eb77468ca5b1e25c850edc94
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-25 20:58:17 -0500

    refactor: remove caching logic from GET /api/animes/getAnimeMetadatas endpoint

--- src/pages/api/animes/getAnimeMetadatas.ts
+++ src/pages/api/animes/getAnimeMetadatas.ts
@@ -1,21 +1,18 @@
 import { AnimeController } from '@anime/controlers'
 import { rateLimit } from '@middlewares/rate-limit'
 import { redisConnection } from '@middlewares/redis-connection'
-import { CacheTTL, CacheUtils } from '@utils/cache-utils'
+
 import { ResponseBuilder } from '@utils/response-builder'
 import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = rateLimit(
   redisConnection(async ({ url }) => {
     try {
-      const id = url.searchParams.get('id')
-      const cacheKey = CacheUtils.generateKey('anime-metadatas', id ?? '')
 
-      const metadata = await CacheUtils.withCache(
-        cacheKey,
-        () => AnimeController.handleGetAnimeMetadata(url),
-        { ttl: CacheTTL.ONE_DAY }
-      )
+
+
+        const metadata = await AnimeController.handleGetAnimeMetadata(url)
+
 
       return ResponseBuilder.success(metadata)
     } catch (error) {
