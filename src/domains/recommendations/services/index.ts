e74e81d5dcf6387517bb9d08a9b5c40f9fada139
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-26 13:56:30 -0500

    refactor(recommendations): simplify recommendations service by removing unused functions

--- src/domains/recommendations/services/index.ts
+++ src/domains/recommendations/services/index.ts
@@ -1,37 +1,13 @@
-import type {
-  FetchRecommendationProps,
-  GenerateRecommendationsProps,
-} from '@recommendations/types'
-import { fetchRecommendations } from '@recommendations/utils/fetch-recommendations'
-import { generateUserRecommendations } from '@recommendations/utils/generate-recomenations'
-import { fetchJikanRecommendations } from '@recommendations/utils/get-jikan-recommendations'
-import { generateContextualPrompt } from '@recommendations/utils/get-recomendation-context'
-import { getUserDataToRecommendations } from '@recommendations/utils/get-user-data-to-recommendations'
-import type { ApiResponse } from '@shared/types/api-response'
-import type { GeneratePromptProps } from 'domains/ai/types'
+import { generatePrompt } from '@recommendations/services/generate-prompt'
+import { generateRecommendations } from '@recommendations/services/generate-recommendations'
+import { getJikanRecommendations } from '@recommendations/services/get-jikan-recommendations'
+import { getRecommendations } from '@recommendations/services/get-recommendations'
+import { getUserPreferences } from '@recommendations/services/get-user-preferences'
 
 export const recommendationsService = {
-  async getRecommendations(
-    props: FetchRecommendationProps
-  ): Promise<ApiResponse<any[]>> {
-    const data = await fetchRecommendations(props)
-    return { data }
-  },
-
-  async getUserPreferences(userId: string | null) {
-    return getUserDataToRecommendations(userId)
-  },
-
-  async generatePrompt(props: GeneratePromptProps) {
-    return generateContextualPrompt(props)
-  },
-  async generateRecommendations(
-    props: GenerateRecommendationsProps
-  ): Promise<ApiResponse<any[]>> {
-    const data = await generateUserRecommendations(props)
-    return { data }
-  },
-  async getJikanRecommendations(mal_id: string) {
-    return await fetchJikanRecommendations(mal_id)
-  },
+  getRecommendations,
+  getUserPreferences,
+  generatePrompt,
+  generateRecommendations,
+  getJikanRecommendations,
 }
