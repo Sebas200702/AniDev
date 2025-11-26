5c2293ca9940ee0d03eb4b2a77958ac91901812e
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-26 09:14:57 -0500

    refactor: replace useBlockedContent with useFetch for improved data handling

--- src/domains/anime/components/anime-info/anime-info-container.tsx
+++ src/domains/anime/components/anime-info/anime-info-container.tsx
@@ -1,26 +1,26 @@
 import { AnimeInfo } from '@anime/components/anime-info/anime-info'
 import { AnimeLoader } from '@anime/components/anime-info/anime-info-loader'
+import type { Anime } from '@anime/types'
 import { DataWrapper } from '@shared/components/data-wrapper'
-import { useBlockedContent } from '@shared/hooks/useBlockedContent'
-import { getAnimeData } from '@utils/get-anime-data'
+import { useFetch } from '@shared/hooks/useFetch'
 
 interface Props {
   id: number
 }
 
 export const AnimeInfoContainer = ({ id }: Props) => {
-  const { animeData, isBlocked, isLoading, error } =
-    useBlockedContent({
-      id,
-      getAnimeData,
-    })
-
-
+  const {
+    data: animeData,
+    loading,
+    error,
+  } = useFetch<Anime>({
+    url: `/animes/getAnime?id=${id}`,
+  })
 
   return (
     <DataWrapper
-      data={isBlocked ? null : animeData}
-      loading={isLoading}
+      data={animeData}
+      loading={loading}
       error={error}
       loadingFallback={<AnimeLoader />}
       noDataFallback={
