f4033b419061cc33d25d2fd58b868d9ea941087c
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-26 11:08:46 -0500

    refactor: add quotes around 'unknown' status for consistency in statusColors function

--- src/utils/status-colors.ts
+++ src/utils/status-colors.ts
@@ -25,7 +25,7 @@ export const statusColors = (status: string) => {
     'Currently Airing' = 'bg-green-400 md:group-hover:bg-green-500',
     'Finished Airing' = 'bg-blue-500 md:group-hover:bg-blue-600',
     'Not yet aired' = 'bg-yellow-500 md:group-hover:bg-yellow-600',
-    unknown = 'bg-gray-400 md:group-hover:bg-gray-500',
+    'unknown' = 'bg-gray-400 md:group-hover:bg-gray-500',
   }
 
   return COLORS[status as keyof typeof COLORS]
