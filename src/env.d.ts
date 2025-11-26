11eba28d38da91d8f6435cd1b0d8e296af028421
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-26 12:38:57 -0500

    chore: update dependencies and env types
    Updated package.json, pnpm-lock.yaml, and env.d.ts to reflect recent changes and dependency updates.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
+import type { UserInfo } from '@user/types'
+
+declare global {
+  namespace App {
+    interface Locals {
+      userInfo: UserInfo | null
+    }
+  }
+}
 
 interface ImportMetaEnv {
   readonly SUPABASE_URL: string
