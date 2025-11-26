567b4863a8af80570ca318194ec126f6591c9412
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-25 20:16:22 -0500

    feat: implement user data retrieval logic in getUserDataToRecomendations

--- src/domains/recomendations/utils/get-user-data-to-recomendations.ts
+++ src/domains/recomendations/utils/get-user-data-to-recomendations.ts
@@ -1,6 +1,9 @@
 import { supabase } from '@libs/supabase'
+import type { UserProfileToRecomendations } from '@recomendations/types'
 
-const userPlaceholder = {
+
+
+const userPlaceholder: UserProfileToRecomendations = {
   name: 'Usuario',
   last_name: 'Anónimo',
   gender: 'No especificado',
@@ -29,7 +32,11 @@ const userPlaceholder = {
 
 export const getUserDataToRecomendations = async (
   userId: string | null | undefined
-) => {
+): Promise<{
+  userProfile: UserProfileToRecomendations | null
+  calculatedAge: number | null
+  error: string | null
+}> => {
   if (!userId) {
     return {
       userProfile: userPlaceholder,
@@ -41,9 +48,10 @@ export const getUserDataToRecomendations = async (
   const { data: userProfile, error: userProfileError } = await supabase
     .from('user_profiles')
     .select(
-      'search_history(search_history), favorite_animes, favorite_genres, favorite_studios, frequency_of_watch, fanatic_level, gender, last_name, name, preferred_format, birthday, watched_animes'
+      'search_history, favorite_animes, favorite_genres, favorite_studios, frequency_of_watch, fanatic_level, gender, last_name, name, preferred_format, birthday, watched_animes'
     )
     .eq('id', userId)
+    .single()
 
   if (userProfileError) {
     return {
@@ -54,10 +62,10 @@ export const getUserDataToRecomendations = async (
   }
 
   const calculatedAge =
-    new Date().getFullYear() - new Date(userProfile[0].birthday).getFullYear()
+    new Date().getFullYear() - new Date(userProfile.birthday).getFullYear()
 
   return {
-    userProfile: userProfile[0],
+    userProfile,
     calculatedAge,
     error: null,
   }
