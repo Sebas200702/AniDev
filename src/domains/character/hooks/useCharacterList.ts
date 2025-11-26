1204843d798a51b9b7c31312ef670f4ee8048e23
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-26 14:46:13 -0500

    fix(useCharacterList): add newline for improved code readability

--- src/domains/character/hooks/useCharacterList.ts
+++ src/domains/character/hooks/useCharacterList.ts
@@ -8,6 +8,7 @@ export const useCharacterList = ({ animeId }: { animeId: number }) => {
     url: `/characters/getAnimeCharacters?animeId=${animeId}&language=${language}`,
   })
 
+
   return {
     language,
     setLanguage,
