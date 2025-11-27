1d539d0c20ee256a10402291dd9d581946d09886
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-26 19:55:59 -0500

    fix(signup-api): correct data structure in success response for sign up

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -8,11 +8,13 @@ export const POST: APIRoute = rateLimit(async ({ request, cookies }) => {
     const result = await AuthController.handleSignUp(request, cookies)
 
     return ResponseBuilder.success({
-      user: {
-        name: result.user.user_metadata.user_name,
-        avatar: result.user.user_metadata.avatar_url,
+      data: {
+        user: {
+          name: result.data.user.user_metadata.user_name,
+          avatar: result.data.user.user_metadata.avatar_url,
+        },
+        session: result.data.session,
       },
-      session: result.session,
     })
   } catch (error) {
     return ResponseBuilder.fromError(error, 'POST /api/auth/signup')
