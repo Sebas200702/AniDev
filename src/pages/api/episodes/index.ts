7a6463f1bacb4cd54fea87e12fc275af7bc8aadf
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-26 19:29:15 -0500

    fix(episode-api): simplify episode retrieval logic by removing unnecessary middleware

--- src/pages/api/episodes/index.ts
+++ src/pages/api/episodes/index.ts
@@ -1,6 +1,5 @@
 import { rateLimit } from '@middlewares/rate-limit'
 import { redisConnection } from '@middlewares/redis-connection'
-import { CacheTTL, CacheUtils } from '@utils/cache-utils'
 import { ResponseBuilder } from '@utils/response-builder'
 import { EpisodeController } from '@watch/controllers'
 import type { APIRoute } from 'astro'
@@ -8,16 +7,10 @@ import type { APIRoute } from 'astro'
 export const GET: APIRoute = rateLimit(
   redisConnection(async ({ url }) => {
     try {
-      const { id, page } = EpisodeController.validateRequest(url)
-      const cacheKey = `episodes:${id}-${page}`
+        const data = await EpisodeController.handleGetEpisodes(url)
 
-      const data = await CacheUtils.withCache(
-        cacheKey,
-        () => EpisodeController.handleGetEpisodes(url),
-        { ttl: CacheTTL.ONE_HOUR }
-      )
 
-      return ResponseBuilder.success({ data })
+      return ResponseBuilder.success(data )
     } catch (error: any) {
       return ResponseBuilder.fromError(error, 'GET /api/episodes')
     }
