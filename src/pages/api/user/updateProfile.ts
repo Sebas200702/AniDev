b78a7dc8850382437f9ca843afc2de6629b35f33
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-26 19:30:09 -0500

    fix(user-api): simplify error handling in update profile endpoint

--- src/pages/api/user/updateProfile.ts
+++ src/pages/api/user/updateProfile.ts
@@ -7,7 +7,6 @@ export const POST: APIRoute = checkSession(async ({ request, cookies }) => {
   try {
     const data = await UserController.handleUpdateUserImages(request, cookies)
     return ResponseBuilder.success({
-      message: 'Profile updated successfully',
       data,
     })
   } catch (error) {
