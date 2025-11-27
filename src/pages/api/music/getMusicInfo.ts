2e149079c101c831491217a551be3d67b497628d
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-26 19:29:28 -0500

    fix(music-api): remove caching logic to simplify music info retrieval

--- src/pages/api/music/getMusicInfo.ts
+++ src/pages/api/music/getMusicInfo.ts
@@ -1,19 +1,11 @@
 import { redisConnection } from '@middlewares/redis-connection'
 import { MusicController } from '@music/controllers'
-import { CacheTTL, CacheUtils } from '@utils/cache-utils'
 import { ResponseBuilder } from '@utils/response-builder'
 import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = redisConnection(async ({ url }) => {
   try {
-    const themeId = MusicController.validateThemeId(url)
-    const cacheKey = `MusicInfo_${themeId}`
-
-    const data = await CacheUtils.withCache(
-      cacheKey,
-      () => MusicController.handleGetMusicInfo(url),
-      { ttl: CacheTTL.ONE_DAY }
-    )
+    const data = await MusicController.handleGetMusicInfo(url)
 
     return ResponseBuilder.success(data)
   } catch (error) {
