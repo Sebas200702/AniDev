e5682c260d718782338c9509f5c215d75d7da2ce
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-26 19:29:58 -0500

    fix(user-api): simplify error handling in save user preferences endpoint

--- src/pages/api/user/saveUserPreferences.ts
+++ src/pages/api/user/saveUserPreferences.ts
@@ -7,7 +7,6 @@ export const POST: APIRoute = checkSession(async ({ request, cookies }) => {
   try {
     const data = await UserController.handleUpdatePreferences(request, cookies)
     return ResponseBuilder.success({
-      message: 'Preferences updated successfully',
       data,
     })
   } catch (error) {
