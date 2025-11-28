d8c334082da62ecca545a1cd58cc4b3ee141262d
Sebas200702 <sebastorregroza6@gmail.com>
2025-11-27 20:51:51 -0500

    fix(auth-form): update layout structure for improved responsiveness

--- src/domains/auth/components/auth-form/auth-form.tsx
+++ src/domains/auth/components/auth-form/auth-form.tsx
@@ -8,11 +8,11 @@ interface Props {
 
 export const AuthFormulary = ({ bgImage, isSignUp }: Props) => {
   return (
-    <section className="flex h-full max-h-[75vh] w-full items-center justify-center text-white md:mt-20 md:px-20">
-      <article className="bg-Complementary border-enfasisColor/30 relative flex h-full w-full max-w-7xl overflow-hidden rounded-lg border-1">
+
+      <article className="bg-Complementary border-enfasisColor/30 relative flex h-[80dvh] w-full max-w-7xl overflow-hidden rounded-lg border-1">
         <Aside bgImage={bgImage} isSignUp={isSignUp} />
         <MainContainer />
       </article>
-    </section>
+
   )
 }
