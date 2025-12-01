3d9d2e0b163ff74faef908619161837498b6b51a
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 14:06:46 -0500

    style(music-card-list): update type to AnimeSong for consistency and improve key usage in map function

--- src/domains/music/components/music-page/music-card-list.tsx
+++ src/domains/music/components/music-page/music-card-list.tsx
@@ -1,11 +1,9 @@
 import { MusicCard } from '@music/components/music-card/music-card'
-import type { AnimeSongWithImage } from '@music/types'
+import type { AnimeSong } from '@music/types'
 import { useFetch } from '@shared/hooks/useFetch'
 
 export const MusicCardLayout = () => {
-  const { data: trending, loading: trendingLoading } = useFetch<
-    AnimeSongWithImage[]
-  >({
+  const { data: trending, loading: trendingLoading } = useFetch<AnimeSong[]>({
     url: `/music?order_by=score&limit_count=12&anime_year=2025&unique_per_anime=true`,
   })
 
@@ -28,7 +26,9 @@ export const MusicCardLayout = () => {
     <div>
       <h2 className="subtitle mb-6">Trending now</h2>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
-        {trending?.map((song) => <MusicCard key={song.song_id} song={song} />)}
+        {trending?.map((song) => (
+          <MusicCard key={song.theme_id} song={song} />
+        ))}
       </div>
     </div>
   )
