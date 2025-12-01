4a06a7ca788c6203d8ff244c67e6103d4b5d0bd8
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 13:41:36 -0500

    style(auth-layout-main): improve code formatting and enhance readability

--- src/domains/auth/components/auth-layout/auth-layout-main.tsx
+++ src/domains/auth/components/auth-layout/auth-layout-main.tsx
@@ -1,11 +1,14 @@
 import { AuthNavigation } from '@auth/components/auth-navigation/auth-navigation'
 import { StepContent } from '@auth/components/auth-steps/auth-step-content'
+import { UseAuthOpenForm } from '@auth/hooks/use-auth-open-form'
 import type { AuthStep } from '@auth/types'
 
 interface Props {
   currentStep: number
   step: AuthStep
   onSubmit: (e: React.FormEvent) => void
+  steps: AuthStep[]
+
   onNext: () => void
   onPrev: () => void
   isSignUp: boolean
@@ -17,6 +20,7 @@ interface Props {
 
 export const Main = ({
   step,
+  steps,
   currentStep,
   isSignUp,
   onSubmit,
@@ -27,6 +31,7 @@ export const Main = ({
   isLastStep,
   isFirstStep,
 }: Props) => {
+  const { handleSignIn, handleSignUp } = UseAuthOpenForm()
   return (
     <section className="bg-Complementary/50 z-10 flex h-full w-full flex-col rounded-md backdrop-blur-sm">
       <form
@@ -56,23 +61,32 @@ export const Main = ({
             isLastStep={isLastStep}
             isSignUp={isSignUp}
             loading={loading}
+            steps={steps}
             onPrev={onPrev}
           />
           {currentStep === 1 && (
             <footer className="text-Primary-200 text-center text-sm">
               {isSignUp ? (
                 <p>
-                  <span>Don&apos;t have an account?</span>{' '}
-                  <a href="/signup" className="text-blue-500 hover:underline">
-                    Sign up
-                  </a>
+                  Already have an account?{' '}
+                  <button
+                    type="button"
+                    onClick={() => handleSignIn(300)}
+                    className="text-blue-500 hover:underline"
+                  >
+                    Sign in
+                  </button>
                 </p>
               ) : (
                 <p>
-                  Already have an account?{' '}
-                  <a href="/signin" className="text-blue-500 hover:underline">
-                    Sign in
-                  </a>
+                  <span>Don&apos;t have an account?</span>{' '}
+                  <button
+                    type="button"
+                    onClick={() => handleSignUp(300)}
+                    className="text-blue-500 hover:underline"
+                  >
+                    Sign up
+                  </button>
                 </p>
               )}
             </footer>
