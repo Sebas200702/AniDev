c4858c3e3b4a164b0dd25465d4a6d1b10d67eb38
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 14:21:22 -0500

    refactor(get-jikan-base): simplify conditional statement formatting for clarity

--- src/domains/recommendations/utils/get-jikan-base.ts
+++ src/domains/recommendations/utils/get-jikan-base.ts
@@ -5,11 +5,7 @@ export async function getJikanBase(
   userProfile: any,
   context: RecommendationContext
 ) {
-
-  if (
-    !context.data.currentAnime?.mal_id  ||
-    !context.data.currentAnime?.title
-  ) {
+  if (!context.data.currentAnime?.mal_id || !context.data.currentAnime?.title) {
     return await pickAnimeForJikan(userProfile)
   }
   return {
