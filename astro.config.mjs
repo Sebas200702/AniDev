e44a9d80dc374bf99e2482fd697fac5ff94a9885
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 13:26:06 -0500

    style(config): format astro.config.mjs for improved readability

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,31 +6,31 @@ import { defineConfig } from 'astro/config'
 import auth from 'auth-astro'
 
 export default defineConfig({
-    output: 'server',
-    site: 'https://anidev.app',
-    adapter: vercel(),
-    integrations: [
-        react(),
-        auth(),
-        sentry({
-            project: "anidev",
-            org: "sebas2007",
-            authToken: process.env.SENTRY_AUTH_TOKEN,
-        }),
-    ],
-    compressHTML: true,
-    build: {
-        inlineStylesheets: 'auto',
-    },
-    prefetch: {
-        prefetchAll: true,
-        defaultStrategy: 'viewport',
-    },
+  output: 'server',
+  site: 'https://anidev.app',
+  adapter: vercel(),
+  integrations: [
+    react(),
+    auth(),
+    sentry({
+      project: 'anidev',
+      org: 'sebas2007',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
 
-    vite: {
-        plugins: [tailwindcss()],
-        ssr: {
-            noExternal: ['@vidstack/react'],
-        },
+  vite: {
+    plugins: [tailwindcss()],
+    ssr: {
+      noExternal: ['@vidstack/react'],
     },
+  },
 })
