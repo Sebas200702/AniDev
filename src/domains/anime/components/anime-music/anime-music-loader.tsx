2e350c4b29ced588a4cf8a38ac3661fb35f12d75
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 13:37:32 -0500

    style(anime-music-loader): improve layout and spacing for better visual consistency

--- src/domains/anime/components/anime-music/anime-music-loader.tsx
+++ src/domains/anime/components/anime-music/anime-music-loader.tsx
@@ -1,10 +1,19 @@
+import { Loader } from '@shared/components/loader'
 export const AnimeMusicLoader = () => {
-  return Array.from({ length: 8 }, (_, i) => (
-    <div
-      key={i + 1}
-      className="mx-auto flex aspect-[100/30] h-full w-full animate-pulse flex-row rounded-lg bg-zinc-800 md:max-h-36"
-    >
-      <div className="aspect-[225/330] h-full animate-pulse rounded-l-lg bg-zinc-700 object-cover object-center transition-all ease-in-out md:max-h-36"></div>
+  return (
+    <div className="z-10 flex flex-col gap-8 rounded-lg p-4 md:p-6">
+      <h2 className="text-lxx font-bold">Anime Music</h2>
+
+      <ul className="grid grid-cols-1 gap-4 md:gap-6 xl:grid-cols-2">
+        {Array.from({ length: 8 }, (_, i) => (
+          <Loader
+            key={i + 1}
+            className="mx-auto flex aspect-[100/30] h-full w-full animate-pulse flex-row rounded-lg bg-zinc-800 md:max-h-36"
+          >
+            <Loader className="aspect-[225/330] h-full animate-pulse rounded-l-lg bg-zinc-700 object-cover object-center transition-all ease-in-out md:max-h-36"></Loader>
+          </Loader>
+        ))}
+      </ul>
     </div>
-  ))
+  )
 }
