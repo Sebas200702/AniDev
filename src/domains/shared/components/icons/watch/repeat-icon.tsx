f922f3d116c6d7c0d3dd4b7253273f56973a3a4d
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 14:33:48 -0500

    feat(repeat-icon): rename RepeatIcon to RepeatIconOn and add RepeatIconOff component

--- src/domains/shared/components/icons/watch/repeat-icon.tsx
+++ src/domains/shared/components/icons/watch/repeat-icon.tsx
@@ -1,6 +1,6 @@
 import type { IconProps } from '@shared/types'
 
-export const RepeatIcon = ({ className }: IconProps) => {
+export const RepeatIconOn = ({ className }: IconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -14,6 +14,26 @@ export const RepeatIcon = ({ className }: IconProps) => {
     >
       <path stroke="none" d="M0 0h24v24H0z" />
       <path d="M4 12V9a3 3 0 0 1 3-3h13m-3-3 3 3-3 3M20 12v3a3 3 0 0 1-3 3H4m3 3-3-3 3-3" />
+    </svg>
+  )
+}
+
+export const RepeatIconOff = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.75"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 12v-3c0 -1.336 .873 -2.468 2.08 -2.856m3.92 -.144h10m-3 -3l3 3l-3 3" />
+      <path d="M20 12v3a3 3 0 0 1 -.133 .886m-1.99 1.984a3 3 0 0 1 -.877 .13h-13m3 3l-3 -3l3 -3" />
+      <path d="M3 3l18 18" />
     </svg>
   )
 }
