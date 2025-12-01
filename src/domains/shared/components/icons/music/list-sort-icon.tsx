ebe2344093bfab46432fb2c83d0468c23e04afe5
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 14:33:01 -0500

    feat(list-sort-icon): add ListSortIcon component for improved UI consistency

---
+++ src/domains/shared/components/icons/music/list-sort-icon.tsx
@@ -0,0 +1,19 @@
+import type { IconProps } from '@shared/types'
+export const ListSortIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.75"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M4 8l16 0" />
+      <path d="M4 16l16 0" />
+    </svg>
+  )
+}
