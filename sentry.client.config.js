bd62d901bd65d40d4c55183ba192a9db338f838c
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 13:35:27 -0500

    style(sentry): standardize quotes and formatting in sentry.client.config.js

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,8 +1,8 @@
-import * as Sentry from "@sentry/astro";
+import * as Sentry from '@sentry/astro'
 
 Sentry.init({
-    dsn: "https://74a5458921d2e070f974160bd17ef9a4@o4510429009674240.ingest.us.sentry.io/4510429062037504",
-    // Adds request headers and IP for users, for more info visit:
-    // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
-    sendDefaultPii: true,
-});
+  dsn: 'https://74a5458921d2e070f974160bd17ef9a4@o4510429009674240.ingest.us.sentry.io/4510429062037504',
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+})
