d15f91b67c6b4572f1927eefe16a10622c0c1cd5
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 13:37:23 -0500

    refactor(anime-music-container): simplify props and enhance data handling with DataWrapper

--- src/domains/anime/components/anime-music/anime-music-container.tsx
+++ src/domains/anime/components/anime-music/anime-music-container.tsx
@@ -1,45 +1,26 @@
-import { AnimeMusic } from '@anime/components/anime-music/anime-music'
 import type { AnimeSong } from '@music/types'
-import { formatSongs } from '@music/utils/format-songs'
+import { DataWrapper } from '@shared/components/data-wrapper'
 import { useFetch } from '@shared/hooks/useFetch'
-import { useEffect, useState } from 'react'
+import { AnimeMusic } from './anime-music'
+import { AnimeMusicLoader } from './anime-music-loader'
 
 interface Props {
   animeId: number
-  image: string
-  placeholder: string
-  banner_image: string
-  anime_title: string
 }
-export const AnimeMusicContainer = ({
-  animeId,
-  image,
-  placeholder,
-  banner_image,
-  anime_title,
-}: Props) => {
-  const [songs, setSongs] = useState<AnimeSong[]>([])
-
+export const AnimeMusicContainer = ({ animeId }: Props) => {
   const { data, error, loading } = useFetch<AnimeSong[]>({
-    url: `/music/getAnimeMusic?animeId=${animeId}`,
+    url: `/music/getAnimeMusic?anime_id=${animeId}`,
   })
 
-  useEffect(() => {
-    if (data) {
-      const formattedSongs = formatSongs(data)
-      setSongs(formattedSongs)
-    }
-  }, [data])
-
   return (
-    <AnimeMusic
-      songs={songs}
+    <DataWrapper
+      data={data!}
+      error={error}
       loading={loading}
-      image={image}
-      placeholder={placeholder}
-      banner_image={banner_image}
-      anime_title={anime_title}
-      error={error!}
-    />
+      noDataFallback={<AnimeMusicLoader />}
+      loadingFallback={<AnimeMusicLoader />}
+    >
+      {(songs) => <AnimeMusic songs={songs} />}
+    </DataWrapper>
   )
 }
