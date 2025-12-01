64081d672e113946b94ccfb2385598d34a2ff720
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 14:42:22 -0500

    refactor(sign-in-up): update import style and improve button click handlers

--- src/domains/shared/components/layout/nav-bar/sign-in-up.tsx
+++ src/domains/shared/components/layout/nav-bar/sign-in-up.tsx
@@ -1,4 +1,4 @@
-import { UseAuthOpenForm } from "@auth/hooks/use-auth-open-form"
+import { UseAuthOpenForm } from '@auth/hooks/use-auth-open-form'
 export const SignInUp = () => {
   const { handleSignIn, handleSignUp } = UseAuthOpenForm()
 
@@ -6,13 +6,13 @@ export const SignInUp = () => {
     <div className="flex-row flex w-full ">
       <button
         className="button-primary w-full py-2 text-sm"
-        onClick={handleSignIn}
+        onClick={() => handleSignIn()}
       >
         Login
       </button>
       <button
         className="button-secondary ml-2 w-full   py-2 text-sm"
-        onClick={handleSignUp}
+        onClick={() => handleSignUp()}
       >
         Sign Up
       </button>
