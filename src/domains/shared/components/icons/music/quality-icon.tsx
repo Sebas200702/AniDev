fa046f30a7a1e59df9eb01981e28dc28b43eeb53
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 14:33:27 -0500

    feat(quality-icon): add QualityIcon component for music representation

---
+++ src/domains/shared/components/icons/music/quality-icon.tsx
@@ -0,0 +1,19 @@
+import type { IconProps } from '@shared/types'
+export const QualityIcon = ({ className }: IconProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.75"
+      className={className}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M3 7a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+      <path d="M7 9v2a1 1 0 0 0 1 1h1M10 9v6M14 9v6M17 9l-2 3 2 3M15 12h-1" />
+    </svg>
+  )
+}
