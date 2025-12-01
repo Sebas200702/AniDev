bba309cbed6fb73616eddcaee73d3b450e1cd4a6
Sebas200702 <sebastorregroza6@gmail.com>
2025-12-01 15:23:42 -0500

    refactor(artist-info-song-list.tsx): update song prop type to use AnimeSong for consistency

--- src/domains/artist/components/artist-info/artist-info-song-list.tsx
+++ src/domains/artist/components/artist-info/artist-info-song-list.tsx
@@ -1,15 +1,15 @@
 import { MusicCard } from '@music/components/music-card/music-card'
-import type { AnimeSongWithImage } from '@music/types'
+import type { AnimeSong } from '@music/types'
 import { InfoSection } from 'domains/shared/components/layout/base/InfoSection'
 
 interface Props {
-  songs: AnimeSongWithImage[]
+  songs: AnimeSong[]
 }
 export const ArtistSongList = ({ songs }: Props) => {
   return (
     <InfoSection title="Songs" ulClassName="w-full">
       {songs.map((song) => (
-        <MusicCard key={song.song_id} song={song} isMini />
+        <MusicCard key={song.theme_id} song={song} isMini />
       ))}
     </InfoSection>
   )
