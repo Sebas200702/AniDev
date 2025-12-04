4f085137b9dcba014bea320b03fc505fe21468f5
Sebas2007 <sebastorregroza6@gmail.com>
2025-12-04 15:54:39 -0500

    refactor(anime-info-similar-to-loader.tsx): adjust key index for LoadingCard to prevent potential rendering issues

--- src/domains/anime/components/anime-info/anime-info-similar-to/anime-info-similar-to-loader.tsx
+++ src/domains/anime/components/anime-info/anime-info-similar-to/anime-info-similar-to-loader.tsx
@@ -1,7 +1,5 @@
 import { LoadingCard } from '@search/components/search-results/loading-card'
 
 export const AnimeSimilarToLoader = () => {
-  return Array(10)
-    .fill(null)
-    .map((_, idx) => <LoadingCard key={idx} />)
+  return new Array(10).fill(null).map((_, idx) => <LoadingCard key={idx + 1} />)
 }
