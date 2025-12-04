108edf8d9db02cc5fab0afc57cea1de27a5aef0d
Sebas2007 <sebastorregroza6@gmail.com>
2025-12-04 15:55:55 -0500

    refactor(colletion-loader.tsx): replace Array constructor with new Array for clarity in loading placeholders

--- src/domains/collection/components/collection-list/colletion-loader.tsx
+++ src/domains/collection/components/collection-list/colletion-loader.tsx
@@ -27,7 +27,7 @@ export const PageCollectionLoader = () => {
     <div className="mb-20 flex h-full w-full flex-col gap-4 px-20 pt-[35dvh]">
       <div className="mb-6 h-24 w-full animate-pulse rounded-lg bg-zinc-800 duration-300 ease-in-out md:h-14 md:w-96"></div>
       <div className="grid grid-cols-1 gap-10 md:grid-cols-2 md:gap-10 xl:grid-cols-3">
-        {Array(30)
+        {new Array(30)
           .fill(0)
           .map((_, i) => (
             <div
