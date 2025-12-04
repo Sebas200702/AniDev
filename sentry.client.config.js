6ef27ab8dc1167f1682e5f059aa88c9e690baa0c
Sebas2007 <sebastorregroza6@gmail.com>
2025-12-04 15:39:41 -0500

    refactor(sentry.client.config.js): replace hardcoded DSN with environment variable for improved security

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/astro'
 
 Sentry.init({
-  dsn: 'https://74a5458921d2e070f974160bd17ef9a4@o4510429009674240.ingest.us.sentry.io/4510429062037504',
+  dsn: import.meta.env.SENTRY_DSN,
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
