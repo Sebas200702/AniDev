f74b50143cc792824c521043ce420862553ea645
Sebas2007 <sebastorregroza6@gmail.com>
2025-12-08 22:17:37 -0500

    refactor(anime-info-container): enhance fetch URL to include parental control and skip logic

--- src/domains/anime/components/anime-info/anime-info-container.tsx
+++ src/domains/anime/components/anime-info/anime-info-container.tsx
@@ -3,19 +3,23 @@ import { AnimeLoader } from '@anime/components/anime-info/anime-info-loader'
 import type { Anime } from '@anime/types'
 import { DataWrapper } from '@shared/components/data-wrapper'
 import { useFetch } from '@shared/hooks/useFetch'
+import { useGlobalUserPreferences } from '@user/stores/user-store'
 
 interface Props {
   id: number
 }
 
 export const AnimeInfoContainer = ({ id }: Props) => {
+  const { parentalControl } = useGlobalUserPreferences()
+
   const {
     data: animeData,
     loading,
     error,
   } = useFetch<Anime>({
-    url: `/animes/getAnime?id=${id}`,
+    url: `/animes/getAnime?id=${id}&parental_control=${parentalControl}`,
     navigate404: true,
+    skip: parentalControl === null,
   })
 
   return (
