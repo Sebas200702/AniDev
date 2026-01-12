504055801ebe6e0cdf4793d32f7839bc09485630
Sebastian Torregroza <143149476+Sebas200702@users.noreply.github.com>
2026-01-12 21:23:29 +0000

    fix: eliminar el componente FooterLoader y simplificar la lógica del componente Footer

--- src/domains/watch/components/footer.tsx
+++ src/domains/watch/components/footer.tsx
@@ -4,21 +4,15 @@ interface FooterProps {
   title: string
   episode: number
 }
-const FooterLoader = () => {
-  return (
-    <footer className="flex h-full w-full animate-pulse flex-col gap-8 rounded-lg p-4 transition-all duration-300 ease-in-out">
-      <div className="h-16 w-full animate-pulse rounded-md bg-zinc-800 transition-all duration-200 ease-in-out md:w-120 xl:h-8"></div>
-      <div className="h-32 w-full animate-pulse rounded-md bg-zinc-800 transition-all duration-200 ease-in-out xl:h-20"></div>
-    </footer>
-  )
-}
+
 export const Footer = ({
   episodeTitle,
   title,
   episode,
   episodeDescription,
 }: FooterProps) => {
-  if (!title || !episode) return <FooterLoader />
+
+  
   return (
     <footer>
       <h3 className="h-auto p-4 text-xl font-bold text-white">
